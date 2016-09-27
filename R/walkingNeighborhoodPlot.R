1fdaa615687bb0426d144604246cec8aa9f2ace8
lindbrook <lindbrook@gmail.com>
2016-09-27 15:31:32 -0700

    fix walkingNeighborhoodPlot(streets = TRUE) and update documentation

--- R/walkingNeighborhoodPlot.R
+++ R/walkingNeighborhoodPlot.R
@@ -1,15 +1,19 @@
 #' Plots walking neighborhoods.
 #'
-#' Plots walking neighborhoods based on the weighted shortest paths between a fatalities and the nearest pump. Beta version. 
+#' Plots walking neighborhoods based on the weighted shortest paths between a fatalities and the nearest pump.
 #'
-#' Currently computationally intensive. For now, it is better to run in batch mode in parallel with multiple cores. See \code{vignette}("walking.neighborhoods") for details
+#' Currently computationally intensive (streets = TRUE appox 2.5 minutes; streets = FALSE appox 2 minutes). For better performance, run in batch mode in parallel with multiple cores. See \code{vignette}("walking.neighborhoods") for details.
 #' @param selection Numeric. Default is NULL; all pumps are used. Otherwise, selection by a vector of numeric IDs: 1 to 13 for \code{pumps}; 1 to 14 for \code{pumps.vestry}
 #' @param vestry Logical. TRUE uses the 14 pumps from the Vestry Report. FALSE uses the 13 in the original map.
 #' @param streets TRUE plots neighborhoods by street. FALSE plots orthogonal neighborhoods.
 #' @param weighted Logical. Shortest path weighted by road distance.
 #' @param add.obs Logical. Include observed addresses.
 #' @param add.landmarks Logical. Include landmarks.
-#' @param cores Numeric or "all". Number of cores to use; default is 1. If you select more than 1 core or use "all", which uses parallel::detectCores(), you should run this function in batch, not interactive mode (i.e., not in GUI).
+#' @param arrow Logical. Use arrows.
+#' @param alpha.level, Numeric. If desired, set alpha level.
+#' @param obs.lwd Numeric. Set line width for observed paths.
+#' @param sim.lwd Numeric. Set line width for expected paths
+#' @param cores Numeric or "all". Number of cores to use (default is 1). If you select more than 1 core or use "all", which uses parallel::detectCores(), you should run this function in batch not interactive mode (i.e., not in GUI).
 #' @return A base R graphics plot.
 #' @seealso \code{addLandmarks()}
 #'
@@ -17,7 +21,7 @@
 
 walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
   streets = TRUE, weighted = TRUE, add.obs = FALSE, add.landmarks = TRUE,
-  cores = 1L) {
+  arrow = FALSE, alpha.level = NULL, obs.lwd = 2, sim.lwd = 2, cores = 1L) {
 
   if (vestry) {
     if (is.null(selection) == FALSE) {
@@ -74,6 +78,7 @@ walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
     select.pumps <- pump.coordinates
     pump.names <- names(pump.coordinates)
   } else {
+    colors <- colors[selection]
     select.pumps <- pump.coordinates[selection]
     pump.names <- names(pump.coordinates[selection])
   }
@@ -93,25 +98,26 @@ walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
 
   # Expected cases
 
-  expected.cases <- cholera::ortho.proj.sp[cholera::ortho.proj.sp$case %in%
-    c(no.pump, falconberg) == FALSE, "case"]
+  selected.case.sp <- cholera::ortho.proj.sp[cholera::ortho.proj.sp$case %in%
+    c(no.pump, falconberg) == FALSE, ]
 
-  selected.case.sp <- lapply(expected.cases, caseSelector, obs = FALSE)
+  selected.case.sp <- split(selected.case.sp, selected.case.sp$case)
 
-  case.road.segments.sp <- parallel::mclapply(expected.cases, function(x) {
+  # Integrates case into road netowrk
+  
+  case.road.segments.sp <- parallel::mclapply(selected.case.sp, function(x) {
     seg <- unlist(strsplit(road.segments$id, "a"))
     seg <- unlist(strsplit(seg, "b"))
-    case <- cholera::ortho.proj.sp[cholera::ortho.proj.sp$case == x, ]
 
-    temp <- road.segments[which(case$road.segment == seg), ]
-    case.coord <- case[, c("x.proj", "y.proj")]
+    temp <- road.segments[which(x$road.segment == seg), ]
+    case.coord <- x[, c("x.proj", "y.proj")]
 
     distance <- vapply(seq_len(nrow(temp)), function(i) {
       sqrt(sum((case.coord - temp[i, c("x1", "y1")])^2)) +
       sqrt(sum((case.coord - temp[i, c("x2", "y2")])^2))
     }, numeric(1L))
 
-    temp <- temp[which(signif(temp$d) == signif(distance)), ]
+    temp <- temp[which(signif(temp$d) == signif(distance)), , drop = FALSE]
     appended <- rbind(temp, temp)
     appended[1, c("x2", "y2")] <- case.coord
     appended[2, c("x1", "y1")] <- case.coord
@@ -128,7 +134,7 @@ walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
                        (appended$y1 - appended$y2)^2)
 
     road.segments2 <- road.segments
-    sel <- road.segments2$id != case$road.segment
+    sel <- road.segments2$id != x$road.segment
     rbind(road.segments2[sel, ], appended)
   }, mc.cores = cores)
 
@@ -137,51 +143,36 @@ walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
     igraph::graph_from_data_frame(edge.list, directed = FALSE)
   })
 
-  wtd.dist.sp <- parallel::mclapply(expected.cases, function(x) {
-    id <- which(x == expected.cases)
-    g <- case.network.sp[[id]]
-    case <- selected.case.sp[[id]]
+  wtd.dist.sp <- parallel::mclapply(seq_along(case.network.sp), function(x) {
+    # id <- which(x == expected.cases)
+    g <- case.network.sp[[x]]
+    case <- selected.case.sp[[x]]
     case.coord <- paste0(case$x.proj, "-", case$y.proj)
     case.node <- which(igraph::V(g)$name == case.coord)
 
-    pump.coordinates <- paste0(cholera::ortho.proj.pump$x.proj, "-",
-      cholera::ortho.proj.pump$y.proj)
-    names(pump.coordinates) <- paste0("p", seq_along(pump.coordinates))
-
-    if (is.null(selection)) {
-      select.pumps <- pump.coordinates
-      pump.names <- names(pump.coordinates)
-    } else {
-      select.pumps <- pump.coordinates[selection]
-      pump.names <- names(pump.coordinates)[selection]
-    }
-
-    pump.nodes <- vapply(seq_along(select.pumps), function(i) {
-      which(igraph::V(g)$name == select.pumps[i])
+    pump.nodes <- vapply(pump.coordinates, function(p) {
+      which(igraph::V(g)$name == p)
     }, numeric(1L))
 
-    wts <- case.road.segments.sp[[id]]$d
+    wts <- case.road.segments.sp[[x]]$d
     d <- unname(igraph::distances(g, case.node, pump.nodes, weights = wts))
   }, mc.cores = cores)
 
   wtd.dist.sp <- data.frame(do.call(rbind, wtd.dist.sp))
   names(wtd.dist.sp) <- pump.names
 
-
+  ortho.cases <- do.call(rbind, selected.case.sp)
   nearest.pump.sp <- names(wtd.dist.sp)[apply(wtd.dist.sp, 1, which.min)]
-
-  nearest.pump.sp <- data.frame(case = expected.cases,
-    wtd.pump = nearest.pump.sp)
+  nearest.pump.sp <- data.frame(case = ortho.cases$case,
+                                wtd.pump = nearest.pump.sp)
 
   nearest.pump.sp$wtd.pump <- as.numeric(substr(nearest.pump.sp$wtd.pump, 2,
     length(nearest.pump.sp$wtd.pump)))
 
-  # ## ------------------- Graphs ------------------- ##
+  ## ------------------- Graphs ------------------- ##
 
-  sel <- cholera::ortho.proj.sp$case %in% expected.cases
-
-  exp.address <- data.frame(cholera::ortho.proj.sp[sel,
-    c("case", "x.proj", "y.proj")], wtd.pump = nearest.pump.sp$wtd.pump)
+  exp.address <- data.frame(ortho.cases[, c("case", "x.proj", "y.proj")],
+    wtd.pump = nearest.pump.sp$wtd.pump)
 
   exp.address$col <- NA
 
@@ -198,13 +189,45 @@ walkingNeighborhoodPlot <- function(selection = NULL, vestry = FALSE,
   if (streets) {
     invisible(lapply(roads.list, lines, col = "gray"))
     invisible(lapply(border.list, lines))
+
+    invisible(lapply(exp.address$case, function(x) {
+      id <- which(exp.address$case == x)
+      g <- case.network.sp[[id]]
+      case <- selected.case.sp[[id]]
+      case.coord <- paste0(case$x.proj, "-", case$y.proj)
+      case.node <- which(igraph::V(g)$name == case.coord)
+      sel <- nearest.pump.sp[nearest.pump.sp$case == x, "wtd.pump"]
+      pump.node <- which(igraph::V(g)$name == pump.coordinates[sel])
+      wts <- case.road.segments.sp[[id]]$d
+      case.path <- unlist(igraph::shortest_paths(g, case.node, pump.node,
+        weights = wts)$vpath)
+      case.path <- names(case.path)
+      dat <- numericNodeCoordinates(case.path)
+      n1 <- dat[1:(nrow(dat) - 1), ]
+      n2 <- dat[2:nrow(dat), ]
+
+      if (is.null(alpha.level)) {
+        seg.col <- exp.address[exp.address$case == x , "col"]
+      } else {
+        seg.col <- scales::alpha(exp.address[exp.address$case == x , "col"],
+          alpha.level)
+      }
+
+      if (arrow) {
+        arrows(n1$x, n1$y, n2$x, n2$y, col = seg.col, lwd = sim.lwd,
+          length = 0.075)
+      } else {
+        segments(n1$x, n1$y, n2$x, n2$y, col = seg.col, lwd = sim.lwd)
+      }
+    }))
+
     text(cholera::pumps[, c("x", "y")], label = cholera::pumps$id, cex = 1,
-      pos = 1)
+         pos = 1)
     points(cholera::pumps[, c("x", "y")], pch = 2, col = colors)
-    invisible(lapply(expected.cases, drawPath, obs = FALSE))
     title(main = "Expected Paths")
+
   } else {
-    points(cholera::regular.cases[expected.cases, ], col = exp.address$col,
+    points(cholera::regular.cases[exp.address$case, ], col = exp.address$col,
       pch = 15)
     invisible(lapply(roads.list, lines, col = "black"))
     invisible(lapply(border.list, lines))
@@ -239,67 +262,6 @@ snowColors <- function(vestry = FALSE) {
   } else {
     c("dodgerblue", "gray", colors.dark[1:4], colors.pair[2], colors.dark[5:8],
       "red", colors.pair[1], "darkorange")
-  }
-}
-
-caseSelector <- function(x, obs = TRUE) {
-  if (obs) {
-    case <- cholera::ortho.proj[cholera::ortho.proj$case == x, ]
-  } else {
-    case <- cholera::ortho.proj.sp[cholera::ortho.proj.sp$case == x, ]
-  }
-  case
-}
-
-numericNodeCoordinates <- function(x) {
-  nodes <- do.call(rbind, (strsplit(x, "-")))
-  data.frame(x = as.numeric(nodes[, 1]), y = as.numeric(nodes[, 2]))
-}
-
-drawPath <- function(x, vestry = FALSE, obs.lwd = 2, sim.lwd = 2, 
-  arrows = FALSE, alpha = NULL, case.network.sp, case.road.segments.sp, 
-  selected.case.sp, expected.cases, exp.address, nearest.pump.sp) {
-
-  if (vestry == FALSE) {
-    pump.coordinates <- paste0(cholera::ortho.proj.pump$x.proj, "-",
-      cholera::ortho.proj.pump$y.proj)
-    names(pump.coordinates) <- paste0("p", seq_along(pump.coordinates))
-  } else {
-    pump.coordinates <- paste0(cholera::ortho.proj.pump.vestry$x.proj, "-",
-      cholera::ortho.proj.pump.vestry$y.proj)
-    names(pump.coordinates) <- paste0("p", seq_along(pump.coordinates))
-  }
-
-  id <- which(x == expected.cases)
-  g <- case.network.sp[[id]]
-  case <- selected.case.sp[[id]]
-  case.coord <- paste0(case$x.proj, "-", case$y.proj)
-  case.node <- which(igraph::V(g)$name == case.coord)
-
-  sel <- nearest.pump.sp[nearest.pump.sp$case == x, "wtd.pump"]
-  pump.node <- which(igraph::V(g)$name == pump.coordinates[sel])
-
-  wts <- case.road.segments.sp[[id]]$d
-  case.path <- unlist(igraph::shortest_paths(g, case.node, pump.node,
-    weights = wts)$vpath)
-  case.path <- names(case.path)
-
-  dat <- numericNodeCoordinates(case.path)
-  n1 <- dat[1:(nrow(dat) - 1), ]
-  n2 <- dat[2:nrow(dat), ]
-
-  if (is.null(alpha)) {
-    seg.col <- exp.address[exp.address$case == x , "col"]
-  } else {
-    seg.col <- scales::alpha(exp.address[exp.address$case == x , "col"],
-      alpha)
-  }
-
-  if (arrows) {
-    arrows(n1$x, n1$y, n2$x, n2$y, col = seg.col, lwd = sim.lwd,
-      length = 0.075)
-  } else {
-    segments(n1$x, n1$y, n2$x, n2$y, col = seg.col, lwd = sim.lwd)
   }
 }
 
@@ -344,4 +306,9 @@ integratePumpNodes <- function(road.segments, vestry = FALSE) {
   out$d <- sqrt((out$x1 - out$x2)^2 +
                           (out$y1 - out$y2)^2)
   out
+}
+
+numericNodeCoordinates <- function(x) {
+  nodes <- do.call(rbind, (strsplit(x, "-")))
+  data.frame(x = as.numeric(nodes[, 1]), y = as.numeric(nodes[, 2]))
 }
