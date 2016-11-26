ab7a6a11f198d013b314f2203a5a9e3a81dfc02e
lindbrook <lindbrook@gmail.com>
2016-11-25 20:03:07 -0800

    increase text label size

--- R/addLandmarks.R
+++ R/addLandmarks.R
@@ -12,47 +12,47 @@
 addLandmarks <- function() {
  # 28 Dean Street
   marx <- data.frame(x = 17.3855, y = 13.371 )
-  text(marx$x, marx$y, labels = "Karl\nMarx", cex = 0.5)
+  text(marx$x, marx$y, labels = "Karl\nMarx", cex = 0.75)
   points(marx$x, marx$y, pch = 15, cex = 1/3)
 
   # 18 Sackville Street
   snow <- data.frame(x = 10.22414, y = 4.383851)
-  text(snow$x, snow$y, labels = "John\nSnow", cex = 0.5)
+  text(snow$x, snow$y, labels = "John\nSnow", cex = 0.75)
   points(snow$x, snow$y, pch = 15, cex = 1/3)
 
   # St. Luke's Church; Henry Whitehead Assistant Curate
   st.luke <- data.frame(x = 14.94156, y = 11.25313)
-  text(st.luke$x, st.luke$y, labels = "St. Luke's\nChurch", cex = 0.5)
+  text(st.luke$x, st.luke$y, labels = "St. Luke's\nChurch", cex = 0.75)
   points(st.luke$x, st.luke$y, pch = 15, cex = 1/3)
 
   # Soho Square
   soho.sq <- data.frame(x = 18.07044, y = 15.85703)
-  text(soho.sq$x, soho.sq$y, labels = "Soho\nSquare", cex = 0.5)
+  text(soho.sq$x, soho.sq$y, labels = "Soho\nSquare", cex = 0.75)
 
   # Golden Square
   golden.sq <- data.frame(x = 11.90927, y = 8.239483)
-  text(golden.sq$x, golden.sq$y, labels = "Golden\nSquare", cex = 0.5)
+  text(golden.sq$x, golden.sq$y, labels = "Golden\nSquare", cex = 0.75)
 
   # St. James Workhouse
   st.james <- data.frame(x = 11.5, y = 13.48414 )
-  text(st.james$x, st.james$y, labels = "St James\nWorkhouse", cex = 0.5)
+  text(st.james$x, st.james$y, labels = "St James\nWorkhouse", cex = 0.75)
 
   # Lion Brewery (Huggins proprietors)
   brewery <- data.frame(x = 13.9022, y = 11.87315)
-  text(brewery$x, brewery$y, labels = "Lion\nBrewery", cex = 0.5)
+  text(brewery$x, brewery$y, labels = "Lion\nBrewery", cex = 0.75)
   points(brewery$x, brewery$y, pch = 15, cex = 1/3)
 
   # Falconberg Court and Mews (isolate)
   Falconberg <- data.frame(x = 19.5, y = 17.184)
   text(Falconberg$x, Falconberg$y, labels = "Falconberg\nCourt & Mews",
-    cex = 0.5)
+    cex = 0.75)
 
   # Adam and Eve Court (isolate)
   adam.eve <- cholera::roads[cholera::roads$name == "Adam and Eve Court",
     c("x", "y")]
   text(stats::quantile(adam.eve[, "x"], 0.25),
        stats::quantile(adam.eve[, "y"], 0.25),
-       labels = "Adam & Eve\nCourt", cex = 0.5)
+       labels = "Adam & Eve\nCourt", cex = 0.75)
 }
 
 #' Add plague pit (Marshall Street).
