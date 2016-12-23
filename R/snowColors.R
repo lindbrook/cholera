ed1c1b5b943afe3969f51d97ac8b1892eac861e7
lindbrook <lindbrook@gmail.com>
2016-12-23 14:25:19 -0800

    document snowColors()

---
+++ R/snowColors.R
@@ -0,0 +1,17 @@
+#' Creates a uniform set of colors for plots.
+#'
+#' @param vestry Logical. TRUE uses the 14 pumps in the Vestry Report. FALSE uses the original 13.
+#' @return A character vector of colors.
+#' @export
+
+snowColors <- function(vestry = FALSE) {
+  colors.pair <- RColorBrewer::brewer.pal(10, "Paired")
+  colors.dark <- RColorBrewer::brewer.pal(8, "Dark2")
+  if (vestry == FALSE) {
+    c("dodgerblue", "gray", colors.dark[1:4], colors.pair[2], colors.dark[5:8],
+      "red", colors.pair[1])
+  } else {
+    c("dodgerblue", "gray", colors.dark[1:4], colors.pair[2], colors.dark[5:8],
+      "red", colors.pair[1], "darkorange")
+  }
+}
