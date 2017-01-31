9f47a8afd6e7b0fedace53e47f9f2d794d6b9c67
lindbrook <lindbrook@gmail.com>
2017-01-31 15:51:36 -0800

    amend and update s3 pumpCases() methods

--- R/pumpCases.R
+++ R/pumpCases.R
@@ -1,38 +1,45 @@
 #' Numerical case IDs by Voronoi neighborhood.
 #'
 #' @param obj An object of class "voronoi" created by neighborhoodVoronoi().
-#' @param statistic Character. "address" summarizes the number of addresses in pump neighbohoods. "fatality", the default, summarizes the number of fatalities in pump neighborhoods.
 #' @return A list of pump neighborhoods with the numeric ID of observed cases.
 #' @export
 #' @examples
 #' dat <- neighborhoodVoronoi()
 #' pumpCases(dat)
-#'
-#' pumpCases(dat, statistic = "address")
 
-pumpCases <- function(obj, statistic) UseMethod("pumpCases", obj)
+pumpCases <- function(obj) UseMethod("pumpCases", obj)
 
-pumpCases.default <- function(obj, statistic) NULL
+pumpCases.default <- function(obj) NULL
 
 #' @export
-pumpCases.voronoi <- function(obj, statistic = "fatality") {
+pumpCases.voronoi <- function(obj) {
   if (class(obj) != "voronoi") {
     stop('Input object\'s class needs to be "voronoi".')
   }
 
-  statistic <- match.arg(statistic, choices = c("address", "fatality"))
+  output <- obj$statistic.data
+  if (is.null(obj$selection)) {
+    if (obj$vestry == TRUE) {
+      stats::setNames(output, paste0("p", 1:14))
+    } else {
+      stats::setNames(output, paste0("p", 1:13))
+    }
+  } else {
+    stats::setNames(output, paste0("p", obj$selection))
+  }
+}
 
-  if (statistic == "address") {
-    output <- lapply(obj$coordinates, function(cell) {
-      sp::point.in.polygon(cholera::fatalities.address$x,
-        cholera::fatalities.address$y, cell$x, cell$y)
-    })
-  } else if (statistic == "fatality") {
-    output <- lapply(obj$coordinates, function(cell) {
-      sp::point.in.polygon(cholera::fatalities.unstacked$x,
-        cholera::fatalities.unstacked$y, cell$x, cell$y)
-    })
+#' @export
+pumpCases.walking <- function(obj) {
+  if (class(obj) != "walking") {
+    stop('Input object\'s class needs to be "walking".')
   }
 
-  lapply(output, function(x) which(x == 1))
+  output <- obj$pump.case
+
+  if (obj$vestry == TRUE) {
+    stats::setNames(output, paste0("p", 1:14))
+  } else {
+    stats::setNames(output, paste0("p", 1:13))
+  }
 }
