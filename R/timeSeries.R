52c736dbd3a91bb6d3badc4f0f716eddc679a689
lindbrook <lindbrook@gmail.com>
2017-06-03 13:57:38 -0700

    consolidate time series data and functions

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -1,14 +1,14 @@
 #' Aggregate time series fatality data from the Vestry report.
 #'
 #' For use in integrating pumps and cases into road network needed to compute walking neighbohoods.
-#' @param vestry Logical. TRUE returns the data from the Vestry committee (Appendix B). FALSE (default) returns John Snow's contribution to the report (p.117).
+#' @param vestry Logical. TRUE returns the data from the Vestry committee (Appendix B, p. 175). FALSE (default) returns John Snow's contribution to the report (p.117).
 #' @return A R list with two objects: "data" and "source" ("snow" or "vestry").
 #' \itemize{
 #'   \item{\code{date}: Calendar date.}
-#'   \item{\code{fatal.attacks}: measure of fatality.}
 #'   \item{\code{deaths}: measure of fatality.}
+#'   \item{\code{fatal.attacks}: measure of fatality.}
 #' }
-#' @section Note: This function documents the code that generates \code{\link[cholera]{time.series.snow}} and \code{\link[cholera]{time.series.vestry}}.
+#' @section Note: the "snow" data appears on p. 117 of the report; the "vestry" data appear in Appendix B on p.175.
 #' @export
 
 timeSeries <- function(vestry = FALSE) {
@@ -20,40 +20,35 @@ timeSeries <- function(vestry = FALSE) {
 
     month.num <- c(7, rep(8, length(3:31)), rep(9, length(1:30)), 10)
 
-    ascertained <- c(rep(0, 3), rep(1, 3), rep(0, 3), 1, 2, 2, 0, 1, 2, 2, 1,
+    deaths <- c(rep(0, 3), rep(1, 3), rep(0, 3), 1, 2, 2, 0, 1, 2, 2, 1,
       3, 0, 3, 1, 0, 3, 0, 0, 1, 0, 2, 2, 4, 72, 127, 76, 71, 45, 40, 34, 30,
       24, 18, 15, 7, 13, 6, 8, 6, 5, 4, 4, 1, 0, 3, 3, 0, 1, 2, 0, 2, 1, 0, 1)
 
-    daily.fatal.attacks <- c(1, 1, 0, 1, 0, 1, rep(0, 3), 2, 3, 0, 3, 0, 3, 1,
+    fatal.attacks <- c(1, 1, 0, 1, 0, 1, rep(0, 3), 2, 3, 0, 3, 0, 3, 1,
       2, 2, 1, 2, 0, 2, 0, 0, 1, 0, 3, 2, 3, 34, 142, 128, 62, 55, 26, 28, 22,
       14, 6, 2, 3, 1, 3, 0, 1, 3, 4, 0, 1, 0, 0, 2, 0, 1, 0, 1, 0, 2, 0, 0, 1)
 
     calendar.date <- as.Date(paste0(1854, "-", month.num, "-", day))
 
-    dat <- data.frame(date = calendar.date,
-                      fatal.attacks = daily.fatal.attacks,
-                      deaths = ascertained)
-
-    output <- list(data = dat, source = "vestry")
+    output <- list(data = data.frame(date = calendar.date, deaths,
+      fatal.attacks))
 
   } else {
+    # Note: entry for an add'l 45 fatal attackes with "Date unknown".
     yr <- 1854
     mo <- c(rep("08", length(19:31)), rep("09", length(1:30)))
     day <- c(19:31, 1:30)
-
-    # +45 unknown
-    fatal.attacks <- c(rep(1, 3), 0, rep(1, 2), 0, rep(1, 4), 8, 56, 143, 116,
-      54, 46, 36, 20, 28, 12, 11, 5, 5, 1, 4, 0, 1, 4, 2, 3, 0, 0, 2, rep(1, 6),
-      rep(0, 3))
 
     deaths <- c(1, 0, 2, 0, 0, 2, 0, 0, 1, 0, 1, 2, 3, 70, 127, 76, 71, 45, 37,
       32, 30, 24, 18, 15, 6, 13, 6, 8, 6, 5, 2, 3, 0, 0, 2, 3, 0, 0, 2, 0, 2,
       1, 0)
 
-    dat <- data.frame(date = as.Date(paste0(yr, "-", mo, "-", day)),
-                      fatal.attacks = fatal.attacks, deaths)
+    fatal.attacks <- c(rep(1, 3), 0, rep(1, 2), 0, rep(1, 4), 8, 56, 143, 116,
+      54, 46, 36, 20, 28, 12, 11, 5, 5, 1, 4, 0, 1, 4, 2, 3, 0, 0, 2, rep(1, 6),
+      rep(0, 3))
 
-    output <- list(data = dat, source = "snow")
+    output <- list(data = data.frame(date = as.Date(paste0(yr, "-", mo, "-",
+      day)), deaths, fatal.attacks))
   }
 
   class(output) <- "time.series"
@@ -63,15 +58,15 @@ timeSeries <- function(vestry = FALSE) {
 #' Plot aggregate time series data from Vestry report.
 #'
 #' Plot aggregate fatality data and indicates the date of the removal of the handle of the Broad Street pump.
-#' @param x An object of class "time.series": \code{\link[cholera]{time.series.snow}} or \code{\link[cholera]{time.series.vestry}}.
-#' @param statistic Character. Fatality measure: either "fatal.attacks" (default) or "deaths".
+#' @param x An object of class "time.series" from timeSeries().
+#' @param statistic Character. Fatality measure: either "fatal.attacks", which is the default, or "deaths".
 #' @param pump.handle Logical. Indicate date of removal of Broad Street pump handle.
 #' @param ... Additional plotting parameters.
 #' @seealso \code{\link{timeSeries}}
 #' @export
 #' @examples
-#' plot(time.series.snow)
-
+#' plot(timeSeries())
+#' plot(timeSeries(), statistic = "deaths")
 
 plot.time.series <- function(x, statistic = "fatal.attacks",
   pump.handle = TRUE, ...) {
@@ -84,11 +79,7 @@ plot.time.series <- function(x, statistic = "fatal.attacks",
     stop('"statistic" must either be "deaths" or "fatal.attacks".')
   }
 
-  if (x$source == "vestry") {
-    time.series <- cholera::time.series.vestry$data
-  } else if (x$source == "snow") {
-    time.series <- cholera::time.series.snow$data
-  }
+time.series <- x$data
 
   plot(time.series$date, time.series[, statistic], type = "o", xlab = "Date",
     ylab = statistic)
