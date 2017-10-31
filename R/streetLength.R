79dd8dcc0fbc6fe8152721a6952e6a3c5d7d24e3
lindbrook <lindbrook@gmail.com>
2017-10-31 11:21:55 -0700

    udpate details about "raod.name" argument

--- R/streetLength.R
+++ R/streetLength.R
@@ -1,7 +1,7 @@
 #' Compute street length.
 #'
 #' Computes length of selected named street.
-#' @param road.name Character. Road name.
+#' @param road.name Character. Road name. Tries to correct for case and to remove extra spaces.
 #' @param unit Character. Unit of measurement: "meter" or "yard". Default is NULL, which returns the map's native scale.
 #' @return  An R vector of length one.
 #' @seealso \code{\link{roads}}, code{\link{road.segments}}, \code{\link{streetNameLocator}}, \code{\link{streetNumberLocator}}, \code{vignette("road.names")}
