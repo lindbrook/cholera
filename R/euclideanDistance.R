c9099d87762be098d6e8837122437a7fe51cb7a9
lindbrook <lindbrook@gmail.com>
2017-11-03 14:45:57 -0700

    set stringsAsFactors = FALSE

--- R/euclideanDistance.R
+++ R/euclideanDistance.R
@@ -53,7 +53,8 @@ euclideanDistance <- function(case, pump.select = NULL, vestry = FALSE,
     out <- data.frame(case,
                       distance = d,
                       pump = p.data[p.data$id == pump.select, "id"],
-                      pump.name = p.data[p.data$id == pump.select, "street"])
+                      pump.name = p.data[p.data$id == pump.select, "street"],
+                      stringsAsFactors = FALSE)
   } else {
     ds <- vapply(p.data$id, function(i) {
       c(stats::dist(rbind(p.data[p.data$id == i, c("x", "y")], c.data)))
@@ -61,7 +62,8 @@ euclideanDistance <- function(case, pump.select = NULL, vestry = FALSE,
     out <- data.frame(case,
                       distance = ds[which.min(ds)],
                       pump = p.data[which.min(ds), "id"],
-                      pump.name = p.data[which.min(ds), "street"])
+                      pump.name = p.data[which.min(ds), "street"],
+                      stringsAsFactors = FALSE)
   }
 
   if (!is.null(unit)) {
