80892efa605a93842a854116b097ec6a0dbe6f0c
lindbrook <lindbrook@gmail.com>
2017-12-25 09:58:27 -0800

    add unitMeter()

---
+++ R/unitMeter.R
@@ -0,0 +1,26 @@
+#' Convert nominal map distance to yards or meters.
+#'
+#' A best guess estimate.
+#' @param x Numeric.
+#' @param unit Character. Unit of measurement: "meter" or "yard". Default is NULL, which returns the map's native scale. See \code{vignette("roads")} for information on coordinate unit distances.
+
+unitMeter <- function(x, unit = NULL) {
+  if (is.numeric(x) == FALSE) {
+    stop('"x" must be numeric.')
+  }
+
+  if (is.null(unit) == FALSE) {
+    if (unit %in% c("meter", "yard") == FALSE)
+      stop('If specified, "unit" must either be "meter" or "yard".')
+  }
+  
+  if (is.null(unit)) {
+    x
+  } else {
+    if (unit == "yard") {
+      x * 177 / 3
+    } else if (unit == "meter") {
+      x * 54
+    }
+  }
+}
