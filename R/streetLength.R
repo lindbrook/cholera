59cf33e851bbd553504bff8ba71bb8664d9664ad
lindbrook <lindbrook@gmail.com>
2018-01-20 15:03:53 -0800

    compute length for street name or number

--- R/streetLength.R
+++ R/streetLength.R
@@ -1,6 +1,6 @@
 #' Compute length of selected street.
 #'
-#' @param road.name Character. Road name. Tries to correct for case and to remove extra spaces.
+#' @param road Character or Numeric. Road name or number. For names, the function ries to correct for case and to remove extra spaces.
 #' @param unit Character. Unit of measurement: "meter" or "yard". Default is NULL, which returns the map's native scale.
 #' @return  An R vector of length one.
 #' @seealso \code{\link{roads}}, code{\link{road.segments}}, \code{\link{streetNameLocator}}, \code{\link{streetNumberLocator}}, \code{vignette("road.names")}
@@ -10,26 +10,31 @@
 #' streetLength("oxford street")
 #' streetLength("oxford street", unit = "meter")
 
-streetLength <- function(road.name = "Oxford Street", unit = NULL) {
+streetLength <- function(road = "Oxford Street", unit = NULL) {
   if (is.null(unit) == FALSE) {
     if (unit %in% c("meter", "yard") == FALSE)
       stop('If specified, "unit" must either be "meter" or "yard".')
   }
 
-  real.road.names <- unique(cholera::roads$name)
+  if (is.character(road)) {
+    real.road.names <- unique(cholera::roads$name)
+    if (road %in% real.road.names == FALSE) {
+      case.name <- caseAndSpace(road)
+      if (case.name %in% real.road.names == FALSE) {
+        error.msg <- paste("Invalid road name.",
+          'Check spelling or see list of road names in vignette("road.names").')
+        stop(error.msg)
+      } else nm <- case.name
+    } else nm <- road
 
-  if (is.character(road.name) == FALSE) {
-    stop("Road name must be a character string.")
-  } else if (road.name %in% real.road.names == FALSE) {
-    case.name <- caseAndSpace(road.name)
-    if (case.name %in% real.road.names == FALSE) {
-      error.msg <- paste("Invalid road name.",
-        'Check spelling or see list of road names in vignette("road.names").')
-      stop(error.msg)
-    } else nm <- case.name
-  } else nm <- road.name
+    dat <- cholera::road.segments[cholera::road.segments$name == nm, ]
 
-  dat <- cholera::road.segments[cholera::road.segments$name == nm, ]
+  } else if (is.numeric(road)) {
+    if (road %in% unique(cholera::roads$street) == FALSE) {
+      stop("If numeric, road must be 1 >= x <= 528.")
+    }
+    dat <- cholera::road.segments[cholera::road.segments$street == road, ]
+  }
 
   distances <- vapply(dat$id, function(i) {
     stats::dist(rbind(as.matrix(dat[dat$id == i, c("x1", "y1")]),
