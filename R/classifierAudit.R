7dd4273fcbb72591352c979f6ba8929f2ee9618a
lindbrook <lindbrook@gmail.com>
2018-01-21 17:35:59 -0800

    add "observed" argument for simulated cases

--- R/classifierAudit.R
+++ R/classifierAudit.R
@@ -3,12 +3,28 @@
 #' Uses orthogonal projector from an observed case to see if it intersects a segment.
 #' @param case Numeric or Integer. Numeric ID of observed case.
 #' @param segment Character. Segment ID. See cholera::road.segments
+#' @param observed Logical. TRUE observed case; FALSE simulated case (cholera::regular.cases).
 #' @note This is a diagnostics function. It is not a guarantee of correct classification.
 #' @return Logical TRUE or FALSE
 #' @export
 
-classifierAudit <- function(case = 483, segment = "326-2") {
-  obs <- cholera::fatalities[cholera::fatalities$case == case, c("x", "y")]
+classifierAudit <- function(case = 483, segment = "326-2", observed = TRUE) {
+  if (!is.numeric(case)) {
+    stop("case must be numeric.")
+  }
+
+  if (observed) {
+    if (case %in% unique(cholera::fatalities$case) == FALSE) {
+      stop("Observed case must be a whole number between 1 and 578.")
+    }
+    obs <- cholera::fatalities[cholera::fatalities$case == case, c("x", "y")]
+  } else {
+    if (case %in% seq_len(nrow(cholera::regular.cases)) == FALSE) {
+      stop("Simulated case must be a whole number between 1 and 4993.")
+    }
+    obs <- cholera::regular.cases[case, c("x", "y")]
+  }
+
   seg.data <- cholera::road.segments[cholera::road.segments$id == segment,
     c("x1", "y1", "x2", "y2")]
 
@@ -69,7 +85,7 @@ print.classifier_audit <- function(x, ...) {
 #' @examples
 #' plot(classifierAudit(case = 483, segment = "326-2"))
 
-plot.classifier_audit <- function(x, radius = 0.1, ...) {
+plot.classifier_audit <- function(x, radius = 0.5, ...) {
   obs <- x$obs
   segment.slope <- stats::coef(x$ols)[2]
   segment.intercept <- stats::coef(x$ols)[1]
@@ -81,17 +97,22 @@ plot.classifier_audit <- function(x, radius = 0.1, ...) {
 
   y.proj <- segment.slope * x.proj + segment.intercept
 
-  cholera::segmentLocator(x$segment, radius = radius)
-  points(x.proj, y.proj, pch = 0)
+  cholera::segmentLocator(x$segment, zoom = TRUE, radius = radius,
+    subtitle = FALSE)
 
   # Bisection / Intersection test
   distB <- stats::dist(rbind(x$seg.df[1, ], c(x.proj, y.proj))) +
            stats::dist(rbind(x$seg.df[2, ], c(x.proj, y.proj)))
 
   if (x$test) {
-    arrows(obs$x, obs$y, x.proj, y.proj, length = 0.1, col = "green")
+    points(obs, col = "green")
+    points(x.proj, y.proj, pch = 0, col = "green")
+    arrows(obs$x, obs$y, x.proj, y.proj, length = 0.1, col = "green", lwd = 3)
   } else {
-    arrows(obs$x, obs$y, x.proj, y.proj, length = 0.1)
+    points(obs, col = "dodgerblue")
+    points(x.proj, y.proj, pch = 0, col = "dodgerblue")
+    arrows(obs$x, obs$y, x.proj, y.proj, length = 0.1, col = "dodgerblue",
+      lwd = 3)
   }
 
   title(sub = x$test)
