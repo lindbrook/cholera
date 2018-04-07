794ba2dcbc07c5205ea2695ea2f36f224f7ec0f4
lindbrook <lindbrook@gmail.com>
2018-04-07 13:53:09 -0700

    Add "add.cases" and "add.pumps" arguments

--- R/snowMap.R
+++ R/snowMap.R
@@ -4,6 +4,8 @@
 #' @param add.landmarks Logical. Add landmarks.
 #' @param vestry Logical. TRUE uses the 14 pumps from the map in the Vestry Report. FALSE uses the 13 pumps from the original map.
 #' @param stacked Logical. Use stacked fatalities.
+#' @param add.cases Logical. Add observed cases.
+#' @param add.pumps Logical. Add pumps.
 #' @param add.title Logical. Add title.
 #' @param ... Additional plotting parameters.
 #' @return A base R graphics plot.
@@ -11,7 +13,6 @@
 #' \code{\link{addKernelDensity}},
 #' \code{\link{addLandmarks}},
 #' \code{\link{addPlaguePit}},
-# #' \code{\link{addSnow}},
 #' \code{\link{addVoronoi}}.
 #' \code{\link{addWhitehead}}
 #' @export
@@ -20,7 +21,7 @@
 #' snowMap(vestry = TRUE, stacked = FALSE)
 
 snowMap <- function(add.landmarks = FALSE, vestry = FALSE, stacked = TRUE,
-  add.title = TRUE, ...) {
+  add.cases = TRUE, add.pumps = TRUE, add.title = TRUE, ...) {
 
   x.range <- range(cholera::roads$x)
   y.range <- range(cholera::roads$y)
@@ -30,26 +31,31 @@ snowMap <- function(add.landmarks = FALSE, vestry = FALSE, stacked = TRUE,
   border.list <- split(borders[, c("x", "y")], borders$street)
 
   if (stacked) {
-    death <- cholera::fatalities
+    cases <- cholera::fatalities
   } else {
-    death <- cholera::fatalities.address
+    cases <- cholera::fatalities.address
   }
 
-  plot(death[, c("x", "y")], xlim = x.range, ylim = y.range, pch = 15,
-    col = "gray", cex = 0.5, asp = 1)
+  plot(cases[, c("x", "y")], xlim = x.range, ylim = y.range, pch = NA, asp = 1)
   invisible(lapply(roads.list, lines, col = "gray"))
   invisible(lapply(border.list, lines))
 
-  if (vestry) {
-    well <- cholera::pumps.vestry
-    points(well[, c("x", "y")], pch = 2, cex = 1, col = "blue")
-    text(well[, c("x", "y")], label = paste0("p", well$id), pos = 1,
-      col = "blue")
-  } else {
-    well <- cholera::pumps
-    points(well[, c("x", "y")], pch = 2, cex = 1, col = "blue")
-    text(well[, c("x", "y")], label = paste0("p", well$id), pos = 1,
-      col = "blue")
+  if (add.cases) {
+    points(cases[, c("x", "y")], pch = 15, col = "gray", cex = 0.5)
+  }
+
+  if (add.pumps) {
+    if (vestry) {
+      well <- cholera::pumps.vestry
+      points(well[, c("x", "y")], pch = 2, cex = 1, col = "blue")
+      text(well[, c("x", "y")], label = paste0("p", well$id), pos = 1,
+        col = "blue")
+    } else {
+      well <- cholera::pumps
+      points(well[, c("x", "y")], pch = 2, cex = 1, col = "blue")
+      text(well[, c("x", "y")], label = paste0("p", well$id), pos = 1,
+        col = "blue")
+    }
   }
 
   if (add.title) {
