0e45f7f5827aaf3d8d7c32bd145b022ef2fdabd7
lindbrook <lindbrook@gmail.com>
2018-04-11 10:03:43 -0700

    set "meter.unit" with unitMeter()

--- R/distanceTime.R
+++ R/distanceTime.R
@@ -2,12 +2,14 @@
 #'
 #' @param x Numeric. Nominal map distance.
 #' @param unit Character. Unit of measurement: "hour", "minute" or "second".
-#' @param meter.unit Numeric. 1 meter is approximately 54 map units.
+#' @param meter.unit Numeric. 1 meter is approximately 54 map units. Set by unitMeter().
 #' @param speed Numeric. Walking speed: 5 km/hr.
 #' @return An R vector.
 #' @export
 
-distanceTime <- function(x, unit = "second", meter.unit = 54, speed = 5) {
+distanceTime <- function(x, unit = "second",
+  meter.unit = cholera::unitMeter(1, unit = "meter"), speed = 5) {
+
   if (is.numeric(x) == FALSE) {
     stop('"x" must be numeric.')
   }
