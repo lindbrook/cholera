340a67f64c00989953bb858ec88aafefaa726f58
lindbrook <lindbrook@gmail.com>
2018-04-10 19:52:25 -0700

    fix distanceTime()

--- R/distanceTime.R
+++ R/distanceTime.R
@@ -1,24 +1,26 @@
 #' Convert distance to time.
 #'
 #' @param x Numeric. Nominal map distance.
-#' @param unit Character. Unit of measurement: "hour" or "minute".
-#' @param meter.unit Numeric. Estimate of meters per map unit: 54.
-#' @param speed Numeric. Walking speed: 3 meters per hour.
+#' @param unit Character. Unit of measurement: "hour", "minute" or "second".
+#' @param meter.unit Numeric. 1 meter is approximately 54 map units.
+#' @param speed Numeric. Walking speed: 5 km/hr.
 #' @return An R vector.
 #' @export
 
-distanceTime <- function(x, unit = "minute", meter.unit = 54, speed = 3) {
+distanceTime <- function(x, unit = "second", meter.unit = 54, speed = 5) {
   if (is.numeric(x) == FALSE) {
     stop('"x" must be numeric.')
   }
 
-  if (unit %in% c("minute", "hour") == FALSE) {
-    stop('"unit" must be "minute" or "hour"')
+  if (unit %in% c("minute", "hour", "second") == FALSE) {
+    stop('"unit" must be "hour", "minute" or "second".')
   }
 
   if (unit == "hour") {
-    (x * meter.unit / 1000) * (1 / speed)
+    (x * meter.unit) / (1000 * speed)
   } else if (unit == "minute") {
-    60 * (x * meter.unit / 1000) * (1 / speed)
+    60 * (x * meter.unit) / (1000 * speed)
+  } else if (unit == "second") {
+    3600 * (x * meter.unit) / (1000 * speed)
   }
 }
