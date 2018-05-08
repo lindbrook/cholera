26ed01ab2bf8664f8710bd6eeda8947b9af55ed0
lindbrook <lindbrook@gmail.com>
2018-05-07 19:53:30 -0700

    delete multiCore()

--- R/walking.R
+++ R/walking.R
@@ -622,35 +622,6 @@ plot.walking <- function(x, type = "road", ...) {
   title(main = "Pump Neighborhoods: Walking")
 }
 
-# multiCore <- function(x) {
-#   if (is.logical(x)) {
-#     if (x) {
-#       cores <- parallel::detectCores()
-#     } else {
-#       if (is.numeric(x)) {
-#         if (is.integer(x)) {
-#           cores <- x
-#         } else {
-#           cores <- as.integer(x)
-#         }
-#       } else {
-#         cores <- 1L
-#       }
-#     }
-#   } else if (is.numeric(x)) {
-#     obs.cores <- parallel::detectCores()
-#     if (x > obs.cores) {
-#       stop(paste0('For your system, "cores" must be <= ', obs.cores, "."))
-#     }
-#     if (is.integer(x)) {
-#       cores <- x
-#     } else {
-#       cores <- as.integer(x)
-#     }
-#   }
-#   cores
-# }
-
 pearlStringRadius <- function() {
   c(stats::dist(cholera::regular.cases[c(1, 3), ]))
 }
