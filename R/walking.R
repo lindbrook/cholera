bafad9ae57eceadb478cd769a3d1ee6c3895cb8b
lindbrook <lindbrook@gmail.com>
2018-06-22 10:16:18 -0700

    update timings

--- R/walking.R
+++ R/walking.R
@@ -16,7 +16,7 @@
 #'   \item{\code{cores}: number of cores to use for parallel implementation.}
 #'   \item{\code{metric}: incremental metric used to find cut point on split road segments.}
 #' }
-#' @section Note: This function is computationally intensive. On a single core of a 2.3 GHz Intel i7, plotting observed paths to PDF takes about 6 seconds while doing so for expected paths takes about 30 seconds. Using the parallel implementation on 4 physical (8 logical) cores, these times fall to about 4 and 12 seconds. Note that parallelization is currently only available on Linux and Mac, and that although some precautions are taken in R.app on macOS, the developers of the 'parallel' package, which neighborhoodWalking() uses, strongly discourage against using parallelization within a GUI or embedded environment. See vignette("parallel") for details.
+#' @section Note: This function is computationally intensive. On a single core of a 2.3 GHz Intel i7, plotting observed paths to PDF takes about 5 seconds while doing so for expected paths takes about 29 seconds. Using the parallel implementation on 4 physical (8 logical) cores, these times fall to about 4 and 11 seconds. Note that parallelization is currently only available on Linux and Mac, and that although some precautions are taken in R.app on macOS, the developers of the 'parallel' package, which neighborhoodWalking() uses, strongly discourage against using parallelization within a GUI or embedded environment. See vignette("parallel") for details.
 #' @export
 #' @examples
 #' # neighborhoodWalking()
@@ -161,7 +161,7 @@ expectedCount <- function(x) {
 #' @param type Character. "road", "area.points" or "area.polygons". "area" flavors only valid when case.set = "expected".
 #' @param ... Additional plotting parameters.
 #' @return A base R plot.
-#' @section Note: When plotting area graphs with simulated data (i.e., case.set = "exected"), there may be discrepancies between observed cases and expected neighborhoods, particularly at the borders between neighborhoods.
+#' @section Note: When plotting area graphs with simulated data (i.e., case.set = "exected"), there may be discrepancies between observed cases and expected neighborhoods, particularly at the borders between neighborhoods. The "area.points" plot takes about 28 seconds (11 using the parallel implementation). The "area.polygons" plot takes 49 seconds (17 using the parallel implementation).
 #' @export
 #' @examples
 #' # plot(neighborhoodWalking())
