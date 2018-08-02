e5e21f38f1f69a49e84048d5a754444f02c36c37
lindbrook <lindbrook@gmail.com>
2018-08-02 06:11:38 -0700

    amend unitMeter() documentation

--- R/unitMeter.R
+++ R/unitMeter.R
@@ -3,8 +3,8 @@
 #' A best guess estimate.
 #' @param x Numeric. Nominal map distance.
 #' @param unit Character. Unit of distance: "meter", "yard" or "native". "native" returns the map's native scale. See \code{vignette("roads")} for information on conversion.
-#' @param yard.unit Numeric. Estimate of yards per map unit: 177 / 3.
-#' @param meter.unit Numeric. Estimate of meters per map unit: 54.
+#' @param yard.unit Numeric. Estimate of yards per map unit.
+#' @param meter.unit Numeric. Estimate of meters per map unit:.
 #' @export
 
 unitMeter <- function(x, unit = "meter", yard.unit = 177 / 3, meter.unit = 54) {
