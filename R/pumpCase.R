16db1c9f1e6273ac31ef6c51b3550491fbf7a1a3
lindbrook <lindbrook@gmail.com>
2018-08-14 17:56:03 -0700

    clean up pumpCase()

--- R/pumpCase.R
+++ R/pumpCase.R
@@ -1,6 +1,6 @@
 #'  Extract numeric case IDs by pump neighborhood.
 #'
-#' @param obj An object created by \code{neighborhoodEuclidean()}, \code{neighborhoodVoronoi()} or \code{neighborhoodWalking()}.
+#' @param x An object created by \code{neighborhoodEuclidean()}, \code{neighborhoodVoronoi()} or \code{neighborhoodWalking()}.
 #' @seealso \code{\link{neighborhoodVoronoi}}, \code{\link{neighborhoodVoronoi}}, \code{\link{neighborhoodEuclidean}},
 #' @return An R list of numeric ID of cases by pump neighborhoods.
 #' @export
@@ -12,52 +12,40 @@
 #' pumpCase(neighborhoodWalking())
 #' }
 
-pumpCase <- function(obj) UseMethod("pumpCase", obj)
+pumpCase <- function(x) UseMethod("pumpCase", x)
 
-pumpCase.default <- function(obj) NULL
+pumpCase.default <- function(x) NULL
 
 #' @export
-pumpCase.euclidean <- function(obj) {
-  if (class(obj) != "euclidean") {
-    stop('obj\'s class needs to be "euclidean".')
-  }
-
-  pumps <- sort(unique(obj$nearest.pump))
+pumpCase.euclidean <- function(x) {
+  pumps <- sort(unique(x$nearest.pump))
   out <- lapply(pumps, function(p) {
-    obj$anchors[obj$nearest.pump == p]
+    x$anchors[x$nearest.pump == p]
   })
 
   stats::setNames(out, paste0("p", pumps))
 }
 
 #' @export
-pumpCase.voronoi <- function(obj) {
-  if (class(obj) != "voronoi") {
-    stop('obj\'s class needs to be "voronoi".')
-  }
-
-  output <- obj$statistic.data
+pumpCase.voronoi <- function(x) {
+  output <- x$statistic.data
   out <- lapply(output, function(x) {
     cholera::fatalities.address$anchor.case[x == 1]
   })
 
-  if (is.null(obj$pump.select)) {
-    if (obj$vestry == TRUE) {
+  if (is.null(x$pump.select)) {
+    if (x$vestry == TRUE) {
       stats::setNames(out, paste0("p", seq_len(nrow(cholera::pumps.vestry))))
     } else {
       stats::setNames(out, paste0("p", seq_len(nrow(cholera::pumps))))
     }
   } else {
-    stats::setNames(out, paste0("p", obj$pump.id))
+    stats::setNames(out, paste0("p", x$pump.id))
   }
 }
 
 #' @export
-pumpCase.walking <- function(obj) {
-  if (class(obj) != "walking") {
-    stop('obj\'s class needs to be "walking".')
-  }
-
-  output <- obj$cases
+pumpCase.walking <- function(x) {
+  output <- x$cases
   stats::setNames(output, paste0("p", names(output)))
 }
