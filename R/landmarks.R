b081d8ece2e572bd0db877480d649aea4bda4bad
lindbrook <lindbrook@gmail.com>
2018-09-27 09:54:20 -0700

    add landmarks() and ortho.proj.landmarks

--- R/landmarks.R
+++ R/landmarks.R
@@ -1,9 +1,13 @@
-#' Landmark data.
+#' Landmark data (prototype).
 #'
 #' Nominal and orthogonal coordinates
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. On Windows, only \code{multi.core = FALSE} is available.
+#' @export
 
-landmarkData <- function(multi.core = FALSE) {
+# ortho.proj.landmarks <- landmarks()
+# devtools::use_data(ortho.proj.landmarks)
+
+landmarks <- function(multi.core = FALSE) {
   marx <- data.frame(x = 17.3855, y = 13.371)
   snow <- data.frame(x = 10.22414, y = 4.383851)
   st.lukes.church <- data.frame(x = 14.94156, y = 11.25313)
