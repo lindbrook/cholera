d40e599b74e93b2ffdcaeca2a62d90d4f336a78c
lindbrook <lindbrook@gmail.com>
2018-12-02 15:09:01 -0800

    pass line.width argument to addSnow(type = "perimeter")

--- R/addSnow.R
+++ R/addSnow.R
@@ -1,9 +1,9 @@
 #' Adds Snow's graphical annotation of the Broad Street pump walking neighborhood.
 #'
-#' @param type Character. Type of annotation plot: "area", "boundary" or "street".
+#' @param type Character. Type of annotation plot: "area", "perimeter" or "street".
 #' @param color Character. Neighborhood color.
 #' @param alpha.level Numeric. Alpha level transparency: a value in [0, 1].
-#' @param line.width Numeric. Line width for \code{type = "street"} and \code{type = "boundary"}.
+#' @param line.width Numeric. Line width for \code{type = "street"} and \code{type = "perimeter"}.
 #' @param ... Additional plotting parameters.
 #' @seealso \code{\link{snowMap}},
 #' \code{\link{addIndexCase}},
@@ -24,8 +24,8 @@
 addSnow <- function(type = "area", color = "dodgerblue", alpha.level = 0.25,
   line.width = 2, ...) {
 
-  if (type %in% c("area", "boundary", "street") == FALSE) {
-    stop('type must be "area", "boundary" or "street".')
+  if (type %in% c("area", "perimeter", "street") == FALSE) {
+    stop('type must be "area", "perimeter" or "street".')
   }
 
   edges <- cholera::neighborhoodData(case.set = "snow")$edges
@@ -37,14 +37,15 @@ addSnow <- function(type = "area", color = "dodgerblue", alpha.level = 0.25,
       segments(n.edges$x1, n.edges$y1, n.edges$x2, n.edges$y2, lwd = line.width,
         col = color)
     }))
-  } else if (type == "area" | type == "boundary") {
+  } else if (type == "area" | type == "perimeter") {
     snow.area <- cholera::regular.cases[snow$sim.case, ]
     radius <- c(stats::dist(cholera::regular.cases[c(1, 3), ]))
     periphery.cases <- peripheryCases(row.names(snow.area), pearlStringRadius())
     pearl.string <- pearlString(periphery.cases, pearlStringRadius())
 
-    if (type == "boundary") {
-      polygon(cholera::regular.cases[pearl.string, ], border = color, col = NA)
+    if (type == "perimeter") {
+      polygon(cholera::regular.cases[pearl.string, ], border = color, col = NA,
+        lwd = line.width)
     } else if (type == "area") {
       polygon(cholera::regular.cases[pearl.string, ], border = "black",
         col = grDevices::adjustcolor(color, alpha.f = alpha.level))
