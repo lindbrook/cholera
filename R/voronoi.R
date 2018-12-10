0246eca4dbc32db3c00bb10ac802d4c726989f0f
lindbrook <lindbrook@gmail.com>
2018-12-10 14:31:19 -0800

    add warning for statistic argument

--- R/voronoi.R
+++ R/voronoi.R
@@ -3,7 +3,7 @@
 #' Group cases into neighborhoods using Voronoi tessellation.
 #' @param pump.select Numeric. Vector of numeric pump IDs to define pump neighborhoods (i.e., the "population"). Negative selection possible. \code{NULL} selects all pumps.
 #' @param vestry Logical. \code{TRUE} uses the 14 pumps from the Vestry report. \code{FALSE} uses the 13 in the original map.
-#' @param statistic NULL or Character. \code{NULL}, the default, makes no summary computation. "address" computes the number of addresses in each selected pump neighborhood. "fatality" computes the number of fatalities in pump neighborhoods.
+#' @param statistic Character. NULL is default. "address" computes the number of addresses in each selected pump neighborhood. "fatality" computes the number of fatalities in pump neighborhoods.
 #' @param polygon.vertices Logical. \code{TRUE} returns a list of x-y coordinates of the vertices of Voronoi cells. Useful for \code{sp::point.in.polygon()} as used in \code{print.voronoi()} method.
 #' @return An R list with 12 objects.
 #' \itemize{
@@ -108,14 +108,15 @@ neighborhoodVoronoi <- function(pump.select = NULL, vestry = FALSE,
         sp::point.in.polygon(cholera::fatalities.address$x,
           cholera::fatalities.address$y, cell$x, cell$y)
       })
-    }
 
-    if (statistic == "fatality") {
+    } else if (statistic == "fatality") {
       statistic.data <- lapply(coordinates, function(cell) {
         sp::point.in.polygon(cholera::fatalities.unstacked$x,
           cholera::fatalities.unstacked$y, cell$x, cell$y)
       })
-    }
+
+    } else (stop('If specified, statistic must either be "address" or
+                 "fatality".'))
   }
 
   output <- list(pump.id = pump.id, voronoi = voronoi,
