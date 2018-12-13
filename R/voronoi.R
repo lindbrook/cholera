84ccd3f4ded612b16b86bf8b748585677e385da4
lindbrook <lindbrook@gmail.com>
2018-12-12 19:31:53 -0800

    use voronoiPolygons()

--- R/voronoi.R
+++ R/voronoi.R
@@ -80,9 +80,7 @@ neighborhoodVoronoi <- function(pump.select = NULL, vestry = FALSE,
     select.string <- paste(sort(pump.select), collapse = ", ")
   }
 
-  cell.data <- voronoi$dirsgs
   voronoi.order <- as.numeric(rownames(voronoi$summary))
-
   pump.sel <- pump.data$id
 
   if (is.null(pump.select)) {
@@ -95,7 +93,11 @@ neighborhoodVoronoi <- function(pump.select = NULL, vestry = FALSE,
                               area = voronoi$summary$dir.area,
                               pct = voronoi$summary$dir.wts)
 
-  coordinates <- polygonCoordinates(pump.id, cell.data, vestry, x.rng, y.rng)
+  if (is.null(pump.select)) {
+    coordinates <- voronoiPolygons(pump.data, cholera::roads)
+  } else {
+    coordinates <- voronoiPolygons(pump.data[pump.select,], cholera::roads)
+  }
 
   if (is.null(statistic)) {
     statistic.data <- lapply(coordinates, function(cell) {
@@ -274,76 +276,6 @@ plot.voronoi <- function(x, voronoi.cells = TRUE, delauny.triangles = FALSE,
 print.voronoi <- function(x, ...) {
   census <- x$statistic.data
   print(vapply(census, sum, numeric(1L)))
-}
-
-# coordinates of Voronoi cell polygons for sp::point.in.polygon()
-polygonCoordinates <- function(pump.id, cell.data, vestry, x.rng, y.rng) {
-  four.corners <- list(nw = data.frame(x = min(x.rng), y = max(y.rng)),
-                       ne = data.frame(x = max(x.rng), y = max(y.rng)),
-                       se = data.frame(x = max(x.rng), y = min(y.rng)),
-                       sw = data.frame(x = min(x.rng), y = min(y.rng)))
-
-  coordinates <- lapply(pump.id, function(i) {
-    if (vestry) {
-      pump <- cholera::pumps.vestry[cholera::pumps.vestry$id == i, c("x", "y")]
-    } else {
-      pump <- cholera::pumps[cholera::pumps$id == i, c("x", "y")]
-    }
-
-    dat <- cell.data[cell.data$ind1 == which(pump.id == i) |
-                     cell.data$ind2 == which(pump.id == i), ]
-
-    a <- dat[, c("x1", "y1")]
-    b <- dat[, c("x2", "y2")]
-    names(a) <- c("x", "y")
-    names(b) <- c("x", "y")
-
-    coords <- unique(rbind(a, b))
-
-    # "open" polygon tests
-    test1 <- any(dat$thirdv1 < 0 | dat$thirdv2 < 0)
-    test2 <- unlist(dat[, c("thirdv1", "thirdv2")])
-    test2 <- length(unique(test2[test2 < 0])) != 1
-
-    # close "open" polygons at corners of deldir::deldir()'s rectangular window
-    if (test1 & test2) {
-      # test by negation:
-      # does segment from pump to corner intersect any of the polygon's sides?
-      corners <- lapply(seq_len(nrow(dat)), function(j) {
-        intersection.points <- lapply(four.corners, function(corner) {
-          segmentIntersection(pump$x, pump$y, corner$x, corner$y,
-            dat[j, "x1"], dat[j, "y1"], dat[j, "x2"], dat[j, "y2"])
-        })
-
-        vapply(intersection.points, function(x) all(is.na(x)) == FALSE,
-               logical(1L))
-      })
-
-      # If a "corner" returns FALSE, include that corner as a vertex
-      corner.id <- which(colSums(do.call(rbind, corners)) == 0)
-      corner.solution <- four.corners[corner.id]
-
-      if (length(corner.solution) > 1) {
-        coords <- rbind(coords, do.call(rbind, corner.solution))
-      } else {
-        coords <- rbind(coords, unlist(corner.solution))
-      }
-    }
-
-    # center vertices relative to pump's coordinates
-    coords.centered <- data.frame(x = coords$x - pump$x, y = coords$y - pump$y)
-
-    # transform coordinates from cartesian to polar
-    # sort vertices by phi (angle); returns vertices in counter-clockwise order
-    idx <- order(apply(coords.centered, 1, pracma::cart2pol)[1, ])
-    coords <- coords[idx, ]
-
-    # adds first vertex to last to close polygon
-    rbind(coords, coords[1, ])
-  })
-
-  names(coordinates) <- pump.id
-  coordinates
 }
 
 polygonColors <- function(resid.vector, upper.limit = 67, alpha = FALSE) {
