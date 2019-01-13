701e55ef1521bc1f5832fef686fe1d2aee3be6ed
lindbrook <lindbrook@gmail.com>
2019-01-13 02:44:00 -0800

    amend roxygen for beta

--- R/walkingPath.R
+++ R/walkingPath.R
@@ -1,4 +1,4 @@
-#' Compute the shortest walking path between cases and/or pumps.
+#' Compute the shortest walking path between cases and/or pumps. (Beta)
 #'
 #' @param origin Numeric or Character. Numeric ID of case or pump. Character landmark name.
 #' @param destination Numeric or Character. Numeric ID(s) of case(s) or pump(s). Exclusion is possible via negative selection (e.g., -7). Default is \code{NULL}: this returns closest pump or "anchor" case. Character landmark name (case insensitive).
