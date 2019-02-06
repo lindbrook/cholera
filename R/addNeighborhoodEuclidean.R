dcfdf67459bb6569b3795fe83cb61bbde51fe6f7
lindbrook <lindbrook@gmail.com>
2019-02-06 11:10:13 -0800

    remove case.set argument (obs v. exp)

--- R/addNeighborhoodEuclidean.R
+++ R/addNeighborhoodEuclidean.R
@@ -1,10 +1,9 @@
-#' Add Euclidean pump neighborhoods (beta).
+#' Add expected Euclidean pump neighborhoods.
 #'
 #' @param pump.subset Numeric. Vector of numeric pump IDs to subset from the neighborhoods defined by \code{pump.select}. Negative selection possible. \code{NULL} selects all pumps in \code{pump.select}.
 #' @param pump.select Numeric. Vector of numeric pump IDs to define pump neighborhoods (i.e., the "population"). Negative selection possible. \code{NULL} selects all pumps.
 #' @param vestry Logical. \code{TRUE} uses the 14 pumps from the Vestry Report. \code{FALSE} uses the 13 in the original map.
-#' @param case.set Character. "observed" or "expected".
-#' @param case.location Character. For \code{observed = FALSE}: "address" or "nominal". "nominal" is the x-y coordinate of \code{regular.cases}.
+#' @param case.location Character. "address" or "nominal". "address" is the x-y coordinates of \code{sim.ortho.proj}. "nominal" is the x-y coordinates of \code{regular.cases}.
 #' @param polygon.method Character. Method of computing polygon vertices: "pearl.string" or "traveling.salesman".
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. On Windows, only \code{multi.core = FALSE} is available.
 #' @param type Character. Type of plot: "star", "area.points" or "area.polygons".
@@ -21,17 +20,13 @@
 #'
 #' streetNameLocator("marshall street", zoom = TRUE, radius = 0.5,
 #'   highlight = FALSE, add.subtitle = FALSE)
-#' addNeighborhoodEuclidean(case.set = "expected", type = "area.polygons")
+#' addNeighborhoodEuclidean(type = "area.points")
 #' }
 
 addNeighborhoodEuclidean <- function(pump.subset = NULL, pump.select = NULL,
-  vestry = FALSE, case.set = "observed", case.location = "nominal",
+  vestry = FALSE, case.location = "nominal",
   polygon.method = "traveling.salesman", multi.core = FALSE, type = "star",
   alpha.level = 0.25) {
-
-  if (case.set %in% c("observed", "expected") == FALSE) {
-    stop('case.set must be "observed" or "expected".')
-  }
 
   if (case.location %in% c("address", "nominal") == FALSE) {
     stop('case.location must be "address" or "nominal".')
@@ -79,28 +74,20 @@ addNeighborhoodEuclidean <- function(pump.subset = NULL, pump.select = NULL,
     }
   }
 
-  if (case.set == "observed") {
-    anchors <- cholera::fatalities.address$anchor.case
-    observed <- TRUE
-  } else if (case.set == "expected") {
-    anchors <- seq_len(nrow(cholera::regular.cases))
-    observed <- FALSE
-  }
+  anchors <- seq_len(nrow(cholera::regular.cases))
 
   nearest.pump <- parallel::mclapply(anchors, function(x) {
     cholera::euclideanDistance(x, destination = pump.id, vestry = vestry,
-      observed = observed, case.location = case.location)$pump
+      observed = FALSE, case.location = case.location)$data$pump
   }, mc.cores = cores)
 
   if (is.null(pump.subset)) {
     x <- list(pump.data = pump.data,
                 pump.select = pump.select,
                 vestry = vestry,
-                case.set = case.set,
                 pump.id = pump.id,
                 snow.colors = snow.colors,
                 anchors = anchors,
-                observed = observed,
                 nearest.pump = unlist(nearest.pump),
                 cores = cores)
   } else {
@@ -120,22 +107,14 @@ addNeighborhoodEuclidean <- function(pump.subset = NULL, pump.select = NULL,
               pump.subset = pump.subset,
               pump.select = pump.select,
               vestry = vestry,
-              case.set = case.set,
               pump.id = pump.id,
               snow.colors = snow.colors,
               anchors = anchors.subset,
-              observed = observed,
               nearest.pump = unlist(nearest.pump.subset),
               cores = cores)
   }
 
   class(x) <- "euclidean"
-
-  if (type %in% c("area.points", "area.polygons")) {
-    if (x$case.set != "expected") {
-      stop('area plots valid only when case.set = "expected".')
-    }
-  }
 
   anchors <- x$anchors
   nearest.pump <- x$nearest.pump
@@ -144,46 +123,28 @@ addNeighborhoodEuclidean <- function(pump.subset = NULL, pump.select = NULL,
     invisible(lapply(seq_along(anchors), function(i) {
       p.data <- pump.data[pump.data$id == nearest.pump[[i]], ]
       n.color <- x$snow.colors[paste0("p", nearest.pump[[i]])]
-      if (x$observed) {
-        sel <- cholera::fatalities.address$anchor.case %in% anchors[i]
-        n.data <- cholera::fatalities.address[sel, ]
-        lapply(n.data$anchor.case, function(case) {
-          c.data <- n.data[n.data$anchor.case == case, ]
-          segments(c.data$x, c.data$y, p.data$x, p.data$y, col = n.color,
-            lwd = 0.5)
-        })
-      } else {
-        n.data <- cholera::regular.cases[anchors[i], ]
-        lapply(seq_len(nrow(n.data)), function(case) {
-          c.data <- n.data[case, ]
-          segments(c.data$x, c.data$y, p.data$x, p.data$y, col = n.color,
-            lwd = 0.5)
-        })
-      }
+      n.data <- cholera::regular.cases[anchors[i], ]
+
+      lapply(seq_len(nrow(n.data)), function(case) {
+        c.data <- n.data[case, ]
+        segments(c.data$x, c.data$y, p.data$x, p.data$y, col = n.color,
+          lwd = 0.5)
+      })
     }))
 
   } else if (type == "area.points") {
     invisible(lapply(seq_along(anchors), function(i) {
       n.color <- x$snow.colors[paste0("p", nearest.pump[[i]])]
+      n.data <- cholera::regular.cases[anchors[i], ]
 
-      if (x$observed) {
-        sel <- cholera::fatalities.address$anchor.case %in% anchors[i]
-        n.data <- cholera::fatalities.address[sel, ]
-        lapply(n.data$anchor.case, function(case) {
-          c.data <- n.data[n.data$anchor.case == case, ]
-          points(c.data$x, c.data$y, col = n.color, pch = 15, cex = 1.25)
-        })
-      } else {
-        n.data <- cholera::regular.cases[anchors[i], ]
-        lapply(seq_len(nrow(n.data)), function(case) {
-          c.data <- n.data[case, ]
-          points(c.data$x, c.data$y, col = n.color, pch = 15, cex = 1.25)
-        })
-      }
+      lapply(seq_len(nrow(n.data)), function(case) {
+        c.data <- n.data[case, ]
+        points(c.data$x, c.data$y, col = n.color, pch = 15, cex = 1.25)
+      })
     }))
 
   } else if (type == "area.polygons") {
-    p.num <- sort(unique(nearest.pump))
+    p.num <- sort(unique(unlist(nearest.pump)))
 
     neighborhood.cases <- lapply(p.num, function(n) {
       which(nearest.pump == n)
