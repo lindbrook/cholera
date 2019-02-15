0b5d4c24bc2da9a77bc9b063b91690e6adaf54b9
lindbrook <lindbrook@gmail.com>
2019-02-15 10:29:13 -0800

    amend warnings

--- R/multiCore.R
+++ R/multiCore.R
@@ -4,32 +4,25 @@
 #' @param x Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number of logical cores to use.
 #' @noRd
 
-multiCore <- function(x) {
-  if (is.logical(x)) {
-    if (x) {
-      cores <- parallel::detectCores()
-    } else {
-      if (is.numeric(x)) {
-        if (is.integer(x)) {
-          cores <- x
-        } else {
-          cores <- as.integer(x)
-        }
+multiCore <- function(x = TRUE) {
+  if (is.logical(x) | is.numeric(x)) {
+    if (is.logical(x)) {
+      if (x == TRUE) {
+        cores <- parallel::detectCores()
       } else {
         cores <- 1L
       }
+    } else if (is.numeric(x)) {
+      obs.cores <- parallel::detectCores()
+      if (x > obs.cores) {
+        msg <- 'For your system, the number of specified cores must be <= '
+        stop(paste0(msg, obs.cores, "."))
+      } else if (x <= 0) {
+        stop("You must specify at least one core.")
+      } else {
+        cores <- as.integer(x)
+      }
     }
-  } else if (is.numeric(x)) {
-    obs.cores <- parallel::detectCores()
-    if (x > obs.cores) {
-      msg <- 'For your system, the number of specified cores must be <= '
-      stop(paste0(msg, obs.cores, "."))
-    }
-    if (is.integer(x)) {
-      cores <- x
-    } else {
-      cores <- as.integer(x)
-    }
-  }
-  cores
+    cores
+  } else stop("'x' must either be logical or numeric.")
 }
