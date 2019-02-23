96ef1cca12ed7696ee8aa3e1b168641b57903444
lindbrook <lindbrook@gmail.com>
2019-02-23 11:06:19 -0800

    use x.proj & y.proj as output variable names

--- R/orthogonalProjection.R
+++ R/orthogonalProjection.R
@@ -18,26 +18,24 @@ orthogonalProjection <- function(case = 12, segment.id = "216-1") {
   if (is.character(segment.id) == FALSE) {
     stop("segment.id must be character")
   } else {
-    streetID <- road.segments$id
+    streetID <- cholera::road.segments$id
     if (segment.id %in% streetID == FALSE) {
-      stop("See road.segments$id for valid segment.id.")
+      stop("See cholera::road.segments$id for valid segment.id.")
     }
   }
 
   case.data <- cholera::fatalities[cholera::fatalities$case == case, ]
-  segment.data <- road.segments[road.segments$id == segment.id,
-    c("x1", "y1", "x2", "y2")]
-    
+  sel <- cholera::road.segments$id == segment.id
+  segment.data <- cholera::road.segments[sel, c("x1", "y1", "x2", "y2")]
   road.segment <- data.frame(x = c(segment.data$x1, segment.data$x2),
                              y = c(segment.data$y1, segment.data$y2))
 
   ols <- stats::lm(y ~ x, data = road.segment)
   road.intercept <- stats::coef(ols)[1]
   road.slope <- stats::coef(ols)[2]
-  orthogonal.slope <- -1 / road.slope
-  orthogonal.intercept <- case.data$y - orthogonal.slope * case.data$x
-  x.proj <- (orthogonal.intercept - road.intercept) /
-            (road.slope - orthogonal.slope)
+  ortho.slope <- -1 / road.slope
+  ortho.intercept <- case.data$y - ortho.slope * case.data$x
+  x.proj <- (ortho.intercept - road.intercept) / (road.slope - ortho.slope)
   y.proj <- road.slope * x.proj + road.intercept
-  data.frame(x = x.proj, y = y.proj, row.names = NULL)
+  data.frame(x.proj, y.proj, row.names = NULL)
 }
