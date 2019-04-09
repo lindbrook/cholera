5ae3d2e24444b2e5fafe909902d4e8af979d847c
lindbrook <lindbrook@gmail.com>
2019-04-09 08:31:37 -0700

    add isolines() prototype

---
+++ R/isolines.R
@@ -0,0 +1,33 @@
+#' Plot isochrone and isodistance regions (prototype)
+#'
+#' @param pump.select Numeric.
+#' @param post Numeric.
+#' @param post.type Character. "distance or "time".
+#' @param palette Character. RColorBrewer palette.
+#' @export
+
+isolines <- function(pump.select = 7, post = 50, post.type = "distance",
+  palette = "OrRd") {
+
+  pump.dist <- cholera::sim.walking.distance[[paste(pump.select)]]
+  cutpoint <- seq(0, 350, post)
+  mypalette <- RColorBrewer::brewer.pal(length(cutpoint), "OrRd")
+  snowMap(add.cases = FALSE, add.roads = FALSE, add.pumps = FALSE)
+
+  invisible(lapply(seq_along(cutpoint), function(i) {
+    if (post.type == "distance") {
+      sel <- pump.dist$distance > cutpoint[i] &
+             pump.dist$distance <= cutpoint[i] + post
+    } else if (post.type == "time") {
+      sel <- pump.dist$time * 60 > cutpoint[i] &
+             pump.dist$time * 60 <= cutpoint[i] + post
+    }
+
+    neighborhood.cases <- pump.dist[sel, "case"]
+    points(cholera::regular.cases[neighborhood.cases, ], pch = 16,
+      col = mypalette[i], cex = 1)
+  }))
+
+  addRoads()
+  addPump()
+}
