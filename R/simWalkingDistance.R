4748c5ed3eb3257eba2338f3b0ad6cab87506850
lindbrook <lindbrook@gmail.com>
2019-05-01 10:21:37 -0700

    add multi.core argument to simWalkingDistance()

--- R/simWalkingDistance.R
+++ R/simWalkingDistance.R
@@ -1,9 +1,10 @@
 #' Compute walking distance for simulated cases.
 #'
 #' @param pump.select Numeric.
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. On Windows, only \code{multi.core = FALSE} is available.
 #' @export
 #' @note This function documents sim.walking.distance data frame.
 
-simWalkingDistance <- function(pump.select = 7) {
-  nearestPump(pump.select, case.set = "expected")
+simWalkingDistance <- function(pump.select = 7, multi.core = FALSE) {
+  nearestPump(pump.select, case.set = "expected", multi.core = multi.core)
 }
