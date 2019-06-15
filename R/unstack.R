387831e6d2bde821c6c31f955f5727bd25effa1c
lindbrook <lindbrook@gmail.com>
2019-06-15 11:14:55 -0700

    test parLapply() in unstackFatalities()

--- R/unstack.R
+++ R/unstack.R
@@ -21,57 +21,19 @@ unstackFatalities <- function(multi.core = FALSE, compute = FALSE,
 
     case.id <- fatalities$case
 
-    orthogonal.projection <- parallel::mclapply(case.id, function(case) {
-      case.data <- fatalities[fatalities$case == case, c("x", "y")]
-
-      within.radius <- lapply(cholera::road.segments$id, function(id) {
-        seg.data <- cholera::road.segments[cholera::road.segments$id == id, ]
-        test1 <- cholera::withinRadius(case.data, seg.data[, c("x1", "y1")])
-        test2 <- cholera::withinRadius(case.data, seg.data[, c("x2", "y2")])
-        if (any(test1, test2)) unique(seg.data$id)
+    if (.Platform$OS.type == "windows" & cores > 1) {
+      cl <- parallel::makeCluster(cores)
+      parallel::clusterExport(cl = cl, varlist = "fatalities",
+        envir = environment())
+      orthogonal.projection <- parallel::parLapply(cl, case.id, function(case) {
+        orthoProj(case, fatalities)
       })
-
-      within.radius <- unlist(within.radius)
-
-      ortho.proj.test <- lapply(within.radius, function(seg.id) {
-        ortho.data <- orthogonalProjection(case, seg.id)
-        x.proj <- ortho.data$x.proj
-        y.proj <- ortho.data$y.proj
-
-        seg.data <- cholera::road.segments[cholera::road.segments$id == seg.id,
-          c("x1", "y1", "x2", "y2")]
-
-        seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
-                             y = c(seg.data$y1, seg.data$y2))
-
-        # segment bisection/intersection test
-        distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
-                 stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
-
-        bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
-
-        if (bisect.test) {
-          dat <- rbind(c(case.data$x, case.data$y), c(x.proj, y.proj))
-          ortho.dist <- c(stats::dist(dat))
-          ortho.pts <- data.frame(x.proj, y.proj)
-          data.frame(road.segment = seg.id, ortho.pts, ortho.dist,
-            stringsAsFactors = FALSE)
-        } else {
-          null.out <- data.frame(matrix(NA, ncol = 4))
-          names(null.out) <- c("road.segment", "x.proj", "y.proj", "ortho.dist")
-          null.out
-        }
-      })
-
-      out <- do.call(rbind, ortho.proj.test)
-
-      if (all(is.na(out)) == FALSE) {
-        sel <- which.min(out$ortho.dist)
-        out[sel, ]
-      } else {
-        out[1, ] # all candidate roads are NA; arbitrarily choose first obs.
-      }
-    }, mc.cores = cores)
+      parallel::stopCluster(cl)
+    } else {
+      orthogonal.projection <- parallel::mclapply(case.id, function(case) {
+        orthoProj(case, fatalities)
+      }, mc.cores = cores)
+    }
 
     ortho.proj <- do.call(rbind, orthogonal.projection)
     row.names(ortho.proj) <- NULL
@@ -338,5 +300,57 @@ unstackFatalities <- function(multi.core = FALSE, compute = FALSE,
         fatalities.address = cholera::fatalities.address,
         fatalities.unstacked = cholera::fatalities.unstacked,
         ortho.proj = cholera::ortho.proj)
+  }
+}
+
+orthoProj <- function(case, fatalities) {
+  case.data <- fatalities[fatalities$case == case, c("x", "y")]
+
+  within.radius <- lapply(cholera::road.segments$id, function(id) {
+    seg.data <- cholera::road.segments[cholera::road.segments$id == id, ]
+    test1 <- cholera::withinRadius(case.data, seg.data[, c("x1", "y1")])
+    test2 <- cholera::withinRadius(case.data, seg.data[, c("x2", "y2")])
+    if (any(test1, test2)) unique(seg.data$id)
+  })
+
+  within.radius <- unlist(within.radius)
+
+  ortho.proj.test <- lapply(within.radius, function(seg.id) {
+    ortho.data <- cholera::orthogonalProjection(case, seg.id)
+    x.proj <- ortho.data$x.proj
+    y.proj <- ortho.data$y.proj
+
+    seg.data <- cholera::road.segments[cholera::road.segments$id == seg.id,
+      c("x1", "y1", "x2", "y2")]
+
+    seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
+                         y = c(seg.data$y1, seg.data$y2))
+
+    # segment bisection/intersection test
+    distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
+             stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
+
+    bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
+
+    if (bisect.test) {
+      dat <- rbind(c(case.data$x, case.data$y), c(x.proj, y.proj))
+      ortho.dist <- c(stats::dist(dat))
+      ortho.pts <- data.frame(x.proj, y.proj)
+      data.frame(road.segment = seg.id, ortho.pts, ortho.dist,
+        stringsAsFactors = FALSE)
+    } else {
+      null.out <- data.frame(matrix(NA, ncol = 4))
+      names(null.out) <- c("road.segment", "x.proj", "y.proj", "ortho.dist")
+      null.out
+    }
+  })
+
+  out <- do.call(rbind, ortho.proj.test)
+
+  if (all(is.na(out)) == FALSE) {
+    sel <- which.min(out$ortho.dist)
+    out[sel, ]
+  } else {
+    out[1, ] # all candidate roads are NA; arbitrarily choose first obs.
   }
 }
