dd95fdfeeb53cdb9dffb6ccd2132a1a3a88dcf2f
lindbrook <lindbrook@gmail.com>
2019-06-23 10:52:00 -0700

    add parallel Windows prototype for simulateFatalities()

--- R/simulateFatalities.R
+++ R/simulateFatalities.R
@@ -4,12 +4,13 @@
 #' @param compute Logical. \code{TRUE} computes data. \code{FALSE} uses pre-computed data. For replication of data used in the package,
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. With Numeric, you specify the number logical cores (rounds with \code{as.integer()}). On Windows, only \code{multi.core = FALSE} is available.
 #' @param simulated.obs Numeric. Number of sample cases.
+#' @param dev.mode Logical. Development mode uses parallel::parLapply().
 #' @return An R list with two elements: \code{\link{sim.ortho.proj}} and \code{\link{regular.cases}}
 #' @note This function is computationally intensive. With "simulated.obs" set to 20,000 simulated cases (actually generating 19,993 cases), the function takes about 83 minutes to run on a single core of a 2.3 GHz Intel Core i7 with R version 3.6.0 and 19 minutes to run on eight logical (four physical) cores. This function documents the code that generates \code{\link{sim.ortho.proj}} and \code{\link{regular.cases}}. In real world terms, the distance between of these simulated cases is approximately 6 meters.
 #' @export
 
 simulateFatalities <- function(compute = FALSE, multi.core = FALSE,
-  simulated.obs = 20000L) {
+  simulated.obs = 20000L, dev.mode = FALSE) {
 
   if (compute == FALSE) {
     sim.ortho.proj <- cholera::sim.ortho.proj
@@ -39,94 +40,201 @@ simulateFatalities <- function(compute = FALSE, multi.core = FALSE,
     names(regular.cases) <- c("x", "y")
     regular.cases <- split(regular.cases, rownames(regular.cases))
 
-    orthogonal.projection <- parallel::mclapply(regular.cases, function(case) {
-      within.radius <- lapply(cholera::road.segments$id, function(x) {
-        dat <- cholera::road.segments[cholera::road.segments$id == x, ]
-        test1 <- cholera::withinRadius(case, dat[, c("x1", "y1")])
-        test2 <- cholera::withinRadius(case, dat[, c("x2", "y2")])
-        if (any(test1, test2)) unique(dat$id)
-      })
+    if ((.Platform$OS.type == "windows" & cores > 1) | dev.mode) {
+      cl <- parallel::makeCluster(cores)
 
-      within.radius <- unlist(within.radius)
+      parallel::clusterExport(cl = cl, envir = environment(),
+        varlist = "regular.cases")
 
-      ortho.proj.test <- lapply(within.radius, function(seg.id) {
-        ortho.data <- orthogonalProjection(as.numeric(row.names(case)), seg.id,
-          observed = FALSE, case.data = case)
-        x.proj <- ortho.data$x.proj
-        y.proj <- ortho.data$y.proj
+      orthogonal.projection <- parallel::parLapply(cl, regular.cases,
+        function(case) {
 
-        seg.data <- cholera::road.segments[cholera::road.segments$id == seg.id,
-          c("x1", "y1", "x2", "y2")]
+        within.radius <- lapply(cholera::road.segments$id, function(x) {
+          dat <- cholera::road.segments[cholera::road.segments$id == x, ]
+          test1 <- cholera::withinRadius(case, dat[, c("x1", "y1")])
+          test2 <- cholera::withinRadius(case, dat[, c("x2", "y2")])
+          if (any(test1, test2)) unique(dat$id)
+        })
 
-        seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
-                             y = c(seg.data$y1, seg.data$y2))
+        within.radius <- unlist(within.radius)
 
-        ## segment bisection/intersection test ##
+        ortho.proj.test <- lapply(within.radius, function(seg.id) {
+          ortho.data <-
+            cholera::orthogonalProjection(as.numeric(row.names(case)),
+            seg.id, observed = FALSE, case.data = case)
+          x.proj <- ortho.data$x.proj
+          y.proj <- ortho.data$y.proj
 
-        distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
-          stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
+          seg.data <- cholera::road.segments[cholera::road.segments$id ==
+            seg.id, c("x1", "y1", "x2", "y2")]
 
-        bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
+          seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
+                               y = c(seg.data$y1, seg.data$y2))
 
-        if (bisect.test) {
-          ortho.dist <- c(stats::dist(rbind(c(case$x, case$y),
-            c(x.proj, y.proj))))
-          ortho.pts <- data.frame(x.proj, y.proj)
-          data.frame(road.segment = seg.id, ortho.pts, ortho.dist,
-            stringsAsFactors = FALSE)
+          ## segment bisection/intersection test ##
+
+          distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
+            stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
+
+          bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
+
+          if (bisect.test) {
+            ortho.dist <- c(stats::dist(rbind(c(case$x, case$y),
+              c(x.proj, y.proj))))
+            ortho.pts <- data.frame(x.proj, y.proj)
+            data.frame(road.segment = seg.id, ortho.pts, ortho.dist,
+              stringsAsFactors = FALSE)
+          } else {
+            null.out <- data.frame(matrix(NA, ncol = 4))
+            names(null.out) <- c("road.segment", "x.proj", "y.proj",
+              "ortho.dist")
+            null.out
+          }
+        })
+
+        ortho <- do.call(rbind, ortho.proj.test)
+
+        if (all(is.na(ortho$ortho.dist))) {
+          ortho.location <- ortho[1, ]
+          names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
+          ortho.location$type <- NA
         } else {
-          null.out <- data.frame(matrix(NA, ncol = 4))
-          names(null.out) <- c("road.segment", "x.proj", "y.proj", "ortho.dist")
-          null.out
+          ortho.location <- ortho[which.min(ortho$ortho.dist), ]
+          names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
+          ortho.location$type <- "ortho"
         }
+
+        # nearest road segment endpoint
+        candidates <- cholera::road.segments[cholera::road.segments$id %in%
+          within.radius, ]
+        no.bisect1 <- stats::setNames(candidates[, c("x1", "y1")], c("x", "y"))
+        no.bisect2 <- stats::setNames(candidates[, c("x2", "y2")], c("x", "y"))
+        no.bisect <- rbind(no.bisect1, no.bisect2)
+
+        endpt.dist <- vapply(seq_len(nrow(no.bisect)), function(i) {
+          stats::dist(rbind(case, no.bisect[i, ]))
+        }, numeric(1L))
+
+        nearest.endpt <- which.min(endpt.dist)
+
+        if (nearest.endpt <= nrow(ortho)) {
+          c.data <- cbind(candidates[nearest.endpt, c("id", "x1", "y1")],
+            endpt.dist[nearest.endpt])
+        } else {
+          c.data <- cbind(candidates[nearest.endpt - nrow(ortho),
+            c("id", "x2", "y2")], endpt.dist[nearest.endpt])
+        }
+
+        c.data$type <- "eucl"
+        prox.location <- stats::setNames(c.data, names(ortho.location))
+
+        nearest <- which.min(c(ortho.location$dist, prox.location$dist))
+
+        if (nearest == 1) {
+          out <- ortho.location
+        } else if (nearest == 2) {
+          out <- prox.location
+        }
+
+        out
       })
 
-      ortho <- do.call(rbind, ortho.proj.test)
+      parallel::stopCluster(cl)
 
-      if (all(is.na(ortho$ortho.dist))) {
-        ortho.location <- ortho[1, ]
-        names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
-        ortho.location$type <- NA
-      } else {
-        ortho.location <- ortho[which.min(ortho$ortho.dist), ]
-        names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
-        ortho.location$type <- "ortho"
-      }
+    } else {
+      orthogonal.projection <- parallel::mclapply(regular.cases,
+        function(case) {
 
-      # nearest road segment endpoint
-      candidates <- cholera::road.segments[cholera::road.segments$id %in%
-        within.radius, ]
-      no.bisect1 <- stats::setNames(candidates[, c("x1", "y1")], c("x", "y"))
-      no.bisect2 <- stats::setNames(candidates[, c("x2", "y2")], c("x", "y"))
-      no.bisect <- rbind(no.bisect1, no.bisect2)
+        within.radius <- lapply(cholera::road.segments$id, function(x) {
+          dat <- cholera::road.segments[cholera::road.segments$id == x, ]
+          test1 <- cholera::withinRadius(case, dat[, c("x1", "y1")])
+          test2 <- cholera::withinRadius(case, dat[, c("x2", "y2")])
+          if (any(test1, test2)) unique(dat$id)
+        })
 
-      endpt.dist <- vapply(seq_len(nrow(no.bisect)), function(i) {
-        stats::dist(rbind(case, no.bisect[i, ]))
-      }, numeric(1L))
+        within.radius <- unlist(within.radius)
 
-      nearest.endpt <- which.min(endpt.dist)
+        ortho.proj.test <- lapply(within.radius, function(seg.id) {
+          ortho.data <-
+            cholera::orthogonalProjection(as.numeric(row.names(case)),
+            seg.id, observed = FALSE, case.data = case)
+          x.proj <- ortho.data$x.proj
+          y.proj <- ortho.data$y.proj
 
-      if (nearest.endpt <= nrow(ortho)) {
-        c.data <- cbind(candidates[nearest.endpt, c("id", "x1", "y1")],
-          endpt.dist[nearest.endpt])
-      } else {
-        c.data <- cbind(candidates[nearest.endpt - nrow(ortho),
-          c("id", "x2", "y2")], endpt.dist[nearest.endpt])
-      }
+          seg.data <- cholera::road.segments[cholera::road.segments$id ==
+            seg.id, c("x1", "y1", "x2", "y2")]
 
-      c.data$type <- "eucl"
-      prox.location <- stats::setNames(c.data, names(ortho.location))
+          seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
+                               y = c(seg.data$y1, seg.data$y2))
 
-      nearest <- which.min(c(ortho.location$dist, prox.location$dist))
+          ## segment bisection/intersection test ##
 
-      if (nearest == 1) {
-        out <- ortho.location
-      } else if (nearest == 2) {
-        out <- prox.location
-      }
+          distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
+            stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
 
-      out
-    }, mc.cores = cores)
+          bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
+
+          if (bisect.test) {
+            ortho.dist <- c(stats::dist(rbind(c(case$x, case$y),
+              c(x.proj, y.proj))))
+            ortho.pts <- data.frame(x.proj, y.proj)
+            data.frame(road.segment = seg.id, ortho.pts, ortho.dist,
+              stringsAsFactors = FALSE)
+          } else {
+            null.out <- data.frame(matrix(NA, ncol = 4))
+            names(null.out) <- c("road.segment", "x.proj", "y.proj",
+              "ortho.dist")
+            null.out
+          }
+        })
+
+        ortho <- do.call(rbind, ortho.proj.test)
+
+        if (all(is.na(ortho$ortho.dist))) {
+          ortho.location <- ortho[1, ]
+          names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
+          ortho.location$type <- NA
+        } else {
+          ortho.location <- ortho[which.min(ortho$ortho.dist), ]
+          names(ortho.location)[names(ortho.location) == "ortho.dist"] <- "dist"
+          ortho.location$type <- "ortho"
+        }
+
+        # nearest road segment endpoint
+        candidates <- cholera::road.segments[cholera::road.segments$id %in%
+          within.radius, ]
+        no.bisect1 <- stats::setNames(candidates[, c("x1", "y1")], c("x", "y"))
+        no.bisect2 <- stats::setNames(candidates[, c("x2", "y2")], c("x", "y"))
+        no.bisect <- rbind(no.bisect1, no.bisect2)
+
+        endpt.dist <- vapply(seq_len(nrow(no.bisect)), function(i) {
+          stats::dist(rbind(case, no.bisect[i, ]))
+        }, numeric(1L))
+
+        nearest.endpt <- which.min(endpt.dist)
+
+        if (nearest.endpt <= nrow(ortho)) {
+          c.data <- cbind(candidates[nearest.endpt, c("id", "x1", "y1")],
+            endpt.dist[nearest.endpt])
+        } else {
+          c.data <- cbind(candidates[nearest.endpt - nrow(ortho),
+            c("id", "x2", "y2")], endpt.dist[nearest.endpt])
+        }
+
+        c.data$type <- "eucl"
+        prox.location <- stats::setNames(c.data, names(ortho.location))
+
+        nearest <- which.min(c(ortho.location$dist, prox.location$dist))
+
+        if (nearest == 1) {
+          out <- ortho.location
+        } else if (nearest == 2) {
+          out <- prox.location
+        }
+
+        out
+      }, mc.cores = cores)
+    }
 
     sim.ortho.proj <- do.call(rbind, orthogonal.projection)
     sim.ortho.proj$case <- as.numeric(names(orthogonal.projection))
