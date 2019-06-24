6cb12ed340f42c2178cb5c9cb1abb1602cac9ec1
lindbrook <lindbrook@gmail.com>
2019-06-24 11:13:24 -0700

    use splitOutcomes() in neighborhoodWalking()

--- R/walkingAuxiliaryFunctions.R
+++ R/walkingAuxiliaryFunctions.R
@@ -366,7 +366,6 @@ areaPointsData <- function(sim.proj.segs, wholes, snow.colors, sim.proj,
        sim.proj.splits = sim.proj.splits)
 }
 
-
 # for neighborhoodWalking(), expectedCount() and pearsonResiduals.walking()
 observedExpected <- function(x, n.data) {
   if (class(x) != "walking") {
@@ -570,4 +569,84 @@ neighborhoodPathData <- function(x) {
        p.node = p.node,
        p.name = p.name,
        neighborhood.path.edges = neighborhood.path.edges)
+}
+
+expectedCount <- function(x) {
+  OE <- observedExpected(x, neighborhoodPathData(x))
+  wholes <- OE$expected.wholes
+  splits <- OE$exp.splits
+  splits.pump <- OE$exp.splits.pump
+  splits.segs <- OE$exp.splits.segs
+
+  sim.proj <- cholera::sim.ortho.proj
+  sim.proj.segs <- unique(sim.proj$road.segment)
+
+  snow.colors <- snowColors(x$vestry)
+
+  if (OE$obs.split.test > 0 | OE$unobs.split.test > 0) {
+    split.outcome <- splitOutcomes(x, splits.segs, sim.proj, splits,
+      splits.pump)
+    split.outcome <- do.call(rbind, split.outcome)
+    split.outcome <- split.outcome[!is.na(split.outcome$pump), ]
+    split.cases <- lapply(sort(unique(split.outcome$pump)), function(p) {
+      split.outcome[split.outcome$pump == p, "case"]
+    })
+    names(split.cases) <- sort(unique(split.outcome$pump))
+  } else stop("error!")
+
+  ap <- areaPointsData(sim.proj.segs, wholes, snow.colors, sim.proj,
+    split.cases)
+
+  split.count <- table(ap$sim.proj.splits$pump)
+  whole.count <- table(ap$sim.proj.wholes$pump)
+
+  split.count <- data.frame(pump = as.numeric(names(split.count)),
+                            count = unclass(split.count),
+                            stringsAsFactors = FALSE)
+  whole.count <- data.frame(pump = as.numeric(names(whole.count)),
+                            count = unclass(whole.count),
+                            stringsAsFactors = FALSE)
+
+  count.data <- merge(whole.count, split.count, by = "pump", all.x = TRUE)
+  count.data[is.na(count.data)] <- 0
+  stats::setNames(count.data$count.x + count.data$count.y,
+    paste0("p", count.data$pump))
+}
+
+splitOutcomes <- function(x, splits.segs, sim.proj, splits, splits.pump) {
+  split_outcomes <- function(i, splits.segs, sim.proj, splits, splits.pump) {
+    id <- sim.proj$road.segment == splits.segs[i] &
+          is.na(sim.proj$road.segment) == FALSE
+    sim.data <- sim.proj[id, ]
+    split.data <- splits[[i]]
+    sel <- vapply(seq_len(nrow(sim.data)), function(j) {
+      obs <- sim.data[j, c("x.proj", "y.proj")]
+      distance <- vapply(seq_len(nrow(split.data)), function(k) {
+        stats::dist(matrix(c(obs, split.data[k, ]), 2, 2, byrow = TRUE))
+      }, numeric(1L))
+      test1 <- signif(sum(distance[1:2])) ==
+        signif(c(stats::dist(split.data[c(1, 2), ])))
+      test2 <- signif(sum(distance[3:4])) ==
+        signif(c(stats::dist(split.data[c(3, 4), ])))
+      ifelse(any(c(test1, test2)), which(c(test1, test2)), NA)
+    }, integer(1L))
+
+    data.frame(case = sim.data$case, pump = splits.pump[[i]][sel])
+  }
+
+  if ((.Platform$OS.type == "windows" & x$cores > 1) | x$dev.mode) {
+    cl <- parallel::makeCluster(x$cores)
+    parallel::clusterExport(cl = cl, envir = environment(),
+      varlist = c("splits.segs", "sim.proj", "splits", "splits.pump"))
+    output <- parallel::parLapply(cl, seq_along(splits.segs), function(i) {
+      split_outcomes(i, splits.segs, sim.proj, splits, splits.pump)
+    })
+    parallel::stopCluster(cl)
+  } else {
+    output <- parallel::mclapply(seq_along(splits.segs), function(i) {
+      split_outcomes(i, splits.segs, sim.proj, splits, splits.pump)
+    }, mc.cores = x$cores)
+  }
+
+  output
 }
