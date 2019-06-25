0e48db1973666702c8c5ad1da2f0bed736305959
lindbrook <lindbrook@gmail.com>
2019-06-25 09:37:04 -0700

    amend function note

--- R/simWalkingDistance.R
+++ R/simWalkingDistance.R
@@ -3,7 +3,7 @@
 #' @param pump.select Numeric.
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. On Windows, only \code{multi.core = FALSE} is available.
 #' @export
-#' @note This function documents sim.walking.distance data frame.
+#' @note This function documents the code that create the sim.walking.distance data frame.
 
 simWalkingDistance <- function(pump.select = 7, multi.core = FALSE) {
   nearestPump(pump.select, case.set = "expected", multi.core = multi.core)
