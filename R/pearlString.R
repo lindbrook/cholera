73254d778e341217194d0ed36684d3b25a7d23e4
lindbrook <lindbrook@gmail.com>
2019-06-30 15:01:32 -0700

    use non-parallel peripheryCases() and travelingSalesman()

--- R/pearlString.R
+++ R/pearlString.R
@@ -6,44 +6,27 @@ pearlStringRadius <- function() {
   c(stats::dist(cholera::regular.cases[c(1, 2), ]))
 }
 
-peripheryCases <- function(n.points, cores, dev.mode,
-  radius = pearlStringRadius()) {
+peripheryCases <- function(n.points, radius = pearlStringRadius()) {
+  n.area <- cholera::regular.cases[n.points, ]
+  periphery.test <- vapply(seq_len(nrow(n.area)), function(i) {
+    case.point <- n.area[i, ]
 
-  periphery_cases <- function(n.points, radius) {
-    n.area <- cholera::regular.cases[n.points, ]
-    periphery.test <- vapply(seq_len(nrow(n.area)), function(i) {
-      case.point <- n.area[i, ]
+    N <- signif(case.point$x) == signif(n.area$x) &
+         signif(case.point$y + radius) == signif(n.area$y)
 
-      N <- signif(case.point$x) == signif(n.area$x) &
-           signif(case.point$y + radius) == signif(n.area$y)
+    E <- signif(case.point$x + radius) == signif(n.area$x) &
+         signif(case.point$y) == signif(n.area$y)
 
-      E <- signif(case.point$x + radius) == signif(n.area$x) &
-           signif(case.point$y) == signif(n.area$y)
+    S <- signif(case.point$x) == signif(n.area$x) &
+         signif(case.point$y - radius) == signif(n.area$y)
 
-      S <- signif(case.point$x) == signif(n.area$x) &
-           signif(case.point$y - radius) == signif(n.area$y)
+    W <- signif(case.point$x - radius) == signif(n.area$x) &
+         signif(case.point$y) == signif(n.area$y)
 
-      W <- signif(case.point$x - radius) == signif(n.area$x) &
-           signif(case.point$y) == signif(n.area$y)
+    sum(c(N, E, S, W)) == 4
+  }, logical(1L))
 
-      sum(c(N, E, S, W)) == 4
-    }, logical(1L))
-
-    row.names(n.area[which(periphery.test == FALSE), ])
-  }
-
-  if ((.Platform$OS.type == "windows" & cores > 1) | dev.mode) {
-    cl <- parallel::makeCluster(cores)
-    parallel::clusterExport(cl = cl, envir = environment(),
-      varlist = c("n.points", "radius"))
-    p.cases <- parallel::parLapply(cl, n.points, periphery_cases, radius)
-    parallel::stopCluster(cl)
-  } else {
-    p.cases <- parallel::mclapply(n.points, periphery_cases, radius,
-      mc.cores = cores)
-  }
-
-  p.cases
+  row.names(n.area[which(periphery.test == FALSE), ])
 }
 
 #' Compute polygon vertices via 'TSP' package.
@@ -65,30 +48,15 @@ travelingSalesman <- function(vertices, cores, dev.mode,
          "nn", or "repetitive_nn".')
   }
 
-  traveling_salesman <- function(v, tsp.method) {
-    d <- stats::dist(cholera::regular.cases[v, ])
-    distances <- data.frame(t(utils::combn(v, 2)), c(d),
-      stringsAsFactors = FALSE)
-    names(distances) <- c("a", "b", "dist")
-    distances$pathID <- paste0(distances$a, "-", distances$b)
-    distances$rev.pathID <- paste0(distances$b, "-", distances$a)
-    tsp <- TSP::TSP(d, labels = v)
-    soln <- TSP::solve_TSP(tsp, method = tsp.method)
-    names(soln)
-  }
-
-  if ((.Platform$OS.type == "windows" & cores > 1) | dev.mode) {
-    cl <- parallel::makeCluster(cores)
-    parallel::clusterExport(cl = cl, envir = environment(),
-      varlist = "vertices", "cores", "dev.mode")
-    p.string <- parallel::parLapply(cl, vertices, traveling_salesman, tsp.method)
-    parallel::stopCluster(cl)
-  } else {
-    p.string <- parallel::mclapply(vertices, traveling_salesman, tsp.method,
-      mc.cores = cores)
-  }
-
-  p.string
+  d <- stats::dist(cholera::regular.cases[vertices, ])
+  distances <- data.frame(t(utils::combn(vertices, 2)), c(d),
+    stringsAsFactors = FALSE)
+  names(distances) <- c("a", "b", "dist")
+  distances$pathID <- paste0(distances$a, "-", distances$b)
+  distances$rev.pathID <- paste0(distances$b, "-", distances$a)
+  tsp <- TSP::TSP(d, labels = vertices)
+  soln <- TSP::solve_TSP(tsp, method = tsp.method)
+  names(soln)
 }
 
 ## diagnostic plots ##
