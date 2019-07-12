c3a3068c54ba85fe73e6651752b6fc702ca318d1
lindbrook <lindbrook@gmail.com>
2019-07-12 14:36:20 -0700

    add simWalkingDistance(dev.mode)

--- R/simWalkingDistance.R
+++ R/simWalkingDistance.R
@@ -2,9 +2,12 @@
 #'
 #' @param pump.select Numeric.
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores.
+#' @param dev.mode Logical. Development mode uses parallel::parLapply().
 #' @export
 #' @note This function documents the code that create the sim.walking.distance data frame.
 
-simWalkingDistance <- function(pump.select = 7, multi.core = FALSE) {
-  nearestPump(pump.select, case.set = "expected", multi.core = multi.core)
+simWalkingDistance <- function(pump.select = 7, multi.core = FALSE,
+  dev.mode = FALSE) {
+  nearestPump(pump.select, case.set = "expected", multi.core = multi.core,
+    dev.mode = dev.mode)
 }
