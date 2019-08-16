e503cd1ab44193f4697dc91c655adfe842d28867
lindbrook <lindbrook@gmail.com>
2019-08-16 11:45:19 -0700

    amnend comment

--- R/distanceTime.R
+++ R/distanceTime.R
@@ -25,7 +25,7 @@ distanceTime <- function(x, distance.unit = "meter", time.unit = "second",
   if (distance.unit == "native") {
     speed <- walking.speed * unitMeter(1)
   } else if (distance.unit == "yard") {
-    speed <- walking.speed * 1.0936 # yd/hr
+    speed <- walking.speed * 1.0936 # convert to yd/hr
   } else if (distance.unit == "meter") {
     speed <- walking.speed
   }
