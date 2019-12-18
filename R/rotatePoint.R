16676bdacbbba8f0e763681ad43c922c0c7ae04d
lindbrook <lindbrook@gmail.com>
2019-12-18 12:57:51 -0800

    amend function documentation

--- R/rotatePoint.R
+++ R/rotatePoint.R
@@ -1,6 +1,6 @@
 #' Rotate points (prototype).
 #'
-#' @param id Numeric. Point ID.
+#' @param id Numeric. Raod segment endpoint ID.
 #' @param unique.coords Logical. Use unique coordinates.
 #' @export
 
