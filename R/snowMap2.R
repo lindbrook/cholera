7d4273f50b072d053b841a4e7448031ab12d4d17
lindbrook <lindbrook@gmail.com>
2020-01-03 12:29:45 -0800

    use :: in snowMap2() prototype

--- R/snowMap2.R
+++ R/snowMap2.R
@@ -6,11 +6,11 @@
 
 snowMap2 <- function() {
   vars <- c("longitude", "latitude")
-  plot(roads2[, vars], pch = NA)
-  roads.list <- split(roads2[, vars], roads2$street)
+  plot(cholera::roads2[, vars], pch = NA)
+  roads.list <- split(cholera::roads2[, vars], cholera::roads2$street)
   invisible(lapply(roads.list, lines, col = "gray"))
-  points(fatalities2[, vars], pch = 15, cex = 0.5, col = "gray")
-  points(pumps2[, vars], pch = 2, col = "blue")
-  text(pumps2[, vars], pch = 2, col = "blue", labels = paste0("p", pumps2$id),
-    pos = 1)
+  points(cholera::fatalities2[, vars], pch = 15, cex = 0.5, col = "gray")
+  points(cholera::pumps2[, vars], pch = 2, col = "blue")
+  text(cholera::pumps2[, vars], pch = 2, col = "blue",
+    labels = paste0("p", cholera::pumps2$id), pos = 1)
 }
