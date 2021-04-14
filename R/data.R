09dcb6015707e0c8d79d596656aa2b119601a06b
lindbrook <lindbrook@gmail.com>
2021-04-14 10:00:01 -0700

    amend distance to Soho

--- R/data.R
+++ R/data.R
@@ -181,7 +181,7 @@
 
 #' Oxford monthly weather data, January 1853 - November 1860.
 #'
-#' Extract from UK Met Office (https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/oxforddata.txt): Lat 51.761 Lon -1.262, 63 metres amsl. Approximate (65 miles) northwest of Soho.
+#' Extract from UK Met Office (https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/oxforddata.txt): Lat 51.761 Lon -1.262, 63 metres amsl. Approximate 90 km (55 miles) northwest of Soho.
 #'
 #' @format A data frame with 7 variables and 95 observations.
 #'  \describe{
