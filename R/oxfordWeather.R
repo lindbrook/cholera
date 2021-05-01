324b5fb169d9ae0d094460d2d9a0aba4bf1f4c24
lindbrook <lindbrook@gmail.com>
2021-04-30 21:33:36 -0700

    add/use temperaturePlot()

--- R/oxfordWeather.R
+++ R/oxfordWeather.R
@@ -42,33 +42,9 @@ plot.oxfordWeather <- function(x, statistic = "temperature",
 
   outbreak <- as.Date("1854-10-01") - 1
   sept <- as.Date(paste0(unique(x$year), "-09-30"))
-
-  if (statistic == "temperature") {
-    temps <- data.frame(date = rep(x$date, 2),
-                        hi.lo = unlist(x[, c("tmax", "tmin")]))
-    plot(x$date, x$tmax, pch = NA, ylim = range(x[, c("tmax", "tmin")]),
-      xlab = "Year", ylab = "Celsius")
-    title(main = "Monthly High and Low Temperatures, Oxford UK")
-    sel <- x$date %in% sept
-    points(x[!sel, "date"], x[!sel, "tmin"], pch = 0)
-    points(x[!sel, "date"], x[!sel, "tmax"], pch = 16, col = "gray")
-    points(x[sel, "date"], x[sel, "tmin"], pch = 15, col = "red")
-    points(x[sel, "date"], x[sel, "tmax"], pch = 16, col = "red")
-    invisible(lapply(seq_len(nrow(x)), function(i) {
-      segments(x$date[i], x$tmax[i], x$date[i], x$tmin[i], col = "gray")
-    }))
-    invisible(lapply(which(sel), function(i) {
-      segments(x$date[i], x$tmax[i], x$date[i], x$tmin[i], col = "red", lwd = 2)
-    }))
-    abline(v = sept, col = "red", lty = "dotted")
-    abline(h = x[x$date %in% outbreak, "tmax"], col = "red")
-    axis(3, at = outbreak, labels = format(outbreak, "%b %Y"), padj = 0.9,
-      col.ticks = "red", cex.axis = 0.9)
-    axis(3, at = sept[-2], labels = NA, padj = 0.9, col.ticks = "red",
-      cex.axis = 0.75)
-  } else if (statistic == "rain") {
-    rainPlot(x, month)
-  } else stop('statistic must be "temperature" or "rain".', call. = FALSE)
+  if (statistic == "temperature") temperaturePlot(x, month)
+  else if (statistic == "rain") rainPlot(x, month)
+  else stop('statistic must be "temperature" or "rain".', call. = FALSE)
 }
 
 rainPlot <- function(x, month) {
@@ -97,4 +73,44 @@ rainPlot <- function(x, month) {
   axis(4, at = x[mo.sel, "rain"], labels = round(x[mo.sel, "rain"], 1),
     col.axis = mo.col, col = mo.col)
   rug(x[mos, "rain"], side = 4, col = mo.col)
+}
+
+temperaturePlot <- function(x, month) {
+  if (month == "august") {
+    outbreak <- as.Date("1854-09-01") - 1
+    suffix <- "-08-31"
+  } else if (month == "september") {
+    outbreak <- as.Date("1854-10-01") - 1
+    suffix <- "-09-30"
+  } else stop('month must be "august" or "september".', call. = FALSE)
+
+  sel <- x$date %in% as.Date(paste0(unique(x$year), suffix))
+  mo.data <- x[sel, ]
+  ttl <- paste0("Monthly High and Low Temperatures in Oxford UK (",
+    tools::toTitleCase(month), ")")
+
+  plot(mo.data$date, mo.data$tmax, pch = NA, xlab = "Year",
+    ylab = "Celsius", ylim = range(mo.data$tmax, mo.data$tmin), main = ttl)
+  axis(3, at = outbreak, labels = format(outbreak, "%b %Y"), padj = 0.9)
+  invisible(lapply(seq_len(nrow(mo.data )), function(i) {
+    segments(mo.data$date[i], mo.data$tmax[i],
+             mo.data$date[i], mo.data$tmin[i], col = "gray")
+  }))
+  points(mo.data$date, mo.data$tmax, col = "red")
+  points(mo.data$date, mo.data$tmin, col = "blue")
+  points(mo.data[mo.data$date == outbreak, "date"],
+         mo.data[mo.data$date == outbreak, "tmax"], col = "red", pch = 16)
+  points(mo.data[mo.data$date == outbreak, "date"],
+         mo.data[mo.data$date == outbreak, "tmin"], col = "blue", pch = 16)
+  lines(stats::lowess(mo.data$date, mo.data$tmax), col = "red", lty = "dashed",
+    lwd = 1.5)
+  lines(stats::lowess(mo.data$date, mo.data$tmin), col = "blue",
+    lty = "dashed", lwd = 1.5)
+  rug(mo.data$tmax, side = 4, col = "red")
+  rug(mo.data$tmin, side = 4, col = "blue")
+  abline(v = as.Date("1854-09-30"), lty = "dotted")
+  abline(h = mo.data[mo.data$date == outbreak, "tmax"], col = "red",
+    lwd = 2)
+  abline(h = mo.data[mo.data$date == outbreak, "tmin"], col = "blue",
+    lwd = 2)
 }
