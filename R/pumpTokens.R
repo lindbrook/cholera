082815613faa9de99b72d2109f8404e8195de287
lindbrook <lindbrook@gmail.com>
2021-05-06 10:05:02 -0700

    amend pumpTokens()

--- R/pumpTokens.R
+++ R/pumpTokens.R
@@ -2,7 +2,7 @@
 #'
 #' @noRd
 
-pumpTokens <- function(pump.select, vestry, case.set, snow.colors, type) {
+pumpTokens <- function(pump.select, vestry, case.set, snow.colors, type, pths) {
   if (vestry) {
     dat <- cholera::pumps.vestry
   } else {
@@ -11,54 +11,65 @@ pumpTokens <- function(pump.select, vestry, case.set, snow.colors, type) {
 
   if (case.set == "observed") {
     if (is.null(pump.select)) {
-      points(dat[, c("x", "y")], pch = 24, lwd = 1.25, col = snow.colors)
+      points(dat[, c("x", "y")], pch = 24, lwd = 2, col = snow.colors)
       text(dat[, c("x", "y")], pos = 1, cex = 0.9, labels = paste0("p", dat$id))
     } else {
       if (all(pump.select > 0)) {
         sel <- dat$id %in% pump.select
       } else if (all(pump.select < 0)) {
-        sel <- dat$id %in% abs(pump.select) == FALSE
+        p.sel <- !dat$id %in% abs(pump.select)
+        unobs <- dat$id %in% as.numeric(names(pths))
+        sel <- p.sel & unobs
       }
-      points(dat[sel, c("x", "y")], pch = 24, lwd = 1.25,
-        col = snow.colors[sel])
+
+      points(dat[sel, c("x", "y")], pch = 24, lwd = 2, col = snow.colors[sel])
       text(dat[sel, c("x", "y")], pos = 1, cex = 0.9,
         labels = paste0("p", dat$id[sel]))
+      points(dat[!sel, c("x", "y")], pch = 24, lwd = 2, col = "gray")
+      text(dat[!sel, c("x", "y")], pos = 1, cex = 0.9, col = "gray",
+        labels = paste0("p", dat$id[!sel]))
     }
 
   } else if (case.set == "expected") {
-    if (type %in% c("road", "star")) {
+    if (type == "road") {
       if (is.null(pump.select)) {
-        points(dat[, c("x", "y")], pch = 24, lwd = 1.25, bg = snow.colors)
+        points(dat[, c("x", "y")], pch = 24, lwd = 2, bg = snow.colors)
         text(dat[, c("x", "y")], pos = 1, cex = 0.9,
-          labels = paste0("p", dat$id))
+          labels = paste0("p", dat$id), col = snow.colors)
       } else {
         if (all(pump.select > 0)) {
           sel <- dat$id %in% pump.select
         } else if (all(pump.select < 0)) {
           sel <- dat$id %in% abs(pump.select) == FALSE
         }
-        points(dat[sel, c("x", "y")], pch = 24, lwd = 1.25,
+        points(dat[sel, c("x", "y")], pch = 24, lwd = 2,
           bg = snow.colors[sel])
         text(dat[sel, c("x", "y")], pos = 1, cex = 0.9,
-          labels = paste0("p", dat$id[sel]))
+          labels = paste0("p", dat$id[sel]), col = snow.colors[sel])
+        points(dat[!sel, c("x", "y")], pch = 24, lwd = 1, col = "gray")
+        text(dat[!sel, c("x", "y")], pos = 1, cex = 0.9, col = "gray",
+          labels = paste0("p", dat$id[!sel]))
       }
 
     } else if (type %in% c("area.points", "area.polygons")) {
       if (is.null(pump.select)) {
-        points(dat[, c("x", "y")], pch = 24, lwd = 1.25,
+        points(dat[, c("x", "y")], pch = 24, lwd = 2,
           col = "white", bg = snow.colors)
         text(dat[, c("x", "y")], pos = 1, cex = 0.9,
-          labels = paste0("p", dat$id))
+          labels = paste0("p", dat$id), col = "white")
       } else {
         if (all(pump.select > 0)) {
           sel <- dat$id %in% pump.select
         } else if (all(pump.select < 0)) {
           sel <- dat$id %in% abs(pump.select) == FALSE
         }
-        points(dat[sel, c("x", "y")], pch = 24, lwd = 1.25, col = "white",
-          bg = snow.colors[sel])
-        text(dat[sel, c("x", "y")], pos = 1, cex = 0.9,
+        points(dat[sel, c("x", "y")], pch = 24, lwd = 2, col = "white",
+          bg = "white")
+        text(dat[sel, c("x", "y")], pos = 1, cex = 0.9, col = "white",
           labels = paste0("p", dat$id[sel]))
+        points(dat[!sel, c("x", "y")], pch = 24, lwd = 1, col = "white")
+        text(dat[!sel, c("x", "y")], pos = 1, cex = 0.9, col = "white",
+          labels = paste0("p", dat$id[!sel]))
       }
     }
   }
