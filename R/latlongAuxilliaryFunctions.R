a36fcdf874a8dd0cd4e2c21b97c9d8d36505eed2
lindbrook <lindbrook@gmail.com>
2021-06-13 10:46:53 -0700

    add pointIndex() and create latlongAuxilliaryFunctions.R

---
+++ R/latlongAuxilliaryFunctions.R
@@ -0,0 +1,35 @@
+#' Extract points from GeoTiff (prototype).
+#'
+#' @param x Object. GeoTIFF.
+#' @export
+
+pointsFromGeoTIFF <- function(x) {
+  ras <- raster::raster(x)
+  pts <- raster::rasterToPoints(ras)
+  data.frame(pts)
+}
+
+#' Compute rectangle vertices (prototype).
+#'
+#' @param x Object. Points/pixel count.
+#' @export
+
+kmeansRectanlge <- function(x) {
+  if (length(unique(x)) > 1) {
+    km <- stats::kmeans(x, 2)
+    km.df <- data.frame(ct = x, cluster = km$cluster)
+    sel <- km.df[km.df$cluster == which.max(km$centers), ]
+    as.numeric(row.names(sel))
+  } else seq_along(x)
+}
+
+#' Index between native and latlong coordinates (prototype).
+#'
+#' @param max.ct Integer. Upper count of observations.
+#' @export
+
+pointIndex <- function(max.ct = 321) {
+  alpha <- seq(1, max.ct, 50)
+  omega <- c(alpha[-1] - 1, max.ct)
+  data.frame(start = alpha, stop = omega)
+}
