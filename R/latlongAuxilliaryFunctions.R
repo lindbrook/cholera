b3d11b14868ac10c950adc76495a660266b01a07
lindbrook <lindbrook@gmail.com>
2021-06-15 10:19:57 -0700

    add radians() and rotatePoint()

--- R/latlongAuxilliaryFunctions.R
+++ R/latlongAuxilliaryFunctions.R
@@ -51,3 +51,67 @@ referenceRadian <- function(id1 = 66, id2 = 171) {
   y2 <- rd[rd$id == id2, "y"]
   atan((x1 - x2) / (y2 - y1))
 }
+
+#' Compute radians between observed point and centroid of 'roads'.
+#'
+#' @param points.data Object. Data frame of centroid and point.
+#' @export
+
+radians <- function(points.data) {
+  ols <- stats::lm(y ~ x, data = points.data)
+  segment.slope <- stats::coef(ols)[2]
+  atan(segment.slope)
+}
+
+#' Rotate points (prototype).
+#'
+#' @param id Numeric. Road segment endpoint ID.
+#' @param dataset Character. "roads", "fatalities", "fatalities.address", "pumps", or "pumps.vestry".
+#' @param unique.coords Logical. Use unique coordinates.
+#' @export
+
+rotatePoint <- function(id = 1, dataset = "roads", unique.coords = TRUE) {
+  rd <- cholera::roads[cholera::roads$name != "Map Frame", ]
+  rd <- rd[order(rd$x, rd$y), ]
+
+  if (unique.coords) {
+    rd$point.id <- paste0(rd$x, "-", rd$y)
+    rd <- rd[!duplicated(rd$point.id), ]
+  }
+
+  center <- data.frame(x = mean(range(rd$x)), y = mean(range(rd$y)))
+
+  if (dataset == "roads") {
+    points.data <- rbind(center, rd[rd$id == id, c("x", "y")])
+  } else if (dataset == "fatalities") {
+    sel <- cholera::fatalities$case == id
+    points.data <- rbind(center, cholera::fatalities[sel, c("x", "y")])
+  } else if (dataset == "fatalities.address") {
+    sel <- cholera::fatalities.address$anchor == id
+    points.data <- rbind(center, cholera::fatalities.address[sel, c("x", "y")])
+  } else if (dataset == "pumps") {
+    sel <- cholera::pumps$id == id
+    points.data <- rbind(center, cholera::pumps[sel, c("x", "y")])
+  } else if (dataset == "pumps.vestry") {
+    sel <- cholera::pumps.vestry$id == id
+    points.data <- rbind(center, cholera::pumps.vestry[sel, c("x", "y")])
+  } else {
+    msg1 <- 'dataset must be "roads", "fatalities", "fatalities.address",'
+    msg2 <- '"pumps", or "pumps.vestry".'
+    stop(paste(msg1, msg2))
+  }
+
+  theta <- radians(points.data)
+  h <- stats::dist(points.data)
+  theta.delta <- referenceRadian()
+
+  if (points.data$x[1] - points.data$x[2] >= 0) {
+    x.prime <- c(center$x - cos(theta - theta.delta) * h)
+    y.prime <- c(center$y - sin(theta - theta.delta) * h)
+  } else {
+    x.prime <- c(center$x + cos(theta - theta.delta) * h)
+    y.prime <- c(center$y + sin(theta - theta.delta) * h)
+  }
+
+  data.frame(x = x.prime, y = y.prime, row.names = NULL)
+}
