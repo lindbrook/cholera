da4ab57d0219cd4fa3effe40c5a580d6bdf331df
lindbrook <lindbrook@gmail.com>
2021-07-31 17:15:13 -0700

    add latlongOrthoProj() prototype

---
+++ R/latlongOrthoProj.R
@@ -0,0 +1,146 @@
+#' Compute orthogonal projection of fatality addresses (prototype).
+#'
+#' Projection from fatality address to nearest road segment.
+#' @param path Character. e.g., "~/Documents/Data/"
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
+#' @param radius Numeric.
+#' @return An R data frame.
+#' @export
+
+latlongOrthoProj <- function(path, multi.core = TRUE, radius = 0.0005) {
+  cores <- multiCore(multi.core)
+  vars <- c("long", "lat")
+  addr <- latitudeLongitudeAddress(path)
+  rds <- latitudeLongitudeRoads(path)
+
+  rd.segs <- lapply(unique(rds$street), function(i) {
+    st <- rds[rds$street == i, ]
+    names(st)[names(st) %in% vars] <- paste0(vars, 1)
+    seg.end <- st[-1, paste0(vars, 1)]
+    names(seg.end) <- paste0(vars, 2)
+    st <- cbind(st[-nrow(st), c("street", "id", "name")],
+                st[-nrow(st), paste0(vars, 1)],
+                seg.end)
+    st$id <- paste0(st$street, "-", seq_len(nrow(st)))
+    st
+  })
+
+  rd.segs <- do.call(rbind, rd.segs)
+
+  soln <- parallel::mclapply(unique(addr$anchor), function(case) {
+    case.data <- addr[addr$anchor == case, vars]
+
+    within.radius <- lapply(rd.segs$id, function(id) {
+      seg.data <- rd.segs[rd.segs$id == id, ]
+      test1 <- cholera::withinRadius(case.data, seg.data[, paste0(vars, 1)],
+        radius)
+      test2 <- cholera::withinRadius(case.data, seg.data[, paste0(vars, 2)],
+        radius)
+      if (any(test1, test2)) unique(seg.data$id)
+    })
+
+    within.radius <- unlist(within.radius)
+
+    ortho.proj.test <- lapply(within.radius, function(seg.id) {
+      sel <- rd.segs$id == seg.id
+      segment.data <- rd.segs[sel, c(paste0(vars, 1), paste0(vars, 2))]
+      road.segment <- data.frame(x = c(segment.data$long1, segment.data$long2),
+                                 y = c(segment.data$lat1, segment.data$lat2))
+
+      ols <- stats::lm(y ~ x, data = road.segment)
+      road.intercept <- stats::coef(ols)[1]
+      road.slope <- stats::coef(ols)[2]
+      ortho.slope <- -1 / road.slope
+      ortho.intercept <- case.data$lat - ortho.slope * case.data$long
+
+      long.proj <- (ortho.intercept - road.intercept) /
+                   (road.slope - ortho.slope)
+      lat.proj <- road.slope * long.proj + road.intercept
+      seg.df <- data.frame(x = c(segment.data$long1, segment.data$long2),
+                           y = c(segment.data$lat1, segment.data$lat2))
+
+      dist <- stats::dist(rbind(seg.df[1, ], c(long.proj, lat.proj))) +
+              stats::dist(rbind(seg.df[2, ], c(long.proj, lat.proj)))
+
+      bisect.test <- signif(stats::dist(seg.df)) == signif(dist)
+
+      if (bisect.test) {
+        dat <- rbind(c(case.data$long, case.data$lat), c(long.proj, lat.proj))
+        ortho.dist <- c(stats::dist(dat))
+        ortho.pts <- data.frame(long.proj, lat.proj)
+        data.frame(road.segment = seg.id, ortho.pts, ortho.dist, case)
+      } else {
+        null.out <- data.frame(matrix(NA, ncol = 5))
+        names(null.out) <- c("road.segment", "long.proj", "lat.proj",
+          "ortho.dist", "case")
+        null.out
+      }
+    })
+
+    out <- do.call(rbind, ortho.proj.test)
+    out[which.min(out$ortho.dist), ]
+  }, mc.cores = cores)
+
+  soln <- do.call(rbind, soln)
+  row.names(soln) <- NULL
+
+  # second pass: classification error fix
+  latlong.ortho <- soln
+
+  sel <- cholera::ortho.proj$case %in% cholera::fatalities.address$anchor
+  xy.ortho <- cholera::ortho.proj[sel, ]
+
+  unclassified <- setdiff(xy.ortho$case, latlong.ortho$case)
+
+  vars <- c("case", "road.segment")
+  seg.test <- merge(xy.ortho[, vars], latlong.ortho[, vars], by = "case")
+  sel <- seg.test$road.segment.x != seg.test$road.segment.y
+  misclassified <- seg.test[sel, "case"]
+
+  errors.case <- sort(c(misclassified, unclassified))
+  vars <- c("long", "lat")
+
+  correct.latlong <- lapply(errors.case, function(case) {
+    case.data <- addr[addr$anchor == case, vars]
+    correct.seg <- xy.ortho[xy.ortho$case == case, "road.segment"]
+    sel <- rd.segs$id == correct.seg
+    segment.data <- rd.segs[sel, c(paste0(vars, 1), paste0(vars, 2))]
+    road.segment <- data.frame(x = c(segment.data$long1, segment.data$long2),
+                               y = c(segment.data$lat1, segment.data$lat2))
+    ols <- stats::lm(y ~ x, data = road.segment)
+    road.intercept <- stats::coef(ols)[1]
+    road.slope <- stats::coef(ols)[2]
+    ortho.slope <- -1 / road.slope
+    ortho.intercept <- case.data$lat - ortho.slope * case.data$long
+    long.proj <- (ortho.intercept - road.intercept) / (road.slope - ortho.slope)
+    lat.proj <- road.slope * long.proj + road.intercept
+
+    seg.df <- data.frame(x = c(segment.data$long1, segment.data$long2),
+                        y = c(segment.data$lat1, segment.data$lat2))
+
+    dist <- stats::dist(rbind(seg.df[1, ], c(long.proj, lat.proj))) +
+            stats::dist(rbind(seg.df[2, ], c(long.proj, lat.proj)))
+
+    data.frame(road.segment = correct.seg, long.proj, lat.proj,
+      ortho.dist = c(dist), case = case, row.names = NULL)
+  })
+
+  correct.latlong <- do.call(rbind, correct.latlong)
+
+  unclassified.data <- do.call(rbind, lapply(unclassified, function(case) {
+    correct.latlong[correct.latlong$case == case, ]
+  }))
+
+  latlong.ortho <- rbind(latlong.ortho, unclassified.data)
+
+  vars <- c("road.segment", "long.proj", "lat.proj", "ortho.dist")
+
+  for (case in misclassified) {
+    tmp <- correct.latlong[correct.latlong$case == case, vars]
+    latlong.ortho[latlong.ortho$case == case, vars] <- tmp
+  }
+
+  latlong.ortho <- latlong.ortho[order(latlong.ortho$case), ]
+  row.names(latlong.ortho) <- NULL
+  latlong.ortho
+}
