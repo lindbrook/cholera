43b2690f90b2b657eea9edce8a3676c2b0b172a4
lindbrook <lindbrook@gmail.com>
2021-08-11 11:11:43 -0700

    use latlongEmbed() in latlongNeighborhoodData()

--- R/latlongNeighborhoodData.R
+++ R/latlongNeighborhoodData.R
@@ -1,31 +1,19 @@
 #' Compute network graph of roads, cases and pumps.
 #'
 #' Assembles cases, pumps and road into a network graph.
-#' @param path Character. e.g., "~/Documents/Data/".
+#' @param tif Character. Georeferenced QGIS TIFF file.
+#' @param path Character. e.g., "~/Documents/Data/"
+#' @param vestry Logical.
 #' @export
 
-latlongNeighborhoodData <- function(path) {
-  dat <- latitudeLongitudeRoads(path)
+latlongNeighborhoodData <- function(path, tif, vestry = FALSE) {
+  dat <- latlongEmbed(path, tif, vestry = FALSE)
+  dat$node1 <- paste0(dat$long1, "-", dat$lat1)
+  dat$node2 <- paste0(dat$long2, "-", dat$lat2)
+  dat$d <- sqrt((dat$long1 - dat$long2)^2 +
+                    (dat$lat1 - dat$lat2)^2)
 
-  rd.segs <- parallel::mclapply(unique(dat$street), function(i) {
-      st <- dat[dat$street == i, ]
-      names(st)[names(st) %in% c("long", "lat")] <- c("long1", "lat1")
-      seg.end <- st[-1, c("long1", "lat1")]
-      names(seg.end) <- c("long2", "lat2")
-      st <- cbind(st[-nrow(st), c("street", "id", "name")],
-                  st[-nrow(st), c("long1", "lat1")],
-                  seg.end)
-      st$id <- paste0(st$street, "-", seq_len(nrow(st)))
-      st
-    })
-
-  rd.segs <- do.call(rbind, rd.segs)
-  rd.segs$node1 <- paste0(rd.segs$long1, "-", rd.segs$lat1)
-  rd.segs$node2 <- paste0(rd.segs$long2, "-", rd.segs$lat2)
-  rd.segs$d <- sqrt((rd.segs$long1 - rd.segs$long2)^2 +
-                    (rd.segs$lat1 - rd.segs$lat2)^2)
-
-  edge.list <- rd.segs[, c("node1", "node2")]
+  edge.list <- dat[, c("node1", "node2")]
   g <- igraph::graph_from_data_frame(edge.list, directed = FALSE)
   out <- list(edge.list = edge.list, g = g)
   class(out) <- "latlong_neighborhood_data"
