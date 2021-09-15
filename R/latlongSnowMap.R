ad7b5fb1cf95db72249cc373aa1a8f2cd6725695
lindbrook <lindbrook@gmail.com>
2021-09-15 11:36:45 -0700

    add fatalities and pumps to latlongSnowMap()

--- R/latlongSnowMap.R
+++ R/latlongSnowMap.R
@@ -7,8 +7,14 @@
 latlongSnowMap <- function(path, asp = 1.65) {
   rd <- latitudeLongitudeRoads(path)
   frame <- latitudeLongitudeFrame(path)
-  rd <- rbind(rd, frame)
+  fatality <- latitudeLongitudeFatality(path)
+  pump <- latlongPumps(path)
   plot(rd[, c("long", "lat")], pch = NA, asp = asp)
   roads.list <- split(rd[, c("long", "lat")], rd$street)
-  invisible(lapply(roads.list, lines))
+  frame.list <- split(frame[, c("long", "lat")], frame$street)
+  invisible(lapply(roads.list, lines, col = "gray"))
+  invisible(lapply(frame.list, lines))
+  points(fatality[, c("long", "lat")], col = "red", pch = 16, cex = 0.5)
+  points(pump[, c("long", "lat")], col = "blue", pch = 24)
+  text(pump[, c("long", "lat")], col = "blue", pos = 1, labels = pump$id)
 }
