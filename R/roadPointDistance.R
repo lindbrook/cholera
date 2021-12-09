b543596f5a53881d4972a7f6f258ad9755a448d1
lindbrook <lindbrook@gmail.com>
2021-12-09 12:26:42 -0800

    add roadPointDistance()

---
+++ R/roadPointDistance.R
@@ -0,0 +1,12 @@
+#' Compute distance between endpoints of road segments.
+#'
+#' @param a Numeric. Road point ID.
+#' @param b Numeric. Road point ID.
+#' @param meters Logical. Compute metric (meters) or nominal distance.
+#' @export
+
+roadPointDistance <- function(a = 85, b = 86, meters = FALSE) {
+  sel <- cholera::roads$id %in% c(a, b)
+  d <- stats::dist(cholera::roads[sel, c("x", "y")])
+  ifelse(meters, cholera::unitMeter(d), d)
+}
