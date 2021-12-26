8775ec8f2365aa5c3cbd4086cabd0b4ffb3aec95
lindbrook <lindbrook@gmail.com>
2021-12-26 09:35:30 -0800

    use amended latlongEmbed(); don't compute segment distance

--- R/latlongNeighborhoodData.R
+++ R/latlongNeighborhoodData.R
@@ -1,18 +1,14 @@
 #' Compute network graph of roads, cases and pumps.
 #'
 #' Assembles cases, pumps and road into a network graph.
-#' @param tif Character. Georeferenced QGIS TIFF file.
 #' @param path Character. e.g., "~/Documents/Data/"
 #' @param vestry Logical.
 #' @export
 
-latlongNeighborhoodData <- function(path, tif, vestry = FALSE) {
-  dat <- latlongEmbed(path, tif, vestry = FALSE)
-  dat$node1 <- paste0(dat$long1, "-", dat$lat1)
-  dat$node2 <- paste0(dat$long2, "-", dat$lat2)
-  dat$d <- sqrt((dat$long1 - dat$long2)^2 +
-                    (dat$lat1 - dat$lat2)^2)
-
+latlongNeighborhoodData <- function(path, vestry = FALSE) {
+  dat <- latlongEmbed(path, vestry = FALSE)
+  dat$node1 <- paste0(dat$lon1, "-", dat$lat1)
+  dat$node2 <- paste0(dat$lon2, "-", dat$lat2)
   edge.list <- dat[, c("node1", "node2")]
   g <- igraph::graph_from_data_frame(edge.list, directed = FALSE)
   out <- list(edge.list = edge.list, g = g)
