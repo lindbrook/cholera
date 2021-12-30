c7aaaeca782f5330a30f1b51447564e3d634c711
lindbrook <lindbrook@gmail.com>
2021-12-30 09:42:50 -0800

    update latlongSnowMap()

--- R/latlongSnowMap.R
+++ R/latlongSnowMap.R
@@ -5,16 +5,16 @@
 #' @export
 
 latlongSnowMap <- function(path, asp = 1.65) {
-  rd <- latitudeLongitudeRoads(path)
-  frame <- latitudeLongitudeFrame(path)
-  fatality <- latitudeLongitudeFatality(path)
+  rd <- latlongRoads(path)
+  frame <- latlongFrame(path)
+  fatality <- latlongAddress(path)
   pump <- latlongPumps(path)
-  plot(rd[, c("long", "lat")], pch = NA, asp = asp)
-  roads.list <- split(rd[, c("long", "lat")], rd$street)
-  frame.list <- split(frame[, c("long", "lat")], frame$street)
+  plot(rd[, c("lon", "lat")], pch = NA, asp = asp)
+  roads.list <- split(rd[, c("lon", "lat")], rd$street)
+  frame.list <- split(frame[, c("lon", "lat")], frame$street)
   invisible(lapply(roads.list, lines, col = "gray"))
   invisible(lapply(frame.list, lines))
-  points(fatality[, c("long", "lat")], col = "red", pch = 16, cex = 0.5)
-  points(pump[, c("long", "lat")], col = "blue", pch = 24)
-  text(pump[, c("long", "lat")], col = "blue", pos = 1, labels = pump$id)
+  points(fatality[, c("lon", "lat")], col = "red", pch = 16, cex = 0.5)
+  points(pump[, c("lon", "lat")], col = "blue", pch = 24)
+  text(pump[, c("lon", "lat")], col = "blue", pos = 1, labels = pump$id)
 }
