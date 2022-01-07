6a9ee4a4ba3ebe6b9f96f76c541bd4a39c1eb2c1
lindbrook <lindbrook@gmail.com>
2022-01-07 11:01:08 -0800

    compute and output edge distance

--- R/latlongNeighborhoodData.R
+++ R/latlongNeighborhoodData.R
@@ -11,7 +11,23 @@ latlongNeighborhoodData <- function(path, vestry = FALSE) {
   dat$node2 <- paste0(dat$lon2, "-", dat$lat2)
   edge.list <- dat[, c("node1", "node2")]
   g <- igraph::graph_from_data_frame(edge.list, directed = FALSE)
-  out <- list(edge.list = edge.list, g = g)
+
+  edges <- attributes(igraph::E(g))$vname
+  edges <- strsplit(edges, "|", fixed = TRUE)
+  edges <- lapply(edges, function(e) {
+    endpts <- lapply(e, function(x) unlist(strsplit(x, "-")))
+    endpts <- lapply(endpts, function(x) as.numeric(x[2:3]))
+    nms <- paste0(c("lon", "lat"), c(rep(1, 2), rep(2, 2)))
+    out <- stats::setNames(data.frame(t(do.call(c, endpts))), nms)
+    out[, grep("lon", names(out))] <- -1 * out[, grep("lon", names(out))]
+    out$d <- sp::spDistsN1(as.matrix(out[, grep(1, names(out))]),
+                           as.matrix(out[, grep(2, names(out))]),
+                           longlat = TRUE) * 1000L
+    out
+  })
+  edges <- do.call(rbind, edges)
+
+  out <- list(edge.list = edge.list, g = g, edges = edges)
   class(out) <- "latlong_neighborhood_data"
   out
 }
