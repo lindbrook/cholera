2e494b193e011bd656304df86c93752fa83942ed
lindbrook <lindbrook@gmail.com>
2022-01-26 14:44:18 -0800

    add remaining n-tuples in partitionOrthoAddresses()

--- R/partitionOrthoAddresses.R
+++ R/partitionOrthoAddresses.R
@@ -77,27 +77,73 @@ partitionOrthoAddresses <- function(inter.point.dist = 0.15) {
   ## sextads ##
 
   sixes <- names(census[census.ct == 6])
-  plotNtuple(sixes, subgraphs)
+  # plotNtuple(sixes, subgraphs)
+  # ntupleOpenClosed(sixes, subgraphs)
+
+  partitioned.six.double.triangle.one.tail <-
+    sixDoubleTriangleOneTail("12", subgraphs)
+
+  partitioned.six.double.triangle.two.tail <-
+    sixDoubleTriangleTwoTail("26", subgraphs)
 
   ## septad ##
 
   sevens <- names(census[census.ct == 7])
-  plotNtuple(sevens, subgraphs)
+  # plotNtuple(sevens, subgraphs)
+
+  partitioned.seven <- list(v1 = c(393, 552),
+                            v2 = c(507, 553, 348),
+                            v3 = c(38, 1))
+
+  # all(unlist(partitioned.seven) %in%
+  #   as.numeric(names(igraph::V(subgraphs[["1"]]))))
 
   ## octad ##
 
   eights <- names(census[census.ct == 8])
-  plotNtuple(eights, subgraphs)
+  # plotNtuple(eights, subgraphs)
+
+  partitioned.eight <- list(v1 = c(40, 165, 210),
+                            v2 = c(5, 512, 94),
+                            v3 = c(315, 28))
+
+  # all(unlist(partitioned.eight) %in%
+  #   as.numeric(names(igraph::V(subgraphs[["3"]]))))
 
   ## decad ##
 
   tens <- names(census[census.ct == 10])
   plotNtuple(tens, subgraphs)
 
+  partitioned.ten.linked.double.triangles <-
+    list(v1 = c(246, 311, 236, 254),
+         v2 = c(26, 475, 204),
+         v3 = c(29, 266, 223))
+
+  # all(unlist(partitioned.ten.linked.double.triangles) %in%
+  #   as.numeric(names(igraph::V(subgraphs[["8"]]))))
+
+  partitioned.ten.adjacent.double.triangles <-
+    list(v1 = c(45, 449, 481, 224),
+         v2 = c(296, 428, 325),
+         v3 = c(68, 213, 49))
+
+  # all(unlist(partitioned.ten.adjacent.double.triangles) %in%
+  #   as.numeric(names(igraph::V(subgraphs[["17"]]))))
+
+
   ## 19-tuple ##
 
   t19 <- names(census[census.ct == 19])
-  plotNtuple(t19, subgraphs)
+  # plotNtuple(t19, subgraphs)
+
+  partitioned.nineteen <- list(v1 = c(321, 32, 188, 120),
+                               v2 = c(25, 577, 289, 359, 137),
+                               v3 = c(239, 21, 231, 180, 418),
+                               v4 = c(572, 242, 386, 141, 102))
+
+  all(unlist(partitioned.nineteen) %in%
+    as.numeric(names(igraph::V(subgraphs[["7"]]))))
 
 }
 
@@ -225,4 +271,68 @@ fiveDoubleTriangleTail <- function(dat, subgraphs) {
   list(v1 = c(primary.triangle[1], tail),
        v2 = primary.triangle[2],
        v3 = c(primary.triangle[3], pivot))
+}
+
+sixDoubleTriangleOneTail <- function(dat, subgraphs) {
+  tmp <- subgraphs[[dat]]
+  vertices <- names(igraph::V(tmp))
+  e.lst <- igraph::as_edgelist(tmp)
+
+  triangles <- names(igraph::triangles(tmp))
+  tail <- setdiff(vertices, triangles)
+
+  v.degree <- igraph::degree(tmp)
+  tail.end <- names(v.degree[v.degree == 1])
+  tail.bridge <- setdiff(tail, tail.end)
+
+  pivot.test1 <- e.lst[, 1] %in% triangles & e.lst[, 2] == tail.bridge
+  pivot.test2 <- e.lst[, 2] %in% triangles & e.lst[, 2] == tail.bridge
+
+  if (any(pivot.test1)) {
+    pivot <- intersect(e.lst[pivot.test1, ], triangles)
+  } else if (any(pivot.test2)) {
+    pivot <- intersect(e.lst[pivot.test2, ], triangles)
+  }
+
+  endpt <- seq_along(triangles)[seq_along(triangles) %% 3 == 0]
+  startpt <- c(0, endpt[-length(endpt)]) + 1
+  id <- lapply(seq_along(startpt), function(i) seq(startpt[i], endpt[i]))
+  triangle.vs <- lapply(id, function(idx) triangles[idx])
+
+  sel <- which(vapply(triangle.vs, function(x) !pivot %in% x, logical(1L)))
+  primary.triangle <- unlist(triangle.vs[sel])
+
+  list(v1 = c(primary.triangle[1], tail[1]),
+       v2 = c(primary.triangle[2], tail[2]),
+       v3 = c(primary.triangle[3], pivot))
+}
+
+sixDoubleTriangleTwoTail <- function(dat, subgraphs) {
+  tmp <- subgraphs[[dat]]
+  vertices <- names(igraph::V(tmp))
+  e.lst <- igraph::as_edgelist(tmp)
+
+  triangles <- names(igraph::triangles(tmp))
+  tail <- setdiff(vertices, triangles)
+
+  pivot.test1 <- e.lst[, 1] %in% triangles & e.lst[, 2] %in% tail
+  pivot.test2 <- e.lst[, 2] %in% triangles & e.lst[, 1] %in% tail
+  pivot1 <- intersect(e.lst[pivot.test1, ], triangles)
+  pivot2 <- intersect(e.lst[pivot.test2, ], triangles)
+
+  tail1 <- e.lst[e.lst[, 1] %in% pivot1 & e.lst[, 2] %in% tail, 2]
+  tail2 <- e.lst[e.lst[, 2] %in% pivot2 & e.lst[, 1] %in% tail, 1]
+
+  endpt <- seq_along(triangles)[seq_along(triangles) %% 3 == 0]
+  startpt <- c(0, endpt[-length(endpt)]) + 1
+  id <- lapply(seq_along(startpt), function(i) seq(startpt[i], endpt[i]))
+  triangle.vs <- lapply(id, function(idx) triangles[idx])
+  triangle.vs <- stats::setNames(triangle.vs, paste(1:2))
+
+  triangle1 <- triangle.vs$`1`
+  # triangle2 <- triangle.vs$`2`
+
+  list(v1 = c(setdiff(triangle1, pivot1)[1], tail1),
+       v2 = c(setdiff(triangle1, pivot1)[2], tail2),
+       v3 = c(pivot1, pivot2))
 }
