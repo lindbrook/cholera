ce9e6d4a86ca6b4c1c3a7e6b62abb49f884905f7
lindbrook <lindbrook@gmail.com>
2022-02-23 14:51:34 -0800

    fix nominal georeferenced coordinates errors in latlongRoads()

--- R/latlongRoads.R
+++ R/latlongRoads.R
@@ -28,6 +28,50 @@ latlongRoads <- function(path, multi.core = TRUE) {
     alters <- geo.scale
     names(alters)[-1] <- c("x", "y")
 
+    ## translation diagnostic (classification error) ##
+
+    # There should be a one-to-one correspondence between native and
+    # georeferenced points.
+
+    # translation[duplicated(translation$geo.id), ]
+    # #      id geo.id
+    # # 42   99      2
+    # # 201 579    205
+    #
+    # translation[duplicated(translation$id), ]
+    # # [1] id     geo.id
+    # # <0 rows> (or 0-length row.names)
+    #
+    # subset(translation, geo.id == 2)
+    # #    id geo.id
+    # # 2  26      2
+    # # 42 99      2
+    #
+    # subset(translation, geo.id == 205)
+    # #      id geo.id
+    # # 197 566    205
+    # # 201 579    205
+
+    # vars <- c("x", "y")
+    # plot(nom.rotate.scale[, vars], pch = 16, cex = 0.5)
+    # points(alters[, vars], pch = 16, cex = 0.5, col = "red")
+    # points(nom.rotate.scale[nom.rotate.scale$id == 566, vars], col = "green")
+    # points(nom.rotate.scale[nom.rotate.scale$id == 579, vars], col = "green")
+    # points(nom.rotate.scale[nom.rotate.scale$id ==  26, vars], col = "green")
+    # points(nom.rotate.scale[nom.rotate.scale$id ==  99, vars], col = "green")
+
+    # data1 <- rbind(nom.rotate.scale[nom.rotate.scale$id %in% c(566, 579), ],
+    #                alters[alters$id %in% c(205, 211), ])
+    # plot(data1[, vars], pch = NA)
+    # text(data1[1:2, vars], labels = data1$id[1:2])
+    # text(data1[3:4, vars], labels = data1$id[3:4], col = "red")
+
+    # data2 <- rbind(nom.rotate.scale[nom.rotate.scale$id %in% c(26, 99), ],
+    #                alters[alters$id %in% 1:2, ])
+    # plot(data2[, vars], pch = NA)
+    # text(data2[1:2, vars], labels = data2$id[1:2])
+    # text(data2[3:4, vars], labels = data2$id[3:4], col = "red")
+
     translation <- lapply(ids, function(id) {
       ego <- nom.rotate.scale[nom.rotate.scale$id == id, c("x", "y")]
       d <- vapply(seq_len(nrow(alters)), function(i) {
@@ -37,6 +81,11 @@ latlongRoads <- function(path, multi.core = TRUE) {
     })
 
     translation <- do.call(rbind, translation)
+
+    # translation manual fixes
+    translation[translation$id == 26, "geo.id"] <- 1L
+    translation[translation$id == 579, "geo.id"] <- 211L
+
     geo.coords <- merge(geo.coords, translation, by.x = "id", by.y = "geo.id")
     names(geo.coords)[c(1, length(names(geo.coords)))] <- c("geo.id", "id")
     merge(nom.coords, geo.coords[, -1], by = "id")
