42b6bf2f029486b2b4f5781334f3bf30fa431b61
lindbrook <lindbrook@gmail.com>
2022-03-02 09:26:36 -0800

    use latlong*() prototypes in latlongEmbed()

--- R/latlongEmbed.R
+++ R/latlongEmbed.R
@@ -6,36 +6,40 @@
 
 latlongEmbed <- function(path, vestry = FALSE) {
   vars <- c("lon", "lat")
-  road.data <- cholera::latlong.road.segments
-  ortho.addr <- cholera::latlong.ortho.proj
-  if (vestry) ortho.pump <- cholera::latlong.ortho.proj.pump.vestry
-  else ortho.pump <- cholera::latlong.ortho.proj.pump
+  rd <- latlongRoads(path)
+  addr <- latlongAddress(path)
+  pump <- latlongPumps(path)
+  ortho.addr <- latlongOrthoAddress(path)
+  ortho.pump <- latlongOrthoPump(path, vestry = vestry)
 
-  obs.segs <- unique(c(ortho.addr$road.segment, ortho.pump$road.segment))
+  road.segments <- lapply(unique(rd$street), function(i) {
+    dat <- rd[rd$street == i, ]
+    names(dat)[names(dat) %in% vars] <- paste0(vars, 1)
+    seg.data <- dat[-1, paste0(vars, 1)]
+    names(seg.data) <- paste0(vars, 2)
+    dat <- cbind(dat[-nrow(dat), ], seg.data)
+    dat$id <- paste0(dat$street, "-", seq_len(nrow(dat)))
+    dat
+  })
+
+  road.data <- do.call(rbind, road.segments)
+  obs.segs <- unique(c(ortho.addr$id, ortho.pump$id))
+
   no_embeds <- road.data[!road.data$id %in% obs.segs, ]
   no_embeds$distance <- NULL
 
   embeds <- lapply(obs.segs, function(s) {
     rd.tmp <- road.data[road.data$id == s, ]
-    addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
-    pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+    addr.tmp <- ortho.addr[ortho.addr$id == s, ]
+    pump.tmp <- ortho.pump[ortho.pump$id == s, ]
     endpts <- data.frame(lon = unlist(rd.tmp[, paste0(vars[1], 1:2)]),
                          lat = unlist(rd.tmp[, paste0(vars[2], 1:2)]),
                          case = 0,
                          pump = 0,
                          row.names = NULL)
 
-    if (nrow(addr.tmp) > 0) {
-      var.old <- c("lon.proj", "lat.proj", "case")
-      var.new <- c(vars, "case")
-      addr.embed <- stats::setNames(addr.tmp[, var.old], var.new)
-    }
-
-    if (nrow(pump.tmp) > 0) {
-      var.old <- c("lon.proj", "lat.proj", "pump")
-      var.new <- c(vars, "pump")
-      pump.embed <- stats::setNames(pump.tmp[, var.old], var.new)
-    }
+    if (nrow(addr.tmp) > 0) addr.embed <- addr.tmp[, c(vars, "case")]
+    if (nrow(pump.tmp) > 0) pump.embed <- pump.tmp[, c(vars, "pump")]
 
     if (nrow(addr.tmp) > 0 & nrow(pump.tmp) > 0) {
       addr.embed$pump <- 0
@@ -61,6 +65,6 @@ latlongEmbed <- function(path, vestry = FALSE) {
   })
 
   embeds <- do.call(rbind, embeds)
-  out <- rbind(embeds, no_embeds)
+  out <- rbind(embeds, no_embeds[, names(embeds)])
   out[order(out$street), ]
 }
