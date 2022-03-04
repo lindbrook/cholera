3ecebdcd545bb7cfcb4f1ba94c726f32c4f235f5
lindbrook <lindbrook@gmail.com>
2022-03-04 09:22:42 -0800

    use cholera::ortho.proj.pump$road.segment in latlongOrthoPump()

--- R/latlongOrthoPump.R
+++ R/latlongOrthoPump.R
@@ -3,13 +3,10 @@
 #' Projection from fatality address to nearest road segment.
 #' @param path Character. e.g., "~/Documents/Data/"
 #' @param vestry Logical.
-#' @param radius Numeric.
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
 #' @export
 
-latlongOrthoPump <- function(path, vestry = FALSE, radius = 0.001,
-  multi.core = TRUE) {
-
+latlongOrthoPump <- function(path, vestry = FALSE, multi.core = TRUE) {
   cores <- multiCore(multi.core)
   rd <- latlongRoads(path)
   addr <- latlongAddress(path)
@@ -29,7 +26,7 @@ latlongOrthoPump <- function(path, vestry = FALSE, radius = 0.001,
   pump$lon <- std(pump$lon, lon.mean, lon.sd)
   pump$lat <- std(pump$lat, lat.mean, lat.sd)
 
-  road.segments <- lapply(unique(rd$street), function(i) {
+  rd.segs <- lapply(unique(rd$street), function(i) {
     dat <- rd[rd$street == i, ]
     names(dat)[names(dat) %in% vars] <- paste0(vars, 1)
     seg.data <- dat[-1, paste0(vars, 1)]
@@ -39,58 +36,48 @@ latlongOrthoPump <- function(path, vestry = FALSE, radius = 0.001,
     dat
   })
 
-  road.segments <- do.call(rbind, road.segments)
+  rd.segs <- do.call(rbind, rd.segs)
 
-  soln <- parallel::mclapply(pump$id, function(p) {
-    case <- pump[pump$id == p, vars]
-    case.st <-  pump[pump$id == p, "street"]
-    case.id <- road.segments[road.segments$name == case.st, "id"]
+  if (vestry) {
+    ortho.pump <- cholera::ortho.proj.pump.vestry
+  } else {
+    ortho.pump <- cholera::ortho.proj.pump
+  }
 
-    ortho.proj.test <- lapply(case.id, function(x) {
-      seg.data <- road.segments[road.segments$id == x,
-        c("lon1", "lat1", "lon2", "lat2")]
+  orthogonal.projection <- parallel::mclapply(pump$id, function(p) {
+    pump.case <- pump[pump$id == p, vars]
+    pump.seg <- ortho.pump[ortho.pump$pump.id == p, "road.segment"]
 
-      seg.df <- data.frame(lon = c(seg.data$lon1, seg.data$lon2),
-                           lat = c(seg.data$lat1, seg.data$lat2))
+    seg.data <- rd.segs[rd.segs$id == pump.seg,
+      c("lon1", "lat1", "lon2", "lat2")]
 
-      ols <- stats::lm(lat ~ lon, data = seg.df)
-      segment.slope <- stats::coef(ols)[2]
-      segment.intercept <- stats::coef(ols)[1]
+    seg.df <- data.frame(lon = c(seg.data$lon1, seg.data$lon2),
+                         lat = c(seg.data$lat1, seg.data$lat2))
 
-      if (segment.slope == 0) {
-        lon.proj <- case$lon
-        lat.proj <- segment.intercept
-      } else {
-         orthogonal.slope <- -1 / segment.slope
-         orthogonal.intercept <- case$lat - orthogonal.slope * case$lon
-         lon.proj <- (orthogonal.intercept - segment.intercept) /
-                     (segment.slope - orthogonal.slope)
-         lat.proj <- segment.slope * lon.proj + segment.intercept
-      }
+    ols <- stats::lm(lat ~ lon, data = seg.df)
+    segment.slope <- stats::coef(ols)[2]
+    segment.intercept <- stats::coef(ols)[1]
 
-      # segment bisection/intersection test
-      distB <- stats::dist(rbind(seg.df[1, ], c(lon.proj, lat.proj))) +
-               stats::dist(rbind(seg.df[2, ], c(lon.proj, lat.proj)))
+    if (segment.slope == 0) {
+      lon.proj <- pump.case$lon
+      lat.proj <- segment.intercept
+    } else {
+       orthogonal.slope <- -1 / segment.slope
+       orthogonal.intercept <- pump.case$lat - orthogonal.slope * pump.case$lon
+       lon.proj <- (orthogonal.intercept - segment.intercept) /
+                   (segment.slope - orthogonal.slope)
+       lat.proj <- segment.slope * lon.proj + segment.intercept
+    }
 
-      bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
-
-      if (bisect.test) {
-        ortho.dist <- c(stats::dist(rbind(c(case$lon, case$lat),
-                                          c(lon.proj, lat.proj))))
-        ortho.pts <- data.frame(lon.proj, lat.proj)
-        data.frame(id = x, ortho.pts, ortho.dist, stringsAsFactors = FALSE)
-      } else NA
-    })
-
-    out <- do.call(rbind, ortho.proj.test)
-    out[which.min(out$ortho.dist), ]
+    pump.data <- rbind(c(pump.case$lon, pump.case$lat), c(lon.proj, lat.proj))
+    ortho.dist <- c(stats::dist(pump.data))
+    data.frame(lon.proj, lat.proj, ortho.dist, stringsAsFactors = FALSE)
   }, mc.cores = cores)
 
-  soln <- do.call(rbind, soln)
+  soln <- do.call(rbind, orthogonal.projection)
   row.names(soln) <- NULL
-
   soln$lon <- unstd(soln$lon, lon.mean, lon.sd)
   soln$lat <- unstd(soln$lat, lat.mean, lat.sd)
-  soln <- data.frame(pump = pump$id, soln)
+  soln <- data.frame(pump = pump$id, seg = ortho.pump$road.segment, soln)
   soln
 }
