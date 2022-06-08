134bc7a7c3767b715ea95f9c6ba04b69fac08a5e
lindbrook <lindbrook@gmail.com>
2022-06-08 07:37:43 -0700

    note deldir code

--- R/addVoronoi.R
+++ R/addVoronoi.R
@@ -56,4 +56,5 @@ addVoronoi <- function(pump.select = NULL, vestry = FALSE,
 
   plot(dat, add = TRUE, wline = "tess", showpoints = FALSE, cmpnt_col = color,
     cmpnt_lty = line.type, lwd = line.width)
+    # cmpnt_col = c(tri = color) wants number?
 }
