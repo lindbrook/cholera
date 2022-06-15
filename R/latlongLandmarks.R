e79d2932986a13a1db51805b63737602b5dc2b3a
lindbrook <lindbrook@gmail.com>
2022-06-15 13:29:14 -0700

    amend latlongLandmarks() prototype for OSM coordinates

--- R/latlongLandmarks.R
+++ R/latlongLandmarks.R
@@ -1,38 +1,45 @@
 #' Compute Georeferenced Latitude and Longitude (prototype).
 #'
 #' @param path Character. e.g., "~/Documents/Data/"
+#' @param orthogonal Logical. Use orthogonal projection coordinates.
 #' @export
+#' @note This documents the computation of the latlong version of the landmarks data frame.
 
-latlongLandmarks <- function(path) {
-  tif <- "landmarks_modified.tif"
-  dat <- cholera::landmarks
+latlongLandmarks <- function(path, orthogonal = FALSE) {
+  vars <- !names(cholera::pumps.vestry) %in% c("lon", "lat")
+  dat <- cholera::landmarks[, vars]
+  if (orthogonal) {
+    tif <- "ortho.landmark_modified.tif"
+  } else {
+    tif <- "landmark_modified.tif"
+  }
+
   k <- nrow(dat)
   coords <- latlongCoordinates(paste0(path, tif), k, path)
   coords.scale <- data.frame(id = coords$id, scale(coords[, c("lon", "lat")]))
 
   tmp <- lapply(dat$case, function(id) rotatePoint(id, dataset = "landmarks"))
   tmp <- do.call(rbind, tmp)
-  landmark.rotate.scale <- data.frame(id = dat$case, scale(tmp))
+  rotate.scale <- data.frame(id = dat$case, scale(tmp))
 
   alters <- coords.scale
   names(alters)[-1] <- c("x", "y")
 
-  match.points <- lapply(landmark.rotate.scale$id, function(id) {
-    ego <- landmark.rotate.scale[landmark.rotate.scale$id == id, c("x", "y")]
+  match.points <- lapply(rotate.scale$id, function(id) {
+    ego <- rotate.scale[rotate.scale$id == id, c("x", "y")]
     d <- vapply(seq_len(nrow(alters)), function(i) {
       stats::dist(rbind(ego, alters[i, c("x", "y")]))
     }, numeric(1L))
-    data.frame(landmark.id = id, geo.id = alters$id[which.min(d)])
+    data.frame(id = id, geo.id = alters$id[which.min(d)])
   })
 
   match.points <- do.call(rbind, match.points)
-  out <- merge(dat, match.points, by.x = "case", by.y = "landmark.id")
+  out <- merge(dat, match.points, by.x = "case", by.y = "id")
   out <- merge(out, coords, by.x = "geo.id", by.y = "id")
   out <- out[order(out$case), ]
   out$geo.id <- NULL
   row.names(out) <- NULL
-  sel <- !names(out) %in% c("x.proj", "y.proj",  "ortho.dist")
-  out[, sel]
+  out
 }
 
 # usethis::use_data(landmarks, overwrite = TRUE)
