5ed5555282a25ba8b266897bcc7907f9f07ccecb
lindbrook <lindbrook@gmail.com>
2022-09-22 10:17:08 -0700

    add geodesicMeters()

---
+++ R/geodesicMeters.R
@@ -0,0 +1,24 @@
+#' Decompose geodesic distances into horizontal and vertical components.
+#'
+#' Compute geodesic distance from origin to pump and decompose result into
+#` horizontal (East-West) and vertical (North-South) components.
+#' @param dat Object. Data.
+#' @param case.address Logical. Use fatalities.address$anchor
+#' @export
+
+geodesicMeters <- function(dat = cholera::pumps, case.address = FALSE) {
+  origin <- data.frame(lon = min(cholera::roads$lon),
+                       lat = min(cholera::roads$lat))
+  if (case.address) {
+    dat <- cholera::fatalities.address
+    names(dat)[names(dat) == "anchor"] <- "id"
+  }
+  do.call(rbind, lapply(dat$id, function(x) {
+    tmp <- dat[dat$id == x, c("lon", "lat")]
+    x.proj <- c(tmp$lon, origin$lat)
+    y.proj <- c(origin$lon, tmp$lat)
+    m.lon <- geosphere::distGeo(y.proj, tmp)
+    m.lat <- geosphere::distGeo(x.proj, tmp)
+    data.frame(id = x, x = m.lon, y = m.lat)
+  }))
+}
