33d8923dfe99e93c22dbeb5947394603701bdeb4
lindbrook <lindbrook@gmail.com>
2022-12-29 09:45:03 -0800

    add/set orientationPlot(pump.id = 6, pump.select = NULL, cases = "orthogonal, convex.hull = TRUE)

--- R/orientationPlot.R
+++ R/orientationPlot.R
@@ -1,67 +1,81 @@
 #' Orientation plot.
 #'
-#' @param pumpID Numeric. Selected pump ID.
-#' @param cases Character. "address" or "ortho".
+#' @param pump.id Numeric. Select pump focus.
+#' @param pump.select Numeric. Pump neighborhoods.
+#' @param cases Character. "address" or "orthogonal".
+#' @param convex.hull Logical. Add convex hull.
 #' @param metric Character. "eucldidean" or "walking".
 #' @param pca.line Logical. Add PCA (TSS) line through data.
 #' @param vestry Logical. \code{TRUE} uses the 14 pumps from the Vestry Report. \code{FALSE} uses the 13 in the original map.
 #' @export
 
-orientationPlot <- function(pumpID = 6, cases = "ortho", metric = "euclidean",
-  pca.line = FALSE, vestry = FALSE) {
+orientationPlot <- function(pump.id = 6, pump.select = NULL,
+    cases = "orthogonal", convex.hull = TRUE, metric = "euclidean",
+    pca.line = FALSE, vestry = FALSE) {
 
-  if (!vestry & pumpID %in% cholera::pumps$id == FALSE) {
-    stop('For original pumps, pumpID must be a whole number between 1 and 13.',
-      call. = FALSE)
+  if (!cases %in% c("address", "orthogonal")) {
+    stop('cases must be "address" or "orthogonal".', call. = FALSE)
   }
-
-  if (vestry & pumpID %in% cholera::pumps.vestry$id == FALSE) {
-    stop('For vestry pumps, pumpID must lie be a whole number 1 and 14.',
-      call. = FALSE)
-  }
-
-  if (!cases %in% c("address", "ortho")) {
-    stop('cases must be "address" or "ortho".', call. = FALSE)
-  }
-
-  if (metric == "euclidean") fn <- neighborhoodVoronoi(vestry = vestry)
-  else if (metric == "walking") fn <- neighborhoodWalking(vestry = vestry)
-
-  vars <- c("x", "y")
-  p7 <- cholera::pumps[cholera::pumps$id == 7, vars]
-  pmp <- cholera::pumps[cholera::pumps$id == pumpID, vars]
-  p.case <- pumpCase(fn)[[paste0("p", pumpID)]]
 
   if (vestry) {
-    pmp <- cholera::pumps.vestry[cholera::pumps.vestry$id == pumpID, vars]
+    pump.data <- cholera::pumps.vestry
   } else {
-    pmp <- cholera::pumps[cholera::pumps$id == pumpID, vars]
+    pump.data <- cholera::pumps
   }
 
-  snowMap(add.cases = FALSE, vestry = vestry)
-  title(main = paste0("p", pumpID))
+  if (!is.null(pump.select)) {
+    pump.select <- selectPump(pump.data, pump.select = pump.select,
+      metric = metric, vestry = vestry)
+  }
+
+
+  if (metric == "euclidean") {
+    fn <- neighborhoodVoronoi(pump.select = pump.select, vestry = vestry)
+  } else if (metric == "walking") {
+    fn <- neighborhoodWalking(pump.select = pump.select, vestry = vestry)
+  }
+
+  vars <- c("x", "y")
+  p7 <- pump.data[pump.data$id == 7, vars]
+
+  pmp <- pump.data[pump.data$id == pump.id, vars]
+  p.cases <- pumpCase(fn)[[paste0("p", pump.id)]]
+
+  snowMap(add.cases = FALSE, add.pumps = FALSE, vestry = vestry)
+  addPump(pump.select = pump.select)
+
+  if (!is.null(pump.select)) {
+    addPump(setdiff(pump.data$id, pump.select), col = "gray")
+  }
+
+  title(main = paste0("p", pump.id))
   segments(p7$x, p7$y, pmp$x, pmp$y, col = "blue", lwd = 1.25)
 
-  if (metric == "euclidean") addVoronoi(vestry = vestry)
-  else if (metric == "walking") addNeighborhoodWalking(vestry = vestry)
+  if (metric == "euclidean") {
+    addVoronoi(pump.select = pump.select, vestry = vestry)
+  } else if (metric == "walking") {
+    addNeighborhoodWalking(pump.select = pump.select, vestry = vestry)
+  }
 
-  no.obs.cases <- all(!cholera::fatalities$case %in% p.case)
+  no.obs.cases <- all(!cholera::fatalities$case %in% p.cases)
 
   if (no.obs.cases) {
     title(sub = "No observed cases.")
   } else {
-    if (cases == "ortho") {
+    if (cases == "orthogonal") {
       vars2 <- c("x.proj", "y.proj")
-      p.data <- cholera::ortho.proj[cholera::ortho.proj$case %in% p.case, vars2]
+      p.data <- cholera::ortho.proj[cholera::ortho.proj$case %in% p.cases, vars2]
     } else if (cases == "address") {
-      p.data <- cholera::fatalities[cholera::fatalities$case %in% p.case, vars]
+      p.data <- cholera::fatalities[cholera::fatalities$case %in% p.cases, vars]
     }
-    
+
     points(p.data, pch = 16, col = "red", cex = 0.75)
-    
-    convex <- grDevices::chull(p.data)
-    polygon(p.data[convex, ], border = "red")
-    
+
+    if (convex.hull) {
+      convex <- grDevices::chull(p.data)
+      polygon(p.data[convex, ], border = "red")
+    }
+
     center <- data.frame(t(colMeans(p.data)))
     points(center, pch = "x", cex = 1.75, col = "forestgreen")
 
