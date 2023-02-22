8572dfc45ef0369b28bdb33878d8ad39b0f1a1b9
lindbrook <lindbrook@gmail.com>
2023-02-22 10:08:31 -0800

    add/set pumpFatalities(multi.core = TRUE)

--- R/pumpFatalities.R
+++ R/pumpFatalities.R
@@ -4,6 +4,7 @@
 #' @param metric Character. "eucldidean" or "walking".
 #' @param vestry Logical. \code{TRUE} uses the 14 pumps from the Vestry Report. \code{FALSE} uses the 13 in the original map.
 #' @param latlong Logical. Use estimated longitude and latitude.
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
 #' @export
 #' @examples
 #' \dontrun{
@@ -13,7 +14,7 @@
 #' }
 
 pumpFatalities <- function(pump.select = NULL, metric = "walking",
-  vestry = FALSE, latlong = FALSE) {
+  vestry = FALSE, latlong = FALSE, multi.core = TRUE) {
 
   if (!metric %in% c("euclidean", "walking")) {
     stop('metric must be "euclidean" or "walking".', call. = FALSE)
