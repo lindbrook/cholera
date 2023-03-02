f032063da1390fdb2c6f01488fb215c05be3ed78
lindbrook <lindbrook@gmail.com>
2023-03-02 14:37:12 -0800

    add a point only for St Luke's Church in addLandmarks(latlong = FALSE)

--- R/addLandmarks.R
+++ R/addLandmarks.R
@@ -52,12 +52,12 @@ addLandmarks <- function(text.size = 0.5, text.col = "black",
     # 28 Dean Street
     marx <- data.frame(x = 17.3855, y = 13.371)
     text(marx$x, marx$y, labels = "Karl\nMarx", cex = text.size)
-    points(marx$x, marx$y, pch = 15, cex = 1/3)
+    # points(marx$x, marx$y, pch = 15, cex = 1/3)
 
     # 18 Sackville Street
     snow <- data.frame(x = 10.22414, y = 4.383851)
     text(snow$x, snow$y, labels = "John\nSnow", cex = text.size)
-    points(snow$x, snow$y, pch = 15, cex = 1/3)
+    # points(snow$x, snow$y, pch = 15, cex = 1/3)
 
     # St. Luke's Church; Henry Whitehead Assistant Curate
     st.luke <- data.frame(x = 14.94156, y = 11.25313)
@@ -94,7 +94,7 @@ addLandmarks <- function(text.size = 0.5, text.col = "black",
     # Lion Brewery (Huggins' proprietors)
     brewery <- data.frame(x = 13.9022, y = 11.87315)
     text(brewery$x, brewery$y, labels = "Lion\nBrewery", cex = text.size)
-    points(brewery$x, brewery$y, pch = 15, cex = 1/3)
+    # points(brewery$x, brewery$y, pch = 15, cex = 1/3)
 
     # Falconberg Court and Mews (isolate)
     Falconberg <- data.frame(x = 19.5, y = 17.184)
@@ -127,7 +127,7 @@ addLandmarks <- function(text.size = 0.5, text.col = "black",
     aberdeen <- segmentIntersection(NW$x, NW$y, SE$x, SE$y,
                                     NE$x, NE$y, SW$x, SW$y)
 
-    points(aberdeen$x, aberdeen$y, pch = 15, cex = 1/3)
+    # points(aberdeen$x, aberdeen$y, pch = 15, cex = 1/3)
     text(aberdeen$x, aberdeen$y, labels = "Argyll\nHouse", cex = text.size)
 
     # 1) Marlborough Mews: Police Station
@@ -165,7 +165,7 @@ addLandmarks <- function(text.size = 0.5, text.col = "black",
     model.lodging <- segmentIntersection(NW$x, NW$y, SE$x, SE$y,
                                          NE$x, NE$y, SW$x, SW$y)
 
-    points(model.lodging$x, model.lodging$y, pch = 15, cex = 1/3)
+    # points(model.lodging$x, model.lodging$y, pch = 15, cex = 1/3)
     text(model.lodging$x, model.lodging$y, labels = "Model\nLodging",
          cex = text.size)
 
