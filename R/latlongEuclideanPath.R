2bbbf527625ff2a00251d90a751125461024b26b
lindbrook <lindbrook@gmail.com>
2023-05-13 10:35:52 -0700

    add milepost graphic prototype to latlongEuclideanPath()

--- R/latlongEuclideanPath.R
+++ R/latlongEuclideanPath.R
@@ -77,10 +77,15 @@ latlongEuclideanPath <- function(case = 1, destination = NULL, vestry = FALSE,
 #'
 #' @param x An object of class "latlong_euclidean_path" created by latlongEuclideanPath().
 #' @param zoom Logical.
+#' @param mileposts Logical. Plot mile/time posts.
+#' @param milepost.unit Character. "distance" or "time".
+#' @param milepost.interval Numeric. Mile post interval unit of distance (yard or meter) or unit of time (seconds).
 #' @param ... Additional plotting parameters.
 #' @export
 
-plot.latlong_euclidean_path <- function(x, zoom = TRUE, ...) {
+plot.latlong_euclidean_path <- function(x, zoom = TRUE, mileposts = TRUE,
+  milepost.unit = "distance", milepost.interval = NULL, ...) {
+
   vars <- c("lon", "lat")
   colors <- snowColors(x$vestry)
 
@@ -96,8 +101,8 @@ plot.latlong_euclidean_path <- function(x, zoom = TRUE, ...) {
     snowMap(latlong = TRUE, vestry = x$vestry)
   }
 
-  d.subtitle <- paste(round(x$data$distance, 1), x$distance.unit)
-  t.subtitle <- paste(round(x$data$time), paste0(x$time.unit, "s"), "@",
+  d.info <- paste(round(x$data$distance, 1), x$distance.unit)
+  t.info <- paste(round(x$data$time), paste0(x$time.unit, "s"), "@",
     x$walking.speed, "km/hr")
 
   p.col <- colors[paste0("p", x$data$pump)]
@@ -107,6 +112,130 @@ plot.latlong_euclidean_path <- function(x, zoom = TRUE, ...) {
   text(x$pump, labels = paste0("p", x$data$pump), pos = 1)
   arrows(x$case$lon, x$case$lat, x$pump$lon, x$pump$lat, col = p.col,
          length = 0.0875, lwd = 3)
-  title(main = paste("Case", x$data$case, "to Pump", x$data$pump),
-        sub = paste(d.subtitle, d.subtitle, sep = "; "))
+
+  if (mileposts) {
+    if (is.null(milepost.interval)) {
+      if (milepost.unit == "distance") {
+        milepost.interval <- 50
+      } else if (milepost.unit == "time") {
+        milepost.interval <- 60
+      }
+    }
+
+    if (milepost.unit == "distance") {
+      if (x$distance.unit == "meter") {
+        post.info <- paste("posts at", milepost.interval, "m intervals")
+      } else if (x$distance.unit == "yard") {
+        post.info <- paste("posts at", milepost.interval, "yd intervals")
+      }
+    } else if (milepost.unit == "time") {
+      post.info <- paste("posts at", milepost.interval, "sec intervals")
+    } else {
+      stop('"milepost.unit" muster either be "distance" or "time".', 
+        call. = FALSE)
+    }
+
+    origin <- data.frame(lon = min(cholera::roads$lon),
+                         lat = min(cholera::roads$lat))
+
+    x.pump.proj <- c(x$pump$lon, origin$lat)
+    y.pump.proj <- c(origin$lon, x$pump$lat)
+
+    x.case.proj <- c(x$case$lon, origin$lat)
+    y.case.proj <- c(origin$lon, x$case$lat)
+
+    # distance to origin (meters)
+    x.pump.meters <- geosphere::distGeo(x.pump.proj, origin)
+    y.pump.meters <- geosphere::distGeo(y.pump.proj, origin)
+    x.case.meters <- geosphere::distGeo(x.case.proj, origin)
+    y.case.meters <- geosphere::distGeo(y.case.proj, origin)
+
+    cartesian <- data.frame(x = c(x.pump.meters, x.case.meters),
+                            y = c(y.pump.meters, y.case.meters),
+                            row.names = c("pump", "case"))
+
+    ols <- stats::lm(y ~ x, data = cartesian)
+    theta <- atan(stats::coef(ols)["x"])
+
+    mileposts <- seq(milepost.interval, x$data$distance, milepost.interval)
+    milepost.ratios <- mileposts / x$data$distance
+
+    # compute milepost meter coordinates
+    milepost.coords <- lapply(milepost.ratios, function(r) {
+      milepostCoordinates(r, cartesian, theta)
+    })
+
+    milepost.coords <- do.call(rbind, milepost.coords)
+
+    # compute milepost latlong coordinates
+    topleft <- data.frame(lon = min(cholera::roads$lon),
+                          lat = max(cholera::roads$lat))
+    bottomright <- data.frame(lon = max(cholera::roads$lon),
+                              lat = min(cholera::roads$lat))
+    milepost.coords <- meterLatLong(milepost.coords, origin, topleft, bottomright)
+
+    # mileposts as arrows
+    arrow.data <- rbind(milepost.coords[, vars], x$case)
+    idx <- seq_len(nrow(arrow.data[-nrow(arrow.data), ]))
+
+    invisible(lapply(idx, function(i) {
+      arrows(arrow.data[i, "lon"], arrow.data[i, "lat"],
+             arrow.data[i + 1, "lon"], arrow.data[i + 1, "lat"],
+             code = 1, col = p.col, length = 0.0875, lwd = 3)
+    }))
+
+    title(main = paste("Case", x$data$case, "to Pump", x$data$pump),
+          sub = paste(d.info, t.info, post.info, sep = "; "))
+  } else {
+    title(main = paste("Case", x$data$case, "to Pump", x$data$pump),
+          sub = paste(d.info, t.info, sep = "; "))
+  }
+}
+
+milepostCoordinates <- function(r, dat, theta) {
+  h <- r * stats::dist(dat)
+  delta <- dat["pump", ] - dat["case", ] # relative postion of pump to case
+
+  # Pseudo-Quadrant I
+  if (all(delta > 0)) {
+    post.x <- dat["pump", "x"] - abs(h * cos(theta))
+    post.y <- dat["pump", "y"] - abs(h * sin(theta))
+
+  # Pseudo-Quadrant II
+  } else if (delta[1] < 0 & delta[2] > 0) {
+    post.x <- dat["pump", "x"] + abs(h * cos(theta))
+    post.y <- dat["pump", "y"] - abs(h * sin(theta))
+
+  # Pseudo-Quadrant III
+  } else if (all(delta < 0)) {
+    post.x <- dat["pump", "x"] + abs(h * cos(theta))
+    post.y <- dat["pump", "y"] + abs(h * sin(theta))
+
+  # Pseudo-Quadrant IV ## ##
+  } else if (delta["x"] > 0 & delta["y"] < 0) {
+    post.x <- dat["pump", "x"] - abs(h * cos(theta))
+    post.y <- dat["pump", "y"] + abs(h * sin(theta))
+
+  # I:IV
+  } else if (delta[1] > 0 & delta[2] == 0) {
+    post.x <- dat["pump", "x"] - abs(h * cos(theta))
+    post.y <- dat["pump", "y"]
+
+  # I:II
+  } else if (delta[1] == 0 & delta[2] > 0) {
+    post.x <- dat["pump", "x"]
+    post.y <- dat["pump", "y"] - abs(h * sin(theta))
+
+  # II:III
+  } else if (delta[1] < 0 & delta[2] == 0) {
+    post.x <- dat["pump", "x"] + abs(h * cos(theta))
+    post.y <- dat["pump", "y"]
+
+  # III:IV
+  } else if (delta[1] == 0 & delta[2] < 0) {
+    post.x <- dat["pump", "x"]
+    post.y <- dat["pump", "y"] + abs(h * sin(theta))
+  }
+
+  data.frame(x = c(post.x), y = c(post.y))
 }
