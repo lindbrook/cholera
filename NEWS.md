5a7a53c6d54ba7bf37345af6fd002d11ec329d85
lindbrook <lindbrook@gmail.com>
2023-07-05 10:41:59 -0700

    add plots for latlongNeighborhoodEuclidean(case.set = "expected")

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+### cholera 0.8.0.9075
+
+- add plots for latlongNeighborhoodEuclidean(case.set = "expected").
+
+
 ### cholera 0.8.0.9074
 
 - add/set latlong = FALSE to pearlStringRadius(), peripheryCases() and 
--- R/latlongNeighborhoodEuclidean.R
+++ R/latlongNeighborhoodEuclidean.R
@@ -34,11 +34,10 @@ latlongNeighborhoodEuclidean <- function(pump.select = NULL, vestry = FALSE,
 
   cells <- latlongVoronoi(pump.select = pump.select, vestry = vestry)
 
-  if (!is.null(pump.select)) {
-    pump.id <- selectPump(pump.data, pump.select = pump.select,
-      metric = "euclidean", vestry = vestry)
-  } else {
+  if (is.null(pump.select)) {
     pump.id <- pump.select
+  } else {
+    pump.id <- selectPump(pump.data, pump.select, "euclidean", vestry)
   }
 
   if (case.set == "observed") {
@@ -59,34 +58,68 @@ latlongNeighborhoodEuclidean <- function(pump.select = NULL, vestry = FALSE,
     sp::point.in.polygon(case.data$lon, case.data$lat, cell$lon, cell$lat)
   })
 
-  out <- list(pump.select = pump.id, vestry = vestry, cells = cells,
-    pump.data = pump.data, case.location = case.location,
-    statistic.data = statistic.data)
+  out <- list(pump.select = pump.select,
+              pump.id = pump.id,
+              vestry = vestry,
+              cells = cells,
+              pump.data = pump.data,
+              case.location = case.location,
+              case.set = case.set,
+              snow.colors = snowColors(vestry = vestry),
+              statistic.data = statistic.data)
+
   class(out) <- "latlongEuclidean"
   out
 }
 
 #' Plot method for latlongNeighborhoodEuclidean()
 #' @param x Object.
-#' @param euclidean.paths Logical.
+#' @param type Character. "star", "area.points" or "area.polygons". "area" flavors only valid when \code{case.set = "expected"}.
 #' @param ... Additional plotting parameters.
 #' @export
 
-plot.latlongEuclidean <- function(x, euclidean.paths = TRUE, ...) {
-  pump.select <- x$pump.select
+plot.latlongEuclidean <- function(x, type = "star", ...) {
+  pump.id <- x$pump.id
   vars <- c("lon", "lat")
 
-  snowMap(vestry = x$vestry, latlong = TRUE, add.cases = FALSE,
-    add.pumps = FALSE)
-  addPump(pump.select, vestry = x$vestry, latlong = TRUE)
+  if (x$case.set == "observed") {
+    snowMap(vestry = x$vestry, latlong = TRUE, add.cases = FALSE,
+      add.pumps = FALSE)
+    pumpTokens(x, type, latlong = TRUE)
 
-  if (!is.null(pump.select)) {
-    unselected <- x$pump.data[!x$pump.data$id %in% pump.select, ]
-    names(x$statistic.data) <- pump.select
-    snow.colors <- snowColors(vestry = x$vestry)[paste0("p", pump.select)]
-    points(unselected[, vars], pch = 2, col = "gray")
-    text(unselected[, vars], labels = paste0("p", unselected$id), pos = 1,
-      col = "gray")
+    if (!is.null(type)) {
+      if (type == "star") {
+        latlongEuclideanStar(x, vars)
+      }
+    }
+
+    latlongEuclideanCases(x, vars)
+
+  } else if (x$case.set == "expected") {
+    snowMap(vestry = x$vestry, add.cases = FALSE, add.pumps = FALSE,
+      add.roads = FALSE, latlong = TRUE)
+
+    if (type == "star") {
+      latlongEuclideanStar(x, vars)
+      addRoads(latlong = TRUE, col = "black")
+      pumpTokens(x, type, latlong = TRUE)
+
+    } else if (type == "area.points") {
+      latlongEuclideanCases(x, vars)
+      addRoads(latlong = TRUE, col = "black")
+      pumpTokens(x, type, latlong = TRUE)
+
+    } else if (type == "area.polygons") {
+      latlongEuclideanAreaPolygons(x)
+      addRoads(latlong = TRUE, col = "black")
+      pumpTokens(x, type, latlong = TRUE)
+    } else {
+      stop('type must be "star", "area.points" or "area.polygons".',
+        call. = FALSE)
+    }
+  }
+
+  if (!is.null(pump.id)) {
     if (x$case.location == "address") {
       title(main = paste0("Pump Neighborhoods: Euclidean (address)", "\n",
         "Pumps ", paste(sort(x$pump.select), collapse = ", ")))
@@ -95,17 +128,97 @@ plot.latlongEuclidean <- function(x, euclidean.paths = TRUE, ...) {
         "Pumps ", paste(sort(x$pump.select), collapse = ", ")))
     }
   } else {
-    snow.colors <- snowColors(vestry = x$vestry)
     if (x$case.location == "address") {
       title(main = "Pump Neighborhoods: Euclidean (address)")
     } else if (x$case.location == "orthogonal") {
       title(main = "Pump Neighborhoods: Euclidean (orthogonal)")
     }
   }
+}
 
-  if (euclidean.paths) {
-    plotLatlongEuclideanPaths(x, pump.select, snow.colors, vars)
+latlongEuclideanCases <- function(x, vars) {
+  if (x$case.set == "observed") {
+    if (x$case.location == "address") {
+      dat <- cholera::fatalities.address
+      names(dat)[names(dat) == "anchor"] <- "case"
+    } else if (x$case.location == "orthogonal") {
+      dat <- cholera::latlong.ortho.addr
+    }
+  } else if (x$case.set == "expected") {
+    if (x$case.location == "address") {
+      dat <- cholera::latlong.regular.cases
+      dat$case <- seq_len(nrow(dat))
+    } else if (x$case.location == "orthogonal") {
+      dat <- cholera::latlong.sim.ortho.proj
+    }
+  } else stop()
+
+  case.partition <- lapply(x$statistic.data, function(neighbor) {
+    dat$case[neighbor == 1]
+  })
+
+  invisible(lapply(names(case.partition), function(nm) {
+    sel <- dat$case %in% case.partition[[nm]]
+    points(dat[sel, vars], col = x$snow.colors[nm], pch = 20, cex = 0.75)
+  }))
+}
+
+latlongEuclideanStar <- function(x, vars) {
+  if (x$case.set == "observed") {
+    cases <- cholera::fatalities.address
+  } else if (x$case.set == "expected") {
+    cases <- cholera::latlong.regular.cases
+    cases$anchor <- seq_len(nrow(cases))
   }
 
-  plotLatlongVoronoiCases(x, snow.colors, vars)
+  if (is.null(x$pump.id)) pump.id <- x$pump.data$id
+  else pump.id <- x$pump.id
+
+  nearest.pump <- do.call(rbind, lapply(cases$anchor, function(a) {
+    p1 <- cases[cases$anchor == a, vars]
+    d <- vapply(pump.id, function(p) {
+      p2 <- x$pump.data[x$pump.data$id == p, vars]
+      geosphere::distGeo(p1, p2)
+    }, numeric(1L))
+    near.id <- which.min(d)
+    if (is.null(pump.id)) p.nr <- x$pump.data$id[near.id]
+    else p.nr <- pump.id[near.id]
+    data.frame(case = a, pump = p.nr, meters = d[near.id])
+  }))
+
+  invisible(lapply(nearest.pump$case, function(c) {
+    ego <- cases[cases$anchor == c, vars]
+    p <- nearest.pump[nearest.pump$case == c, "pump"]
+    alter <- x$pump.data[x$pump.data$id == p, vars]
+    segments(ego$lon, ego$lat, alter$lon, alter$lat,
+             col = x$snow.colors[paste0("p", p)], lwd = 0.5)
+  }))
+}
+
+latlongEuclideanAreaPolygons <- function(x) {
+  if (x$case.set == "expected") {
+    if (x$case.location == "address") {
+      dat <- cholera::latlong.regular.cases
+      dat$case <- seq_len(nrow(dat))
+    } else if (x$case.location == "orthogonal") {
+      dat <- cholera::latlong.sim.ortho.proj
+    }
+  }
+
+  neighborhood.cases <- lapply(x$statistic.data, function(neighbor) {
+    dat$case[neighbor == 1]
+  })
+
+  periphery.cases <- lapply(neighborhood.cases, function(x) {
+    peripheryCases(x, latlong = TRUE)
+  })
+
+  pearl.string <- lapply(periphery.cases, function(x) {
+    travelingSalesman(x, latlong = TRUE)
+  })
+
+  invisible(lapply(names(pearl.string), function(nm) {
+    polygon(dat[pearl.string[[nm]], c("lon", "lat")],
+      col = grDevices::adjustcolor(x$snow.colors[nm], alpha.f = 2/3))
+  }))
 }
