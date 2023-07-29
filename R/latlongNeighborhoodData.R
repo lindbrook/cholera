465853de1d2c97adf0133fe2ba278e729dd23f38
lindbrook <lindbrook@gmail.com>
2023-07-29 10:04:32 -0700

    add/set latlongNeighborhoodData(case.set = "observed")

--- R/latlongNeighborhoodData.R
+++ R/latlongNeighborhoodData.R
@@ -2,12 +2,15 @@
 #'
 #' Assembles cases, pumps and road into a network graph.
 #' @param vestry Logical.
+#' @param case.set Character. "observed" or "expected".
 #' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
 #' @export
 
-latlongNeighborhoodData <- function(vestry = FALSE, multi.core = TRUE) {
+latlongNeighborhoodData <- function(vestry = FALSE, case.set = "observed",
+  multi.core = TRUE) {
+  
   cores <- multiCore(multi.core)
-  dat <- latlongEmbed(vestry = vestry, multi.core = cores)
+  dat <- latlongEmbed(vestry = vestry, case.set = case.set, multi.core = cores)
 
   edges <- dat$edges
   edges$node1 <- paste0(edges$lon1, "_&_", edges$lat1)
