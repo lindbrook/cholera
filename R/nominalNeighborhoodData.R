4bc175579b86ccf5bd045cf38d7e70c529116a8e
lindbrook <lindbrook@gmail.com>
2023-08-11 14:50:52 -0700

    add/set nominalNeighborhoodData(multi.core = TRUE)

--- R/nominalNeighborhoodData.R
+++ R/nominalNeighborhoodData.R
@@ -5,14 +5,15 @@
 #' @param case.set Character. "observed", "expected", or "snow". "snow" captures John Snow's annotation of the Broad Street pump neighborhood printed in the Vestry report version of the map.
 #' @param embed.addr Logical. Embed cases into road network.
 #' @param embed.landmarks Logical. Embed landmarks into road network.
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
 #' @export
 #' @return An R list of nodes, edges and an 'igraph' network graph.
 
 nominalNeighborhoodData <- function(vestry = FALSE, case.set = "observed",
-  embed.addr = TRUE, embed.landmarks = TRUE) {
-  
+  embed.addr = TRUE, embed.landmarks = TRUE, multi.core = TRUE) {
+
   args <- list(embed.addr = embed.addr, embed.landmarks = embed.landmarks,
-    vestry = vestry, case.set = case.set)
+    vestry = vestry, case.set = case.set, multi.core = multiCore(multi.core))
 
   node.data <- do.call("nominalEmbed", args)
   nodes <- node.data$nodes
