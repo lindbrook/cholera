ac3250a0209dfa866abf5417cbe7a082872b3577
lindbrook <lindbrook@gmail.com>
2023-08-23 09:22:30 -0700

    add embedNodes() prototype

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+### cholera 0.8.0.9107
+
+- add embedNodes() prototype.
+
+
 ### cholera 0.8.0.9106
 
 - add orthogonal longitude and latitude to landmarks data frame.
---
+++ R/embedNodes.R
@@ -0,0 +1,351 @@
+#' Embed anchors and pumps into road segments (prototype).
+#'
+#' @param vestry Logical.
+#' @param case.set Character. "observed" or "expected".
+#' @param embed.addr Logical. Embed the 321 case addresses into graph network.
+#' @param embed.landmarks Logical. Embed landmarks into road network.
+#' @param embed.pumps Logical. Embed pumps into road network.
+#' @param latlong Logical. Use estimated longitude and latitude.
+#' @param multi.core Logical or Numeric. \code{TRUE} uses \code{parallel::detectCores()}. \code{FALSE} uses one, single core. You can also specify the number logical cores. See \code{vignette("Parallelization")} for details.
+#' @noRd
+
+embedNodes <- function(vestry = FALSE, case.set = "observed", embed.addr = TRUE,
+  embed.landmarks = TRUE, embed.pumps = TRUE, latlong = FALSE,
+  multi.core = TRUE) {
+
+  cores <- multiCore(multi.core)
+  road.data <- roadSegments(latlong = latlong)
+  if (latlong) vars <- c("lon", "lat")
+  else vars <- c("x", "y")
+
+  if (embed.addr & embed.landmarks & embed.pumps) {
+    ortho.addr <- orthoAddrC(case.set = case.set, latlong = latlong)
+    ortho.land <- orthoLandC(latlong = latlong)
+    ortho.pump <- orthoPumpsC(vestry = vestry, latlong = latlong)
+    obs.segs <- unique(c(ortho.addr$road.segment, ortho.land$road.segment,
+      ortho.pump$road.segment))
+
+  } else if (!embed.addr & embed.landmarks & embed.pumps) {
+    ortho.land <- orthoLandC(latlong = latlong)
+    ortho.pump <- orthoPumpsC(vestry = vestry, latlong = latlong)
+    obs.segs <- union(ortho.land$road.segment, ortho.pump$road.segment)
+
+  } else if (embed.addr & !embed.landmarks & embed.pumps) {
+    ortho.addr <- orthoAddrC(case.set = case.set, latlong = latlong)
+    ortho.pump <- orthoPumpsC(vestry = vestry, latlong = latlong)
+    obs.segs <- union(ortho.addr$road.segment, ortho.pump$road.segment)
+
+  } else if (!embed.addr & !embed.landmarks & embed.pumps) {
+    ortho.pump <- orthoPumpsC(vestry = vestry, latlong = latlong)
+    obs.segs <- unique(ortho.pump$road.segment)
+
+  } else if (embed.addr & embed.landmarks & !embed.pumps) {
+    ortho.addr <- orthoAddrC(case.set = case.set, latlong = latlong)
+    ortho.land <- orthoLandC(latlong = latlong)
+    obs.segs <- union(ortho.addr$road.segment, ortho.land$road.segment)
+
+  } else if (!embed.addr & embed.landmarks & !embed.pumps) {
+    ortho.land <- orthoLandC(latlong = latlong)
+    obs.segs <- unique(ortho.land$road.segment)
+
+  } else if (embed.addr & !embed.landmarks & !embed.pumps) {
+    ortho.addr <- orthoAddrC(case.set = case.set, latlong = latlong)
+    obs.segs <- unique(ortho.addr$road.segment)
+
+  } else if (!embed.addr & !embed.landmarks & !embed.pumps) {
+    obs.segs <- NULL
+  }
+
+  if (!is.null(obs.segs)) {
+    no_embeds <- road.data[!road.data$id %in% obs.segs, ]
+
+    vars2 <- c(vars, "case", "pump")
+
+    null.nms <- c("case", "road.segment", vars)
+    null.df <- stats::setNames(data.frame(matrix(nrow = 0, ncol = 4)), null.nms)
+
+    embeds <- parallel::mclapply(obs.segs, function(s) {
+      rd.tmp <- road.data[road.data$id == s, ]
+
+      if (latlong) {
+        endpts <- data.frame(lon = unlist(rd.tmp[, paste0(vars[1], 1:2)]),
+                             lat = unlist(rd.tmp[, paste0(vars[2], 1:2)]),
+                             case = 0,
+                             pump = 0,
+                             row.names = NULL)
+      } else {
+        endpts <- data.frame(x = unlist(rd.tmp[, paste0(vars[1], 1:2)]),
+                             y = unlist(rd.tmp[, paste0(vars[2], 1:2)]),
+                             case = 0,
+                             pump = 0,
+                             row.names = NULL)
+      }
+
+      if (exists("ortho.addr")) {
+        addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
+      } else {
+        addr.tmp <- null.df
+      }
+
+      if (exists("ortho.land")) {
+        land.tmp <- ortho.land[ortho.land$road.segment == s, ]
+      } else {
+        land.tmp <- null.df
+      }
+
+      if (exists("ortho.pump")) {
+        pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+      } else {
+        pump.tmp <- null.df
+      }
+
+      if (nrow(addr.tmp) > 0 & nrow(land.tmp) > 0 & nrow(pump.tmp) > 0) {
+        addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
+
+        land.embed <- land.tmp[, c(vars, "case")]
+        addr.embed <- rbind(addr.tmp[, c(vars, "case")], land.embed)
+
+        pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+        pump.tmp$case <- 0L
+        pump.embed <- pump.tmp[, vars2]
+
+        addr.embed$pump <- 0
+        pump.embed$case <- 0
+        pump.embed <- pump.embed[, vars2]
+        embed.data <- rbind(endpts, addr.embed, pump.embed)
+
+      } else if (nrow(addr.tmp) == 0 &
+                 nrow(land.tmp) > 0 &
+                 nrow(pump.tmp) > 0) {
+
+        addr.embed <- land.tmp[, c("case", vars)]
+
+        pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+        pump.tmp$case <- 0L
+        pump.embed <- pump.tmp[, vars2]
+
+        addr.embed$pump <- 0
+        pump.embed$case <- 0
+        pump.embed <- pump.embed[, vars2]
+        embed.data <- rbind(endpts, addr.embed, pump.embed)
+
+      } else if (nrow(addr.tmp) > 0 &
+                 nrow(land.tmp) == 0 &
+                 nrow(pump.tmp) > 0) {
+
+        addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
+        addr.embed <- addr.tmp[, c(vars, "case")]
+
+        pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+        pump.tmp$case <- 0L
+        pump.embed <- pump.tmp[, vars2]
+
+        addr.embed$pump <- 0
+        pump.embed$case <- 0
+        pump.embed <- pump.embed[, vars2]
+        embed.data <- rbind(endpts, addr.embed, pump.embed)
+
+      } else if (nrow(addr.tmp) == 0 &
+                 nrow(land.tmp) == 0 &
+                 nrow(pump.tmp) > 0) {
+
+        pump.tmp <- ortho.pump[ortho.pump$road.segment == s, ]
+        pump.tmp$case <- 0L
+        pump.embed <- pump.tmp[, vars2]
+
+        pump.embed$case <- 0
+        pump.embed <- pump.embed[, vars2]
+        embed.data <- rbind(endpts, pump.embed)
+
+      } else if (nrow(addr.tmp) > 0 &
+                 nrow(land.tmp) > 0 &
+                 nrow(pump.tmp) == 0) {
+
+        addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
+
+        land.embed <- land.tmp[, c(vars, "case")]
+        addr.embed <- rbind(addr.tmp[, c(vars, "case")], land.embed)
+        addr.embed$pump <- 0
+
+        embed.data <- rbind(endpts, addr.embed)
+
+      } else if (nrow(addr.tmp) == 0 &
+                 nrow(land.tmp) > 0 &
+                 nrow(pump.tmp) == 0) {
+        
+        addr.embed <- land.tmp[, c(vars, "case")]
+        addr.embed$pump <- 0
+
+        embed.data <- rbind(endpts, addr.embed)
+
+      } else if (nrow(addr.tmp) > 0 &
+                 nrow(land.tmp) == 0 &
+                 nrow(pump.tmp) == 0) {
+
+        addr.tmp <- ortho.addr[ortho.addr$road.segment == s, ]
+        addr.embed <- addr.tmp[, c(vars, "case")]
+
+        addr.embed$pump <- 0
+
+        embed.data <- rbind(endpts, addr.embed)
+
+      } else if (nrow(addr.tmp) == 0 &
+                 nrow(land.tmp) == 0 &
+                 nrow(pump.tmp) == 0) {
+
+        embed.data <- endpts
+      }
+
+      if (latlong) nodes <- embed.data[order(embed.data$lon, embed.data$lat), ]
+      else nodes <- embed.data[order(embed.data$x, embed.data$y), ]
+
+      tmp <- nodes[, vars]
+      tmp <- cbind(tmp[-nrow(tmp), ], tmp[-1, ])
+      coord.nms <- paste0(names(tmp), c(rep(1, 2), rep(2, 2)))
+      names(tmp) <- coord.nms
+      tmp <- cbind(tmp, rd.tmp[, c("street", "id", "name")])
+      edges <- tmp[, c("street", "id", "name", coord.nms)]
+
+      if (case.set == "observed") {
+        edges$id2 <- paste0(edges$id, letters[seq_len(nrow(edges))])
+      } else if (case.set == "expected") {
+        edges$id2 <- paste0(edges$id, "-", seq_len(nrow(edges)))
+      }
+
+      list(edges = edges, nodes = nodes)
+    }, mc.cores = cores)
+
+    no_embeds$id2 <- paste0(no_embeds$id, "a")
+
+    edges <- do.call(rbind, lapply(embeds, function(x) x$edges))
+    edges <- rbind(edges, no_embeds[, names(edges)])
+    edges <- edges[order(edges$street), ]
+
+    if (latlong) {
+      edges$node1 <- paste0(edges$lon1, "_&_", edges$lat1)
+      edges$node2 <- paste0(edges$lon2, "_&_", edges$lat2)
+      edges$d <- vapply(seq_len(nrow(edges)), function(i) {
+        p1 <- edges[i, c("lon1", "lat1")]
+        p2 <- edges[i, c("lon2", "lat2")]
+        geosphere::distGeo(p1, p2)
+      }, numeric(1L))
+    } else {
+      edges$node1 <- paste0(edges$x1, "_&_", edges$y1)
+      edges$node2 <- paste0(edges$x2, "_&_", edges$y2)
+      edges$d <- vapply(seq_len(nrow(edges)), function(i) {
+        stats::dist(rbind(stats::setNames(edges[i, paste0(vars, 1)], vars),
+                          stats::setNames(edges[i, paste0(vars, 2)], vars)))
+      }, numeric(1L))
+    }
+
+    edge.list <- edges[, c("node1", "node2")]
+    g <- igraph::graph_from_data_frame(edge.list, directed = FALSE)
+
+    nodes <- do.call(rbind, lapply(embeds, function(x) x$nodes))
+    n1 <- stats::setNames(no_embeds[, paste0(vars, 1)], vars)
+    n2 <- stats::setNames(no_embeds[, paste0(vars, 2)], vars)
+    nodes.no_embeds <- rbind(n1, n2)
+    nodes.no_embeds$case <- 0
+    nodes.no_embeds$pump <- 0
+    nodes <- rbind(nodes, nodes.no_embeds)
+
+    if (latlong) nodes$node <- paste0(nodes$lon, "_&_", nodes$lat)
+    else nodes$node <- paste0(nodes$x, "_&_", nodes$y)
+
+  } else {
+    edges <- road.data
+    if (latlong) {
+      edges$node1 <- paste0(edges$lon1, "_&_", edges$lat1)
+      edges$node2 <- paste0(edges$lon2, "_&_", edges$lat2)
+      edges$d <- vapply(seq_len(nrow(edges)), function(i) {
+        p1 <- edges[i, paste0(vars, 1)]
+        p2 <- edges[i, paste0(vars, 2)]
+        geosphere::distGeo(p1, p2)
+      }, numeric(1L))
+    } else {
+      edges$node1 <- paste0(edges$x1, "_&_", edges$y1)
+      edges$node2 <- paste0(edges$x2, "_&_", edges$y2)
+      edges$d <- vapply(seq_len(nrow(edges)), function(i) {
+        stats::dist(rbind(stats::setNames(edges[i, paste0(vars, 1)], vars),
+                          stats::setNames(edges[i, paste0(vars, 1)], vars)))
+      }, numeric(1L))
+    }
+
+    edge.list <- edges[, c("node1", "node2")]
+    g <- igraph::graph_from_data_frame(edge.list, directed = FALSE)
+
+    node.nms <- c(vars, "node")
+    n1 <- stats::setNames(edges[, paste0(node.nms, 1)], node.nms)
+    n2 <- stats::setNames(edges[, paste0(node.nms, 2)], node.nms)
+    nodes <- rbind(n1, n2)
+    nodes$case <- 0
+    nodes$pump <- 0
+    nodes <- nodes[, c(vars, "case", "pump", "node")]
+  }
+
+  if (!is.null(obs.segs)) {
+    edges <- edges[order(edges$street, edges$id2), ]
+  } else {
+    edges <- edges[order(edges$street, edges$id), ]
+  }
+
+  if (latlong) nodes <- nodes[order(nodes$lon, nodes$lat), ]
+  else nodes <- nodes[order(nodes$x, nodes$y), ]
+  # names(nodes)[names(nodes) == "case"] <- "anchor"
+  row.names(edges) <- NULL
+  row.names(nodes) <- NULL
+
+  list(g = g, edges = edges, nodes = nodes)
+}
+
+orthoAddrC <- function(case.set = "observed", latlong = FALSE) {
+  if (latlong) {
+    if (case.set == "observed") {
+      out <- cholera::latlong.ortho.addr
+    } else if (case.set == "expected") {
+      out <- cholera::latlong.sim.ortho.proj
+    }
+  } else {
+    if (case.set == "observed") {
+      sel <- cholera::ortho.proj$case %in% cholera::fatalities.address$anchor
+      out <- cholera::ortho.proj[sel, ]
+    } else if (case.set == "expected") {
+      out <- cholera::sim.ortho.proj
+    }
+    sel <- names(out) %in% c("x.proj", "y.proj")
+    names(out)[sel] <- c("x", "y")
+    }
+  out
+}
+
+orthoLandC <- function(latlong = FALSE) {
+  dat <- landmarkDataB()
+  sel <- grepl("lon", names(cholera::landmarks)) |
+         grepl("lat", names(cholera::landmarks))
+  dat <- cbind(dat, cholera::landmarks[, sel])
+  sel.latlong <- grepl("lon", names(dat)) | grepl("lat", names(dat))
+  sel.xy <- grepl("x", names(dat)) | grepl("y", names(dat))
+  if (latlong) coords <- dat[, sel.latlong]
+  else coords <- dat[, sel.xy]
+  cbind(dat[, c("case", "name", "road.segment")], coords)
+}
+
+orthoPumpsC <- function(vestry = TRUE, latlong = FALSE) {
+  if (latlong) {
+    if (vestry) {
+      out <- cholera::latlong.ortho.pump.vestry
+    } else {
+      out <- cholera::latlong.ortho.pump
+    }
+    names(out)[names(out) == "id"] <- "pump"
+  } else {
+    if (vestry) {
+      out <- cholera::ortho.proj.pump.vestry
+    } else {
+      out <- cholera::ortho.proj.pump
+    }
+    sel <- c("x.proj", "y.proj", "pump.id")
+    names(out)[names(out) %in% sel] <- c("x", "y", "pump")
+  }
+  out
+}
