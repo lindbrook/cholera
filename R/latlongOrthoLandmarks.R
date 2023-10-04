96a660b0ba33b7176bb541de6350e2348f6da7db
lindbrook <lindbrook@gmail.com>
2023-10-04 10:03:41 -0700

    set Lion Brewery address (lon.proj and lat.proj) to Broad Street

--- R/latlongOrthoLandmarks.R
+++ R/latlongOrthoLandmarks.R
@@ -8,20 +8,14 @@ latlongOrthoLandmarks <- function(multi.core = TRUE) {
   lndmks <- cholera::landmarks
 
   # reset (delete) x.proj-y.proj for recomputation
-  lndmks <- lndmks[, !names(lndmks) %in% c("x.proj", "y.proj")]
+  lndmks <- lndmks[, !names(lndmks) %in% c("lon.proj", "lat.proj")]
 
-  # TODO geodesicMeters(lndmks)
   origin <- data.frame(lon = min(cholera::roads$lon),
                        lat = min(cholera::roads$lat))
-
-  geo.lndmks <- do.call(rbind, lapply(lndmks$case, function(x) {
-    tmp <- lndmks[lndmks$case == x, c("lon", "lat")]
-    x.proj <- c(tmp$lon, origin$lat)
-    y.proj <- c(origin$lon, tmp$lat)
-    m.lon <- geosphere::distGeo(y.proj, tmp)
-    m.lat <- geosphere::distGeo(x.proj, tmp)
-    data.frame(id = x, x = m.lon, y = m.lat)
-  }))
+  topleft <- data.frame(lon = min(cholera::roads$lon),
+                        lat = max(cholera::roads$lat))
+  bottomright <- data.frame(lon = max(cholera::roads$lon),
+                            lat = min(cholera::roads$lat))
 
   # roads #
 
@@ -39,89 +33,116 @@ latlongOrthoLandmarks <- function(multi.core = TRUE) {
   })
 
   geo.rd.segs <- do.call(rbind, geo.rd.segs)
-  seg.endpts <- c("x1", "y1", "x2", "y2")
+  seg.endpts <- names(geo.rd.segs)[!names(geo.rd.segs) %in% c("street", "id")]
+
+  geo.lndmks <- do.call(rbind, lapply(lndmks$case, function(x) {
+    tmp <- lndmks[lndmks$case == x, c("lon", "lat")]
+    x.proj <- c(tmp$lon, origin$lat)
+    y.proj <- c(origin$lon, tmp$lat)
+    m.lon <- geosphere::distGeo(y.proj, tmp)
+    m.lat <- geosphere::distGeo(x.proj, tmp)
+    data.frame(id = x, x = m.lon, y = m.lat)
+  }))
 
   # orthogonal projection road addresses #
+  # Lion Brewery exception: entrance likely on Broad Street.
 
   orthogonal.projection <- parallel::mclapply(geo.lndmks$id, function(id) {
-    case <- geo.lndmks[geo.lndmks$id == id, c("x", "y")]
+    if (id != 20004) {
+      case <- geo.lndmks[geo.lndmks$id == id, c("x", "y")]
 
-    within.radius <- lapply(geo.rd.segs$id, function(x) {
-      seg.data <- geo.rd.segs[geo.rd.segs$id == x, ]
-      # Check tolerance
-      test1 <- withinRadius(case, seg.data[, c("x1", "y1")], 75)
-      test2 <- withinRadius(case, seg.data[, c("x2", "y2")], 75)
-      if (any(test1, test2)) unique(seg.data$id)
-    })
+      within.radius <- lapply(geo.rd.segs$id, function(x) {
+        seg.data <- geo.rd.segs[geo.rd.segs$id == x, ]
+        # Check tolerance
+        test1 <- withinRadius(case, seg.data[, c("x1", "y1")], 75)
+        test2 <- withinRadius(case, seg.data[, c("x2", "y2")], 75)
+        if (any(test1, test2)) unique(seg.data$id)
+      })
 
-    within.radius <- unlist(within.radius)
+      within.radius <- unlist(within.radius)
 
-    ortho.proj.test <- lapply(within.radius, function(seg.id) {
-      sel <- geo.rd.segs$id == seg.id
-      segment.data <- geo.rd.segs[sel, seg.endpts]
-      road.segment <- data.frame(x = c(segment.data$x1, segment.data$x2),
-                                 y = c(segment.data$y1, segment.data$y2))
+      ortho.proj.test <- lapply(within.radius, function(seg.id) {
+        sel <- geo.rd.segs$id == seg.id
+        segment.data <- geo.rd.segs[sel, seg.endpts]
+        road.segment <- data.frame(x = c(segment.data$x1, segment.data$x2),
+                                   y = c(segment.data$y1, segment.data$y2))
 
-      # tmp <- rbind(road.segment, case)
-      # plot(road.segment, xlim = range(tmp$x), ylim = range(tmp$y), asp = 1)
-      # segments(segment.data$x1, segment.data$y1, segment.data$x2, segment.data$y2)
-      # points(case, pch = 2, col = "red")
-      # title(main = paste0("p", p))
+        # tmp <- rbind(road.segment, case)
+        # plot(road.segment, xlim = range(tmp$x), ylim = range(tmp$y), asp = 1)
+        # segments(segment.data$x1, segment.data$y1, segment.data$x2, segment.data$y2)
+        # points(case, pch = 2, col = "red")
+        # title(main = paste0("p", p))
 
-      ols <- stats::lm(y ~ x, data = road.segment)
+        ols <- stats::lm(y ~ x, data = road.segment)
+        road.intercept <- stats::coef(ols)[1]
+        road.slope <- stats::coef(ols)[2]
+        ortho.slope <- -1 / road.slope
+        ortho.intercept <- case$y - ortho.slope * case$x
+        x.proj <- (ortho.intercept - road.intercept) /
+                  (road.slope - ortho.slope)
+        y.proj <- road.slope * x.proj + road.intercept
+
+        seg.data <- geo.rd.segs[geo.rd.segs$id == seg.id, seg.endpts]
+        seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
+                             y = c(seg.data$y1, seg.data$y2))
+
+        # segment bisection/intersection test
+        distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
+                 stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
+
+        bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
+
+        if (bisect.test) {
+          ortho.dist <- c(stats::dist(rbind(c(case$x, case$y),
+            c(x.proj, y.proj))))
+          ortho.pts <- data.frame(x.proj, y.proj)
+          data.frame(road.segment = seg.id, ortho.pts, ortho.dist)
+        } else {
+          null.out <- data.frame(matrix(NA, ncol = 4))
+          names(null.out) <- c("road.segment", "x.proj", "y.proj", "ortho.dist")
+          null.out
+        }
+      })
+
+      out <- do.call(rbind, ortho.proj.test)
+
+      if (all(is.na(out)) == FALSE) {
+        sel <- which.min(out$ortho.dist)
+        out <- out[sel, ]
+      } else {
+        # all candidate roads are NA so arbitrarily choose the first obs.
+        out <- out[1, ]
+      }
+
+      out$id <- id
+      row.names(out) <- NULL
+      out
+    } else {
+      broad.st <- geo.rd.segs[geo.rd.segs$id == "187-1", ]
+      broad <- rbind(stats::setNames(broad.st[, c("x1", "y1")], c("x", "y")),
+                     stats::setNames(broad.st[, c("x2", "y2")], c("x", "y")))
+
+      h <- c(stats::dist(broad))
+
+      ols <- stats::lm(y ~ x, data = broad)
       road.intercept <- stats::coef(ols)[1]
       road.slope <- stats::coef(ols)[2]
-      ortho.slope <- -1 / road.slope
-      ortho.intercept <- case$y - ortho.slope * case$x
-      x.proj <- (ortho.intercept - road.intercept) / (road.slope - ortho.slope)
-      y.proj <- road.slope * x.proj + road.intercept
+      theta <- ifelse(is.na(road.slope), pi / 2, atan(road.slope))
 
-      seg.data <- geo.rd.segs[geo.rd.segs$id == seg.id, seg.endpts]
-      seg.df <- data.frame(x = c(seg.data$x1, seg.data$x2),
-                           y = c(seg.data$y1, seg.data$y2))
+      delta.x <- (h / 2) * cos(theta)
+      delta.y <- (h / 2) * sin(theta)
 
-      # segment bisection/intersection test
-      distB <- stats::dist(rbind(seg.df[1, ], c(x.proj, y.proj))) +
-               stats::dist(rbind(seg.df[2, ], c(x.proj, y.proj)))
+      left <- broad[which.min(broad$x), ]
+      x.new <- left$x + delta.x
+      y.new <- left$y + delta.y
 
-      bisect.test <- signif(stats::dist(seg.df)) == signif(distB)
-
-      if (bisect.test) {
-        ortho.dist <- c(stats::dist(rbind(c(case$x, case$y),
-          c(x.proj, y.proj))))
-        ortho.pts <- data.frame(x.proj, y.proj)
-        data.frame(road.segment = seg.id, ortho.pts, ortho.dist)
-      } else {
-        null.out <- data.frame(matrix(NA, ncol = 4))
-        names(null.out) <- c("road.segment", "x.proj", "y.proj", "ortho.dist")
-        null.out
-      }
-    })
-
-    out <- do.call(rbind, ortho.proj.test)
-
-    if (all(is.na(out)) == FALSE) {
-      sel <- which.min(out$ortho.dist)
-      out <- out[sel, ]
-    } else {
-      # all candidate roads are NA so arbitrarily choose the first obs.
-      out <- out[1, ]
+      data.frame(road.segment = "187-1", x.proj = x.new, y.proj = y.new,
+                 ortho.dist = 0, id = id)
     }
 
-    out$id <- id
-    row.names(out) <- NULL
-    out
   }, mc.cores = cores)
 
   coords <- do.call(rbind, orthogonal.projection)
-
-  origin <- data.frame(lon = min(cholera::roads$lon),
-                       lat = min(cholera::roads$lat))
-  topleft <- data.frame(lon = min(cholera::roads$lon),
-                        lat = max(cholera::roads$lat))
-  bottomright <- data.frame(lon = max(cholera::roads$lon),
-                            lat = min(cholera::roads$lat))
-
   est.lonlat <- meterLatLong(coords, origin, topleft, bottomright)
   est.lonlat[order(est.lonlat$id), ]
 }
