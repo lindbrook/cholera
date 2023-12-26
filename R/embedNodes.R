af27cd78faf50228a6dc1629e4bc1815cddd42d2
lindbrook <lindbrook@gmail.com>
2023-12-26 13:42:00 -0800

    use landmarksB in orthoLandC()

--- R/embedNodes.R
+++ R/embedNodes.R
@@ -291,7 +291,7 @@ orthoAddrC <- function(case.set = "observed", latlong = FALSE) {
 }
 
 orthoLandC <- function(latlong = FALSE) {
-  dat <- cholera::landmarks
+  dat <- cholera::landmarksB
   sel.xy <- grepl("x", names(dat)) | grepl("y", names(dat))
   sel.latlong <- grepl("lon", names(dat)) | grepl("lat", names(dat))
   if (latlong) out <- dat[, !sel.xy]
