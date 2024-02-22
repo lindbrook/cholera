11524a4152f886c5254e6a84dbaec6e51adbe6d5
lindbrook <lindbrook@gmail.com>
2024-02-22 15:11:17 -0800

    format code

--- R/walkingPathB.R
+++ R/walkingPathB.R
@@ -91,14 +91,14 @@ walkingPathB <- function(origin = 1, destination = NULL, type = "case-pump",
   network.data <- neighborhoodDataB(vestry = vestry, latlong = latlong)
 
   if (type == "case-pump") {
-    path.data <- casePump(orgn, orgn.nm, dstn, destination, network.data, pmp, vestry,
-      weighted)
+    path.data <- casePump(orgn, orgn.nm, dstn, destination, network.data, pmp,
+      vestry, weighted)
   } else if (type == "cases") {
     path.data <- caseCase(orgn, orgn.nm, dstn, destination, include.landmarks,
       network.data, origin, vestry, weighted)
   } else if (type == "pumps") {
-    path.data <- pumpPump(orgn, orgn.nm, dstn, destination, network.data, origin, pmp,
-      vestry, weighted)
+    path.data <- pumpPump(orgn, orgn.nm, dstn, destination, network.data, 
+      origin, pmp, vestry, weighted)
   }
 
   if (length(orgn) > 1) orgn <- path.data$orgn
