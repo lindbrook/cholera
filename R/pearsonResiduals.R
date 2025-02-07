cef6e5f729707a204e10ec96aa17ced777ae7835
lindbrook <lindbrook@gmail.com>
2025-02-07 10:27:28 -0800

    amend pearsonResiduals(); add pearsonResiduals.walkingB()

--- R/pearsonResiduals.R
+++ R/pearsonResiduals.R
@@ -7,7 +7,7 @@
 #' \dontrun{
 #' pearsonResiduals(neighborhoodEuclidean())
 #' pearsonResiduals(neighborhoodVoronoi())
-#' pearsonResiduals(neighborhoodWalking())
+#' pearsonResiduals(walkingB())
 #' }
 
 pearsonResiduals <- function(x) UseMethod("pearsonResiduals", x)
@@ -21,14 +21,15 @@ pearsonResiduals.euclidean <- function(x) {
   exp <- unclass(table(neighborhoodEuclidean(pump.select = x$pump.id,
     case.set = "expected", multi.core = x$cores)$nearest.pump))
 
-  exp.data <- data.frame(pump.id = as.numeric(names(exp)),
+  exp.data <- data.frame(Pump = as.numeric(names(exp)),
                          exp.ct = exp,
                          Percent = round(100 * exp / sum(exp), 2))
 
-  obs.data <- data.frame(pump.id = as.numeric(names(obs)),
+  obs.data <- data.frame(Pump = as.numeric(names(obs)),
                          Count = obs)
 
-  output <- merge(obs.data, exp.data, by = "pump.id")
+  output <- merge(obs.data, exp.data, by = "Pump", all.y = TRUE)
+  output$Count[is.na(output$Count)] <- 0
   output$exp.ct <- NULL
   output$Expected <- sum(output$Count) * output$Percent / 100
   output$Pearson <- pearson(output)
@@ -39,11 +40,12 @@ pearsonResiduals.euclidean <- function(x) {
 pearsonResiduals.voronoi <- function(x) {
   census <- x$statistic.data
   count <- vapply(census, sum, numeric(1L))
-  output <- data.frame(pump.id = x$pump.id,
+  output <- data.frame(Pump = x$pump.data$id,
                        Count = count,
-                       Percent = round(100 * count / sum(count), 2))
-  output <- merge(output, x$expected.data[, c("pump", "pct")],
-    by.x = "pump.id", by.y = "pump")
+                       Percent = round(100 * count / sum(count), 2),
+                       row.names = NULL)
+  output <- merge(output, x$expected.data[, c("pump", "pct")], by.x = "Pump",
+    by.y = "pump")
   output$Expected <- output$pct * sum(output$Count)
   output$pct <- NULL
   output$Pearson <- pearson(output)
@@ -51,105 +53,52 @@ pearsonResiduals.voronoi <- function(x) {
 }
 
 #' @export
-pearsonResiduals.walking <- function(x) {
-  dat <- expectedWalkingLength(x)
-  obs <- dat$obs
-  exp <- dat$exp
-  exp.data <- data.frame(pump.id = as.numeric(names(exp)),
-                         exp.ct = exp,
-                         Percent = round(100 * exp / sum(exp), 2))
+pearsonResiduals.walkingB <- function(x) {
+  vars <- c("x", "y")
+  exp <- walkingB(x$p.sel, case.set = "expected")
 
-  obs.data <- data.frame(pump.id = as.numeric(names(obs)),
-                         Count = obs)
+  neigh.seg.ds <- lapply(exp$same_pump.road_segs, function(neigh) {
+    neigh <- cholera::road.segments[cholera::road.segments$id %in% neigh, ]
+    vapply(neigh$id, function(s) {
+      seg <- neigh[neigh$id == s, ]
+      dat <- rbind(stats::setNames(seg[, paste0(vars, 1)], vars),
+                   stats::setNames(seg[, paste0(vars, 2)], vars))
+      stats::dist(dat)
+    }, numeric(1L))
+  })
 
-  output <- merge(obs.data, exp.data, by = "pump.id")
-  output$exp.ct <- NULL
-  output$Expected <- sum(output$Count) * output$Percent / 100
-  output$Pearson <- pearson(output)
-  output[!is.na(output$Pearson), ]
+  whole.segs <- vapply(neigh.seg.ds, sum, numeric(1L))
+
+  split.segs <- vapply(exp$diff_pump.road_segs, function(neigh) {
+    sum(neigh$d)
+  }, numeric(1L))
+
+  split.segs <- c(split.segs, 0)
+  names(split.segs)[length(split.segs)] <- "2"
+  split.segs <- split.segs[order(as.integer(names(split.segs)))]
+
+  exp.d <- whole.segs + split.segs
+
+  obs.d <- vapply(x$neigh.edges, function(neigh) {
+    sum(x$edges[x$edges$id2 %in% neigh, "d"])
+  }, numeric(1L))
+
+  names(obs.d) <- substr(names(obs.d), 2, length(obs.d))
+
+  exp.data <- data.frame(Pump = as.numeric(names(exp.d)), exp.d = exp.d,
+    Percent = round(100 * exp.d / sum(exp.d), 2))
+
+  obs.data <- data.frame(Pump = as.numeric(names(obs.d)), Observed = obs.d)
+
+  output <- merge(obs.data, exp.data, by = "Pump", all.y = TRUE)
+  output$Observed[is.na(output$Observed)] <- 0
+  output$Expected <- sum(output$Observed) * output$Percent / 100
+  output$Pearson <- (output$Observed - output$Expected) / sqrt(output$Expected)
+  output$Pearson <- round(output$Pearson, 2)
+  output$exp.d <- NULL
+  output
 }
 
 pearson <- function(x) {
   (x$Count - x$Expected) / sqrt(x$Expected)
-}
-
-#' Compute Total Length of Roads by Neighborhood (prototype)
-#'
-#' @param x An object created by \code{neighborhoodWalking()}.
-#' @noRd
-
-expectedWalkingLength <- function(x) {
-  dat <- observedExpected(x, neighborhoodPathData(x))
-  observed.wholes <- dat$observed.wholes
-  expected.wholes <- dat$expected.wholes
-  obs.splits <- dat$exp.splits
-  obs.splits.pump <- dat$exp.splits.pump
-  obs.splits.segs <- dat$exp.splits.segs
-  exp.splits <- dat$exp.splits
-  exp.splits.pump <- dat$exp.splits.pump
-  exp.splits.segs <- dat$exp.splits.segs
-
-  observed.wholes.total.length <- lapply(observed.wholes, function(x) {
-    sum(vapply(x, segmentLength, numeric(1L)))
-  })
-
-  observed.splits.total.length <- lapply(seq_along(exp.splits), function(i) {
-    dat <- exp.splits[[i]]
-    lst <- list(rbind(dat[1, c("x", "y")], dat[2, c("x", "y")]),
-                rbind(dat[3, c("x", "y")], dat[4, c("x", "y")]))
-    data.frame(dist = vapply(lst, stats::dist, numeric(1L)),
-               pump = exp.splits.pump[[i]])
-  })
-
-  observed.splits.lengths <- do.call(rbind, observed.splits.total.length)
-  observed.splits.pumps <- sort(unique(observed.splits.lengths$pump))
-
-  observed.splits.total.length <- lapply(observed.splits.pumps, function(p) {
-    sum(observed.splits.lengths[observed.splits.lengths$pump == p, "dist"])
-  })
-
-  names(observed.splits.total.length) <- observed.splits.pumps
-
-  neighs <- names(observed.wholes.total.length)
-
-  observed.total.length <- vapply(neighs, function(nm) {
-    if (nm %in% names(observed.splits.total.length)) {
-      observed.wholes.total.length[[nm]] + observed.splits.total.length[[nm]]
-    } else {
-      observed.wholes.total.length[[nm]]
-    }
-  }, numeric(1L))
-
-  expected.wholes.total.length <- lapply(expected.wholes, function(x) {
-    sum(vapply(x, segmentLength, numeric(1L)))
-  })
-
-  expected.splits.total.length <- lapply(seq_along(exp.splits), function(i) {
-    dat <- exp.splits[[i]]
-    lst <- list(rbind(dat[1, c("x", "y")], dat[2, c("x", "y")]),
-                rbind(dat[3, c("x", "y")], dat[4, c("x", "y")]))
-    data.frame(dist = vapply(lst, stats::dist, numeric(1L)),
-               pump = exp.splits.pump[[i]])
-  })
-
-  expected.splits.lengths <- do.call(rbind, expected.splits.total.length)
-  expected.splits.pumps <- sort(unique(expected.splits.lengths$pump))
-
-  expected.splits.total.length <- lapply(expected.splits.pumps, function(p) {
-     sum(expected.splits.lengths[expected.splits.lengths$pump == p, "dist"])
-  })
-
-  names(expected.splits.total.length) <- expected.splits.pumps
-
-  neighs <- names(expected.wholes.total.length)
-
-  expected.total.length <- vapply(neighs, function(nm) {
-    if (nm %in% names(expected.splits.total.length)) {
-      expected.wholes.total.length[[nm]] + expected.splits.total.length[[nm]]
-    } else {
-      expected.wholes.total.length[[nm]]
-    }
-  }, numeric(1L))
-
-  list(obs = observed.total.length, exp = expected.total.length)
 }
