415a0f48c8710f3bd2a0634c4a501cc34f0b2250
lindbrook <lindbrook@gmail.com>
2025-02-22 13:08:00 -0800

    add exception for p8 polygon with neighborhoodWalking(6:9, case.set = "expected")

--- R/walking.R
+++ R/walking.R
@@ -306,6 +306,14 @@ plot.walking <- function(x, type = "area.points", add = FALSE,
       neighborhood.cases <- lapply(x$exp.pump.case, function(x) x - case.fix)
       periphery.cases <- parallel::mclapply(neighborhood.cases,
         peripheryCases, latlong = x$latlong, mc.cores = x$cores)
+
+      # p8 fix with pump.select = 6:9
+      if (identical(x$p.sel, 6:9)) {
+        # "Picadilly", "Bruton Street" (x2), "New Burlington Street" (x2)
+        sel <- periphery.cases$`8` %in% c("206", "4022", "3875", "7131", "7277")
+        periphery.cases$`8` <- periphery.cases$`8`[!sel]
+      }
+
       pearl.string <- parallel::mclapply(periphery.cases, travelingSalesman,
         latlong = x$latlong, tsp.method = tsp.method, mc.cores = x$cores)
 
